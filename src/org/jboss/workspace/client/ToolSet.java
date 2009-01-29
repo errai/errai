ca3a6c654fd2e0e247d4ecdc91334b573c2774e1
Mike Brock <brockm@gmail.com>
2009-01-29 22:49:54 +0000

    remove old source tree

--- src/org/jboss/workspace/client/ToolSet.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.workspace.client;
-
-import org.jboss.workspace.client.framework.WidgetProvider;
-import org.jboss.workspace.client.framework.Tool;
-
-public interface ToolSet extends WidgetProvider {
-    public Tool[] getAllProvidedTools();
-    public String getToolSetName();
-}
--- src/org/jboss/workspace/client/framework/ModuleBinding.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.workspace.client.framework;
-
-import org.jboss.workspace.client.layout.WorkspaceLayout;
-
-public interface ModuleBinding {
-    public void initModule(WorkspaceLayout workspace);
-}
