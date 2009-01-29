fbe009033fcf6680a156464bf5d8a9449a2cc2cc
Mike Brock <brockm@gmail.com>
2009-01-29 21:07:33 +0000

    initial import

---
+++ src/org/jboss/workspace/client/framework/ModuleBinding.java
@@ -0,0 +1,7 @@
+package org.jboss.workspace.client.framework;
+
+import org.jboss.workspace.client.layout.WorkspaceLayout;
+
+public interface ModuleBinding {
+    public void initModule(WorkspaceLayout workspace);
+}
