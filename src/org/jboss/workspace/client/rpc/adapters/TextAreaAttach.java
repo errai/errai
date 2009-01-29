8b671b49acb7c890d8ddb7104dde6f172ca55f05
Mike Brock <brockm@gmail.com>
2009-01-29 21:08:13 +0000

    initial import

---
+++ src/org/jboss/workspace/client/rpc/adapters/TextAreaAttach.java
@@ -0,0 +1,28 @@
+package org.jboss.workspace.client.rpc.adapters;
+
+import com.google.gwt.user.client.ui.ChangeListener;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.Widget;
+import org.jboss.workspace.client.Workspace;
+import org.jboss.workspace.client.rpc.Attachable;
+import org.jboss.workspace.client.rpc.StatePacket;
+
+public class TextAreaAttach implements Attachable {
+    public void attach(final String id, final Widget widget, final StatePacket packet) {
+        final TextArea w = (TextArea) widget;
+
+        w.addChangeListener(
+                new ChangeListener() {
+                    public void onChange(Widget widget) {
+                        packet.setParameter(id, w.getText());
+                        Workspace.notifyState(packet);
+                    }
+                }
+        );
+
+        String val = packet.getParameter(id);
+        if (val != null) {
+            w.setText(val);
+        }
+    }
+}
