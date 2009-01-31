ca782255413f3ce37cf26f7a555b40658d01e29b
Mike Brock <brockm@gmail.com>
2009-01-31 01:26:16 +0000

    create new WSGrid widget

---
+++ workspace-core/src/main/java/org/jboss/workspace/client/widgets/WSGrid.java
@@ -0,0 +1,19 @@
+package org.jboss.workspace.client.widgets;
+
+import com.google.gwt.user.client.ui.Composite;
+import org.gwt.mosaic.ui.client.Table;
+
+public class WSGrid extends Composite {
+     
+
+
+    public class WSAbstractGrid extends Composite {
+        private Table table;
+
+        public WSAbstractGrid() {
+           // table = new Table();
+
+
+        }
+    }
+}
--- workspace-sampler/src/main/java/org/jboss/workspace/sampler/client/WorkspaceSampler.java
+++ workspace-sampler/src/main/java/org/jboss/workspace/sampler/client/WorkspaceSampler.java
@@ -5,11 +5,15 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.widgetideas.table.client.FixedWidthFlexTable;
+
+
 import org.jboss.workspace.client.ToolSet;
 import org.jboss.workspace.client.Workspace;
 import org.jboss.workspace.client.framework.Tool;
 import org.jboss.workspace.client.layout.WorkspaceLayout;
 import org.jboss.workspace.client.rpc.StatePacket;
+import org.gwt.mosaic.ui.client.table.ScrollTable;
 
 import java.util.Date;
 
@@ -65,6 +69,51 @@ public class WorkspaceSampler implements EntryPoint {
 
                             public boolean multipleAllowed() {
                                 return true;
+                            }
+                        }
+                        ,
+
+                        new Tool() {
+
+                            public Widget getWidget(StatePacket packet) {
+
+                                FixedWidthFlexTable header = new FixedWidthFlexTable();
+                                header.setHTML(0, 1, "User ID");
+                                header.setHTML(0, 2, "Name");
+                                header.setHTML(0, 3, "User Type");
+                                
+                                ScrollTable.DataGrid dataGrid = new ScrollTable.DataGrid();
+
+                                dataGrid.setHTML(0, 1, "1");
+                                dataGrid.setHTML(0, 2, "John Doe");
+                                dataGrid.setHTML(0, 3, "Regular User");
+
+                                dataGrid.setHTML(1, 1, "2");
+                                dataGrid.setHTML(1, 2, "Jane Doe");
+                                dataGrid.setHTML(1, 3, "Super User");
+
+
+                                ScrollTable scrollTable = new ScrollTable(dataGrid, header);
+
+
+                                return scrollTable;
+
+                            }
+
+                            public String getName() {
+                                return "Grid Demo";  //To change body of implemented methods use File | Settings | File Templates.
+                            }
+
+                            public String getId() {
+                                return "gridDemo";  //To change body of implemented methods use File | Settings | File Templates.
+                            }
+
+                            public Image getIcon() {
+                                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                            }
+
+                            public boolean multipleAllowed() {
+                                return false;  //To change body of implemented methods use File | Settings | File Templates.
                             }
                         }
 
