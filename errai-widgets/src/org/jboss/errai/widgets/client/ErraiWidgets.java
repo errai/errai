d32b20d82fbb15913d3eda564f9d9df1e2a5311c
Mike Brock <brockm@gmail.com>
2009-10-05 21:52:51 +0000

    create GWT module for ErraiWidgets

---
+++ errai-widgets/src/org/jboss/errai/widgets/client/ErraiWidgets.java
@@ -0,0 +1,9 @@
+package org.jboss.errai.widgets.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+public class ErraiWidgets implements EntryPoint {
+    public void onModuleLoad() {
+    }
+}
