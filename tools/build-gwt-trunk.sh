5d4798c17d95fb2e732a55f967d56ace1a03d1b7
Heiko Braun <ike.braun@googlemail.com>
2009-10-08 17:48:59 +0000

    remove dependeny on external SDK installation

--- tools/build-gwt-trunk.sh
+++ tools/build-gwt-trunk.sh
@@ -1,22 +1,30 @@
 #!/bin/bash 
-export GWT_TOOLS=../tools 
-export GWT_VERSION=2.0.0-SNAPSHOT 
+export GWT_TOOLS=/Users/hbraun/dev/prj/gwt-trunk/tools/ 
+export GWT_VERSION=2.0.0-6320 
 export LOCAL_REPO=file:///Users/hbraun/dev/prj/repository.jboss.org/maven2
+export PLATFORM=mac
 
 svn up 
 ant clean 
 ant 
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-user.jar 
+mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-user.jar 
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=2.0.0-SNAPSHOT -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-servlet.jar 
+mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-servlet.jar 
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=2.0.0-SNAPSHOT -Dclassifier=mac -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-dev-mac.jar 
+mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev-$PLATFORM -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-dev-$PLATFORM.jar 
+
+mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev-oophm -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-dev-oophm.jar 
+
+mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-soyc-vis -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-soyc-vis.jar 
+
+
+mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=$GWT_VERSION -Dclassifier=$PLATFORM -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-dev-$PLATFORM.jar 
 
 mkdir temp 
-tar -zxf build/dist/gwt-mac-2.0.0-SNAPSHOT.tar.gz -C temp 
-cd temp/gwt-mac-2.0.0-SNAPSHOT 
-zip -0 gwt-mac-2.0.0-SNAPSHOT.zip lib*.jnilib 
+tar -zxf build/dist/gwt-$PLATFORM-$GWT_VERSION.tar.gz -C temp 
+cd temp/gwt-$PLATFORM-$GWT_VERSION 
+zip -0 gwt-$PLATFORM-$GWT_VERSION.zip lib*.jnilib 
 cd ../.. 
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=2.0.0-SNAPSHOT -Dclassifier=mac-libs -Dpackaging=zip -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=temp/gwt-mac-2.0.0-SNAPSHOT/gwt-mac-2.0.0-SNAPSHOT.zip 
+mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=$GWT_VERSION -Dclassifier=$PLATFORM-libs -Dpackaging=zip -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=temp/gwt-$PLATFORM-$GWT_VERSION/gwt-mac-$GWT_VERSION.zip 
