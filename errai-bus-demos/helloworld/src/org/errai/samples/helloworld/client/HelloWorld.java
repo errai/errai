40169425531fe9607f22ac8c6ae58e1c4c8420ce
Mike Brock <brockm@gmail.com>
2009-10-11 16:09:13 +0000

    fixes to get compiling again.

--- errai-bus-demos/helloworld/src/org/errai/samples/helloworld/client/HelloWorld.java
+++ errai-bus-demos/helloworld/src/org/errai/samples/helloworld/client/HelloWorld.java
@@ -3,13 +3,14 @@ package org.errai.samples.helloworld.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
 import org.jboss.errai.bus.client.CommandMessage;
-import org.jboss.errai.bus.client.ErraiClient;
+import org.jboss.errai.bus.client.ErraiBus;
 import org.jboss.errai.bus.client.MessageBus;
 
 public class HelloWorld implements EntryPoint {
-    private MessageBus bus = ErraiClient.getBus();
+    private MessageBus bus = ErraiBus.get();
 
     public void onModuleLoad() {
         Button clickMe = new Button("Click Me!");
