287f455beef5ea88f9674d1e61b1d26146d63288
Mike Brock <brockm@gmail.com>
2009-10-14 19:37:19 +0000

    test

--- errai-bus/src/main/java/org/jboss/errai/bus/client/BooleanRoutingRule.java
+++ errai-bus/src/main/java/org/jboss/errai/bus/client/BooleanRoutingRule.java
@@ -1,5 +1,8 @@
 package org.jboss.errai.bus.client;
 
+/**
+ * A RoutingRule that is called by the bus before routing any message.
+ */
 public interface BooleanRoutingRule {
     public boolean decision(CommandMessage message);
 }
