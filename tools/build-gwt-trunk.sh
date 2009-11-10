fac5e25e3ef8cb16df88ef46e864823f57f21329
Heiko Braun <ike.braun@googlemail.com>
2009-11-10 09:10:46 +0000

    Fix build script for GWT trunk

--- tools/build-gwt-trunk.sh
+++ tools/build-gwt-trunk.sh
@@ -1,30 +1,37 @@
-#!/bin/bash 
-export GWT_TOOLS=/Users/hbraun/dev/prj/gwt-trunk/tools/ 
-export GWT_VERSION=2.0.0-6320 
-export LOCAL_REPO=file:///Users/hbraun/dev/prj/repository.jboss.org/maven2
-export PLATFORM=mac
+#!/bin/sh
 
-svn up 
-ant clean 
-ant 
+# Make sure you have Ant and Maven installed
+# and execute this scriptin GWT trunk
+#
+# Taken from http://asgeirf.blogspot.com/2009/10/building-maven-artifacts-for-gwt-200.html
+# Thanks.
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-user -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-user.jar 
+if [ "x$LOCAL_REPO" == "x" ]; then
+  echo "LOCAL_REPO not set!"
+  echo "Please point to your svn repository checkout"
+  exit
+fi
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-servlet -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-servlet.jar 
+SVN_REV=`svn info | grep Revision | awk '{print $2}'`
+GWT_VERSION=2.0.0-$SVN_REV
+ant clean
+ant -Dgwt.version=$GWT_VERSION
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev-$PLATFORM -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-dev-$PLATFORM.jar 
+for artifact in `ls build/lib`; do 
+	artifact_packaging=${artifact##*.}
+	artifact_name=${artifact%.*}
+	mvn deploy:deploy-file -DgroupId=com.google.gwt \
+	-DartifactId=$artifact_name \
+	-Dversion=$GWT_VERSION \
+	-Dpackaging=$artifact_packaging \
+	-Durl=$LOCAL_REPO -DgeneratePom=true \
+	-Dfile=build/lib/$artifact
+done
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev-oophm -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-dev-oophm.jar 
+mvn deploy:deploy-file \
+	-DgroupId=com.google.gwt \
+	-DartifactId=gwt \
+	-Dversion=$GWT_VERSION -Dpackaging=zip \
+	-Durl=$LOCAL_REPO -DgeneratePom=true \
+	-Dfile=build/dist/gwt-$GWT_VERSION.zip
 
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-soyc-vis -Dversion=$GWT_VERSION -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-soyc-vis.jar 
-
-
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=$GWT_VERSION -Dclassifier=$PLATFORM -Dpackaging=jar -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=build/lib/gwt-dev-$PLATFORM.jar 
-
-mkdir temp 
-tar -zxf build/dist/gwt-$PLATFORM-$GWT_VERSION.tar.gz -C temp 
-cd temp/gwt-$PLATFORM-$GWT_VERSION 
-zip -0 -R gwt-$PLATFORM-$GWT_VERSION.zip * 
-cd ../.. 
-
-mvn deploy:deploy-file -DgroupId=com.google.gwt -DartifactId=gwt-dev -Dversion=$GWT_VERSION -Dclassifier=$PLATFORM-libs -Dpackaging=zip -Durl=$LOCAL_REPO -DgeneratePom=true -Dfile=temp/gwt-$PLATFORM-$GWT_VERSION/gwt-mac-$GWT_VERSION.zip 
