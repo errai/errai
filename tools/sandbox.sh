8ffc6a3c86bba3a6fb6bfb6f860131d2afa57e30
Heiko Braun <ike.braun@googlemail.com>
2009-11-26 12:37:42 +0000

    remove platform identifier

---
+++ tools/sandbox.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# Creates an Errai Project skeleton
+#
+
+mvn archetype:generate \
+	-DarchetypeGroupId=org.jboss.errai \
+	-DarchetypeArtifactId=sandbox-archetype \
+	-DarchetypeVersion=1.0-SNAPSHOT \
+	-DarchetypeRepository=http://snapshots.jboss.org/maven2 \
+	-Dpackage=org.jboss.errai
+	
+	
