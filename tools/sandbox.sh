6b7a30b5865f0d14cd79faea04ce81cc073371a8
Heiko Braun <ike.braun@googlemail.com>
2009-12-02 13:49:44 +0000

    use archetype 1.0-Beta1

--- tools/sandbox.sh
+++ tools/sandbox.sh
@@ -7,7 +7,7 @@
 mvn archetype:generate \
 	-DarchetypeGroupId=org.jboss.errai \
 	-DarchetypeArtifactId=sandbox-archetype \
-	-DarchetypeVersion=1.0-SNAPSHOT \
+	-DarchetypeVersion=1.0-Beta1 \
 	-DarchetypeRepository=http://snapshots.jboss.org/maven2 \
 #	-Dpackage=org.jboss.errai
 	
