a9231ffd57268263de0cb9b5b16876ec0b78c554
Heiko Braun <ike.braun@googlemail.com>
2009-12-02 20:10:17 +0000

    Point archetype repo to repository.jboss.com

--- tools/sandbox.sh
+++ tools/sandbox.sh
@@ -8,7 +8,7 @@ mvn archetype:generate \
 	-DarchetypeGroupId=org.jboss.errai \
 	-DarchetypeArtifactId=sandbox-archetype \
 	-DarchetypeVersion=1.0-Beta1 \
-	-DarchetypeRepository=http://snapshots.jboss.org/maven2 \
+	-DarchetypeRepository=http://repository.jboss.org/maven2 \
 #	-Dpackage=org.jboss.errai
 	
 	
