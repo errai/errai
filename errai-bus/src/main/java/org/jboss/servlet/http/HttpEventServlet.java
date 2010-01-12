d63d3eb09cd138ea14b6f602ddc4959c60f06acf
Mike Brock <brockm@gmail.com>
2010-01-12 19:22:27 +0000

    delete compat libs

--- errai-bus/src/main/java/org/jboss/servlet/http/HttpEventServlet.java
+++
@@ -1,5 +0,0 @@
-package org.jboss.servlet.http;
-
-public interface HttpEventServlet extends javax.servlet.Servlet {
-   public void event(HttpEvent httpEvent) throws java.io.IOException, javax.servlet.ServletException;
-}
