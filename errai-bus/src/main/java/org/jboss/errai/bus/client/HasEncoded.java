95a78e99090acff81ed7010d174bfc443d044f77
Lillian Angel <langel@redhat.com>
2010-01-20 21:18:16 +0000

    Added documentation

--- errai-bus/src/main/java/org/jboss/errai/bus/client/HasEncoded.java
+++ errai-bus/src/main/java/org/jboss/errai/bus/client/HasEncoded.java
@@ -1,5 +1,18 @@
 package org.jboss.errai.bus.client;
 
+
+/**
+ * This interface indicates to the bus whether or not the Message being routed already contains a pre-built JSON
+ * encoding. It is implemented by the <tt>JSONMessage</tt> class. The main purpose is to accelerate the performance
+ * of the message building, so the bus does not need to deconstruct the message. Rather, it indicates that the
+ * underlying message has already been constructed.
+ */
 public interface HasEncoded {
+
+    /**
+     * Gets the encoded JSON string
+     *
+     * @return the encoded JSON string
+     */
     public String getEncoded();
 }
