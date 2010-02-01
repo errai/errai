0ff5281d307ef12cf6b9de3297e2e1d995cafed0
Lillian Angel <langel@redhat.com>
2010-02-01 15:08:41 +0000

    Added documentation

--- errai-bus/src/main/java/org/jboss/errai/bus/server/SessionControl.java
+++ errai-bus/src/main/java/org/jboss/errai/bus/server/SessionControl.java
@@ -2,7 +2,21 @@ package org.jboss.errai.bus.server;
 
 import org.jboss.errai.bus.client.Message;
 
+/**
+ * This interface, <tt>SessionControl</tt>, provides functions for checking up on a session
+ */
 public interface SessionControl {
+
+    /**
+     * Returns true if the current session is still valid
+     *
+     * @return true if session is valid, false otherwise
+     */
     public boolean isSessionValid();
+
+    /**
+     * This function indicates activity on the session, so the session knows when the last time there was activity.
+     * For example, <tt>MessageQueue</tt> relies on this function to figure out whether or not to timeout
+     */
     public void activity();
 }
--- errai-bus/src/main/java/org/jboss/errai/bus/server/SessionProvider.java
+++ errai-bus/src/main/java/org/jboss/errai/bus/server/SessionProvider.java
@@ -2,6 +2,19 @@ package org.jboss.errai.bus.server;
 
 import org.jboss.errai.bus.server.QueueSession;
 
+/**
+ * This interface, <tt>SessionProvider</tt>, is a template for creating a session provider based on the type of
+ * session specified
+ *
+ * @param <T> - the type of session to create the provider for
+ */
 public interface SessionProvider<T> {
+
+    /**
+     * Gets an instance of <tt>QueueSession</tt> using the external session reference given.
+     *
+     * @param externSessRef - the external session reference
+     * @return an instance of <tt>QueueSession</tt>
+     */
     public QueueSession getSession(T externSessRef);
 }
