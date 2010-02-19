68e9b882f6b58f2f64003b0f7ef6c47b22bd7235
Heiko Braun <ike.braun@googlemail.com>
2010-02-19 09:29:19 +0000

    Include quickstart in root codebase

---
+++ quickstart/src/main/java/com/foo/bar/app/server/HelloWorldService.java
@@ -0,0 +1,35 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.foo.bar.app.server;
+
+import org.jboss.errai.bus.client.api.Message;
+import org.jboss.errai.bus.client.api.MessageCallback;
+import org.jboss.errai.bus.server.annotations.Service;
+
+@Service("HelloWorld")
+public class HelloWorldService implements MessageCallback {
+
+  public void callback(Message message)
+  {
+    System.out.println("Hello " + message.get(String.class, "messageText"));
+  }
+}
