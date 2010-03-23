7a2426b77e8b56f541bc593a4dd3a965022db96d
Mike Brock <brockm@gmail.com>
2010-03-23 21:45:36 +0000

    removing forms

---
+++ errai-tools/src/main/java/org/jboss/errai/tools/monitoring/MainMonitorGUI.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2009 JBoss, a divison Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.errai.tools.monitoring;
+
+import org.jboss.errai.bus.client.framework.MessageBus;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainMonitorGUI extends JFrame implements Attachable {
+    public static final String APPLICATION_NAME = "ErraiBus Monitor";
+
+    private JTabbedPane tabbedPane1;
+    private ServerMonitorPanel serverMonitorPanel;
+    private Map<Object, ServerMonitorPanel> remoteBuses;
+    private MessageBus serverBus;
+
+    private Dataservice dataStore;
+
+    private ActivityProcessor processor;
+
+    public MainMonitorGUI(Dataservice service, MessageBus serverBus) {
+        this.serverBus = serverBus;
+        this.dataStore = service;
+
+        tabbedPane1 = new JTabbedPane();
+
+        setTitle(APPLICATION_NAME);
+        getContentPane().add(tabbedPane1);
+
+        serverMonitorPanel = new ServerMonitorPanel(this, serverBus, "Server");
+
+        tabbedPane1.add("Server", serverMonitorPanel.getPanel());
+
+        remoteBuses = new HashMap<Object, ServerMonitorPanel>();
+
+        setMinimumSize(new Dimension(600, 500));
+        setSize(600, 500);
+        setLocation(150, 150);
+    }
+
+    public ServerMonitorPanel getServerMonitorPanel() {
+        return serverMonitorPanel;
+    }
+
+    public void attachRemoteBus(Object id) {
+        if (remoteBuses.containsKey(id)) {
+            return;
+        }
+
+        ServerMonitorPanel newServerMonitor = new ServerMonitorPanel(this, new ClientBusProxyImpl(serverBus), String.valueOf(id));
+        newServerMonitor.attach(processor);
+
+        remoteBuses.put(id, newServerMonitor);
+
+        tabbedPane1.add(String.valueOf(id), newServerMonitor.getPanel());
+    }
+
+    public ServerMonitorPanel getBus(Object id) {
+        return "Server".equals(id) ? serverMonitorPanel : remoteBuses.get(id);
+    }
+
+    public Dataservice getDataStore() {
+        return dataStore;
+    }
+
+    public void attach(ActivityProcessor proc) {
+        this.processor = proc;
+
+        proc.registerEvent(EventType.BUS_EVENT, new MessageMonitor() {
+            public void monitorEvent(MessageEvent event) {
+                switch (event.getSubType()) {
+                    case REMOTE_ATTACHED:
+                        attachRemoteBus(event.getFromBus());
+                        break;
+                    case SERVER_SUBSCRIBE:
+                    case REMOTE_SUBSCRIBE:
+                        if (!"Server".equals(event.getFromBus()) && !remoteBuses.containsKey(event.getFromBus())) {
+                             return;
+                        }
+
+                        getBus(event.getFromBus()).addServiceName(event.getSubject());
+                        break;
+                    case SERVER_UNSUBSCRIBE:
+                    case REMOTE_UNSUBSCRIBE:
+                        getBus(event.getFromBus()).removeServiceName(event.getSubject());
+                        break;
+                }
+            }
+        });
+
+        serverMonitorPanel.attach(proc);
+    }
+}
