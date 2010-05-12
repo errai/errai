72e4baacd3be2c02f136b954d85bcb9e1f827459
Mike Brock <brockm@gmail.com>
2010-05-12 18:30:23 +0000

    remove @Multi annotation.

--- errai-bus-demos/queryservice/src/main/java/org/errai/samples/queryservice/client/QueryServiceRemote.java
+++ errai-bus-demos/queryservice/src/main/java/org/errai/samples/queryservice/client/QueryServiceRemote.java
@@ -1,10 +1,8 @@
 package org.errai.samples.queryservice.client;
 
-import org.jboss.errai.bus.server.annotations.Multi;
 import org.jboss.errai.bus.server.annotations.Remote;
 
 @Remote
-@Multi
 public interface QueryServiceRemote {
     public String[] query(String queryString);
     public String append(String... arg);
