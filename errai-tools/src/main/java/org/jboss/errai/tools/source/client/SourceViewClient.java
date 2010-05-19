9f13b2bd8d4e898bb361ca49864c79e27d0c62d2
Heiko Braun <ike.braun@googlemail.com>
2010-05-19 01:21:28 +0000

    Added source view toolset

---
+++ errai-tools/src/main/java/org/jboss/errai/tools/source/client/SourceViewClient.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2009 JBoss, a divison Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.errai.tools.source.client;
+
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
+import org.gwt.mosaic.ui.client.layout.BoxLayout;
+import org.gwt.mosaic.ui.client.layout.BoxLayoutData;
+import org.gwt.mosaic.ui.client.layout.LayoutPanel;
+import org.jboss.errai.bus.client.ErraiBus;
+import org.jboss.errai.bus.client.api.Message;
+import org.jboss.errai.bus.client.api.MessageCallback;
+import org.jboss.errai.bus.client.api.base.MessageBuilder;
+import org.jboss.errai.bus.client.framework.MessageBus;
+import org.jboss.errai.bus.client.protocols.MessageParts;
+import org.jboss.errai.workspaces.client.api.ProvisioningCallback;
+import org.jboss.errai.workspaces.client.api.WidgetProvider;
+import org.jboss.errai.workspaces.client.api.annotations.LoadTool;
+
+/**
+ * @author: Heiko Braun <hbraun@redhat.com>
+ * @date: May 18, 2010
+ */
+@LoadTool(name="SourceView", group = "Tools")
+public class SourceViewClient implements WidgetProvider
+{
+  private MessageBus bus = ErraiBus.get();
+
+  class SourceViewWidget extends LayoutPanel
+  {
+    private SourcePanel formatted;
+
+    public SourceViewWidget(boolean hasInput)
+    {
+      super(new BoxLayout(BoxLayout.Orientation.VERTICAL));
+
+      if(hasInput)
+      {
+        final TextBox sourceName = new TextBox();
+
+        this.add(sourceName);
+        this.add(new Button("View Source", new ClickHandler()
+        {
+          public void onClick(ClickEvent clickEvent)
+          {
+            String text = sourceName.getText();
+            requestSource(text);
+          }
+        }));
+
+      }
+      
+      createSourcePanel(this);
+      setupListener();
+    }
+
+    public SourceViewWidget(String[] sourceNames)
+    {
+      super(new BoxLayout(BoxLayout.Orientation.VERTICAL));
+
+      LayoutPanel toolbar = new LayoutPanel(new BoxLayout(BoxLayout.Orientation.HORIZONTAL));
+      final ListBox listBox = new ListBox();
+      for(String s : sourceNames)
+        listBox.addItem(s);
+      toolbar.add(listBox, new BoxLayoutData(BoxLayoutData.FillStyle.HORIZONTAL));
+      toolbar.add(new Button("View", new ClickHandler()
+      {
+        public void onClick(ClickEvent clickEvent)
+        {
+          requestSource(listBox.getItemText(listBox.getSelectedIndex())); 
+        }
+      }));
+
+      this.add(toolbar);
+
+      listBox.addChangeHandler(new ChangeHandler()
+      {
+        public void onChange(ChangeEvent changeEvent)
+        {
+          requestSource(listBox.getItemText(listBox.getSelectedIndex()));
+        }
+      });
+
+      createSourcePanel(this);
+      setupListener();
+    }
+
+    public void requestSource(String className)
+    {
+      MessageBuilder.createMessage()
+          .toSubject("SourceViewService")
+          .signalling()
+          .with(MessageParts.ReplyTo, "SourceViewClient")
+          .with("className", className)
+          .noErrorHandling()
+          .sendNowWith(bus);
+    }
+
+    private void setupListener()
+    {
+      bus.subscribe("SourceViewClient", new MessageCallback()
+      {
+        public void callback(Message message)
+        {
+          String source = message.get(String.class, "source");
+          formatted.setSource(source);
+        }
+      });
+    }
+
+    private void createSourcePanel(LayoutPanel panel)
+    {
+      formatted = new SourcePanel();
+      panel.add(formatted, new BoxLayoutData(BoxLayoutData.FillStyle.BOTH));
+    }
+  }
+
+  public void provideWidget(ProvisioningCallback callback)
+  {
+    SourceViewWidget viewWidget = new SourceViewWidget(
+        new String[] {"org/jboss/errai/demo/thestore/client/modules/domain/User.java"}
+    );
+
+    callback.onSuccess(viewWidget);
+  }
+}
---
+++ errai-tools/src/main/java/org/jboss/errai/tools/source/server/SourceViewService.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2009 JBoss, a divison Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.errai.tools.source.server;
+
+import com.google.inject.Inject;
+import org.jboss.errai.bus.client.api.Message;
+import org.jboss.errai.bus.client.api.MessageCallback;
+import org.jboss.errai.bus.client.api.base.MessageBuilder;
+import org.jboss.errai.bus.client.framework.RequestDispatcher;
+import org.jboss.errai.bus.server.annotations.Service;
+import org.jboss.errai.tools.source.server.JavaToHTML;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * @author: Heiko Braun <hbraun@redhat.com>
+ * @date: May 18, 2010
+ */
+@Service
+public class SourceViewService implements MessageCallback
+{
+
+  @Inject
+  RequestDispatcher dispatcher;
+
+  public void callback(Message message)
+  {
+    String sourceClassName = message.get(String.class, "className");
+    System.out.println("source view :" + sourceClassName);
+
+    String rawSource = sourceAsString(sourceClassName);
+    MessageBuilder.createConversation(message)
+        .subjectProvided()
+        .signalling()
+        .with("source", JavaToHTML.format(rawSource))
+        .noErrorHandling().sendNowWith(dispatcher);
+  }
+
+  private String sourceAsString(String sourceClassName)
+  {
+    String source = null;
+
+    ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    InputStream in = cl.getResourceAsStream(sourceClassName);
+    if(in!=null)
+    source = convertStreamToString(in);
+
+    return source;
+  }
+
+  public String convertStreamToString(InputStream is)
+  {
+    try
+    {
+      StringBuilder sb = new StringBuilder();
+      String line;
+
+      try {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        while ((line = reader.readLine()) != null) {
+          sb.append(line).append("\n");
+        }
+      } finally {
+        is.close();
+      }
+      return sb.toString();
+    }
+    catch (IOException e)
+    {
+      throw new RuntimeException("Failed to read stream", e);
+    }
+
+  }
+}
