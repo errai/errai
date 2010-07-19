2cf61ccb2e2ef882d167e7ed2e3b5356b2db0fbb
Mike Brock <brockm@gmail.com>
2010-07-19 21:04:44 +0000

    code cleanup

--- errai-bus-demos/helloworld/src/main/java/org/errai/samples/helloworld/server/HelloWorldService.java
+++ errai-bus-demos/helloworld/src/main/java/org/errai/samples/helloworld/server/HelloWorldService.java
@@ -1,8 +1,6 @@
 package org.errai.samples.helloworld.server;
 
-import com.google.inject.Inject;
 import org.jboss.errai.bus.client.api.Message;
-import org.jboss.errai.bus.client.framework.RequestDispatcher;
 import org.jboss.errai.bus.client.util.SimpleMessage;
 import org.jboss.errai.bus.server.annotations.ApplicationComponent;
 import org.jboss.errai.bus.server.annotations.Service;
@@ -10,10 +8,6 @@ import org.jboss.errai.bus.server.annotations.Service;
 
 @ApplicationComponent
 public class HelloWorldService {
-
-    @Inject
-    public RequestDispatcher dispatcher;
-
     public void helloWorld(@Service("HelloWorldService") Message message) {
         SimpleMessage.send(message, "Hello, World");
     }
