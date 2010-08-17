c6285896e7210ec3d3bd755a3ce613881ec836cf
Heiko Braun <ike.braun@googlemail.com>
2010-08-17 14:02:40 +0000

    Implement collapsable menu in docbook docs

---
+++ userguide/src/js/menu-collapsed.js
@@ -0,0 +1,13 @@
+
+function initMenu() {   
+  $("div > dl dd").hide();
+  $("div dl dt:has(.chapter)").click(
+    function(event) {          
+        $(this).find("a").attr ('href', '#');
+        $(this).next().slideToggle('fast');
+        event.stopImmediatePropagation();
+      }
+    );
+    
+  }
+$(document).ready(function() {initMenu();});
