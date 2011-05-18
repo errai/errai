1230f99eb002d017ac90840b11ef8e3cfe1db6d9
Mike Brock <brockm@gmail.com>
2011-05-18 17:54:24 -0400

    ...

--- integration/src/test/java/org/jboss/errai/cdi/client/test/EventProducerIntegrationTest.java
+++ integration/src/test/java/org/jboss/errai/cdi/client/test/EventProducerIntegrationTest.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.jboss.errai.cdi.client.EventProducerTestModule;
 
 import com.google.gwt.user.client.Timer;
+import org.junit.Test;
 
 /**
  * Tests CDI event producers.
