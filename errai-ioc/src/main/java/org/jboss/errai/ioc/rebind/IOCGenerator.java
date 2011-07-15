ad6a2012e935758e8687e0efaeb0a24820c25ee8
Mike Brock <brockm@gmail.com>
2011-07-15 16:55:50 -0400

    add option to display IOC boostrap generator output: -Derrai.ioc.generator.print_out_result=true

--- errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/IOCGenerator.java
+++ errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/IOCGenerator.java
@@ -365,11 +365,15 @@ public class IOCGenerator extends Generator {
 
     blockBuilder.finish();
 
-    System.out.println("----Emitting Class--->\n\n");
-    System.out.println(classBuilder.toJavaString());
-    System.out.println("<---Emitting Class----");
+    String generated = classBuilder.toJavaString();
 
-    sourceWriter.print(classBuilder.toJavaString());
+    if (Boolean.getBoolean("errai.ioc.generator.print_out_result")) {
+      System.out.println("----Emitting Class--->\n\n");
+      System.out.println(generated);
+      System.out.println("<---Emitting Class----");
+    }
+
+    sourceWriter.print(generated);
   }
 
   public void addType(final MetaClass type) {
