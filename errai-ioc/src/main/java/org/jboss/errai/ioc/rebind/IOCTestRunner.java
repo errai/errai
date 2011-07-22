3ff2fb4035627b6e7c51aef5e8c72ef3815a52b4
Mike Brock <brockm@gmail.com>
2011-07-22 13:31:02 -0400

    fixed IOCTestRunner. Now support running single test at a time.

--- errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/IOCTestRunner.java
+++ errai-ioc/src/main/java/org/jboss/errai/ioc/rebind/IOCTestRunner.java
@@ -1,118 +1,190 @@
 package org.jboss.errai.ioc.rebind;
 
 import com.google.gwt.junit.JUnitShell;
+import com.google.gwt.junit.client.GWTTestCase;
+import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
-import junit.framework.TestSuite;
 import org.jboss.errai.ioc.client.IOCClientTestCase;
 import org.jboss.errai.ioc.client.InterfaceInjectionContext;
-import org.junit.Test;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.ParentRunner;
+import org.junit.runners.model.TestClass;
+import sun.rmi.transport.ObjectTable;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Mike Brock <cbrock@redhat.com>
  */
-public class IOCTestRunner extends Runner {
+public class IOCTestRunner extends ParentRunner<Runner> {
   public static boolean SIMULATED = Boolean.getBoolean("errai.ioc.debug.simulated_client");
 
-  private Class<? extends TestCase> toRun;
 
-  public IOCTestRunner(Class<? extends TestCase> toRun) {
-    this.toRun = toRun;
+  List<Runner> runners = new ArrayList<Runner>();
+  private Object instance;
+
+  public IOCTestRunner(Class<? extends TestCase> toRun) throws Throwable {
+    super(toRun);
+
+
+    for (final Method method : toRun.getDeclaredMethods()) {
+      if (method.getName().startsWith("test") && method.getParameterTypes().length == 0) {
+        runners.add(new Runner() {
+          @Override
+          public Description getDescription() {
+            return Description.createTestDescription(getTestClass().getJavaClass(), method.getName());
+          }
+
+          @Override
+          public void run(RunNotifier notifier) {
+            final IOCClientTestCase iocClientTestCase = (IOCClientTestCase) getInstance();
+
+            Description description = getDescription();
+
+            notifier.fireTestStarted(description);
+            TestResult result = new TestResult();
+
+            try {
+              if (SIMULATED) {
+                iocClientTestCase.gwtSetUp();
+                method.invoke(getInstance());
+              }
+              else {
+                iocClientTestCase.setName(method.getName());
+                JUnitShell.runTest(iocClientTestCase, result);
+              }
+            }
+            catch (InvocationTargetException e) {
+              notifier.fireTestFailure(new Failure(description, e.getTargetException()));
+              return;
+            }
+            catch (Exception e) {
+              notifier.fireTestFailure(new Failure(description, e));
+              return;
+            }
+
+            notifier.fireTestRunFinished(new Result());
+
+            if (!result.wasSuccessful()) {
+              notifier.fireTestFailure(new Failure(description, null));
+            }
+            else {
+              notifier.fireTestFinished(description);
+            }
+          }
+        });
+      }
+    }
+  }
+
+
+  public Object getInstance() {
+    if (instance == null) {
+      try {
+        instance = getTestClass().getJavaClass().newInstance();
+      }
+      catch (InstantiationException e) {
+        throw new RuntimeException("Could not intantiate test class: " + getTestClass().getJavaClass().getName(), e);
+      }
+      catch (IllegalAccessException e) {
+        throw new RuntimeException("Could not intantiate test class: " + getTestClass().getJavaClass().getName(), e);
+      }
+      catch (Exception e) {
+        throw new RuntimeException("could not bootstrap", e);
+      }
+    }
+    return instance;
+  }
+
+
+  @Override
+  protected List<Runner> getChildren() {
+    return runners;
   }
 
   @Override
-  public Description getDescription() {
-    Description description = Description.createSuiteDescription(toRun);
+  protected Description describeChild(Runner child) {
+    return child.getDescription();
+  }
 
-    for (Method method : toRun.getDeclaredMethods()) {
-      if (method.getName().startsWith("test") && method.getParameterTypes().length == 0) {
-        Description testMethod = Description.createTestDescription(toRun, method.getName());
-
-        description.addChild(testMethod);
-      }
-    }
-
-    return description;
+  @Override
+  protected void runChild(Runner child, RunNotifier notifier) {
+    child.run(notifier);
   }
 
   @Override
   public void run(RunNotifier notifier) {
-    Object instance;
+    final IOCClientTestCase iocClientTestCase = (IOCClientTestCase) getInstance();
 
-    try {
-      if (SIMULATED) {
+    if (SIMULATED) {
 
-        instance = toRun.newInstance();
-
-        if (instance instanceof IOCClientTestCase) {
-          final IOCClientTestCase iocClientTestCase = (IOCClientTestCase) instance;
-          iocClientTestCase.setInitializer(new IOCClientTestCase.ContainerBootstrapper() {
-            @Override
-            public InterfaceInjectionContext bootstrap() {
-              try {
-                MockIOCGenerator mockIOCGenerator = new MockIOCGenerator();
-                mockIOCGenerator.setPackageFilter(iocClientTestCase.getModulePackage());
-                return mockIOCGenerator.generate().newInstance().bootstrapContainer();
-              }
-              catch (Exception e) {
-                throw new RuntimeException("failed to run in emulated mode", e);
-              }
+      if (instance instanceof IOCClientTestCase) {
+        iocClientTestCase.setInitializer(new IOCClientTestCase.ContainerBootstrapper() {
+          @Override
+          public InterfaceInjectionContext bootstrap() {
+            try {
+              MockIOCGenerator mockIOCGenerator = new MockIOCGenerator();
+              mockIOCGenerator.setPackageFilter(iocClientTestCase.getModulePackage());
+              return mockIOCGenerator.generate().newInstance().bootstrapContainer();
             }
-          });
+            catch (Exception e) {
+              throw new RuntimeException("failed to run in emulated mode", e);
+            }
+          }
+        });
 
-          iocClientTestCase.setForcePureJava(true);
-          iocClientTestCase.gwtSetUp();
-        }
-
-      }
-      else {
-        IOCTestSuite suite = new IOCTestSuite(toRun, notifier);
-        suite.run(new TestResult());
-
-        return;
+        iocClientTestCase.setForcePureJava(true);
       }
 
-
+      super.run(notifier);
     }
-    catch (InstantiationException e) {
-      throw new RuntimeException("Could not intantiate test class: " + toRun.getName(), e);
-    }
-    catch (IllegalAccessException e) {
-      throw new RuntimeException("Could not intantiate test class: " + toRun.getName(), e);
-    }
-    catch (Exception e) {
-      throw new RuntimeException("could not bootstrap", e);
-    }
-
-    for (Description description : getDescription().getChildren()) {
-      try {
-        notifier.fireTestStarted(Description.createTestDescription(toRun, description.getMethodName()));
-        Result result = new Result();
-        toRun.getDeclaredMethod(description.getMethodName()).invoke(instance);
-        notifier.fireTestRunFinished(result);
-
-        if (!result.wasSuccessful()) {
-          notifier.fireTestFailure(new Failure(description, null));
-        }
-        else {
-          notifier.fireTestFinished(description);
-        }
-
-      }
-      catch (InvocationTargetException e) {
-        notifier.fireTestFailure(new Failure(description, e.getTargetException()));
-      }
-      catch (Exception e) {
-        notifier.fireTestFailure(new Failure(description, e));
-      }
+    else {
+      super.run(notifier);
     }
   }
+
+//    try {
+//      if (SIMULATED) {
+//
+//
+//      }
+//      else {
+//        IOCTestSuite suite = new IOCTestSuite(toRun, notifier);
+//        suite.run(new TestResult());
+//
+//        return;
+//      }
+//
+//
+//    }
+//    catch (InstantiationException e) {
+//      throw new RuntimeException("Could not intantiate test class: " + toRun.getName(), e);
+//    }
+//    catch (IllegalAccessException e) {
+//      throw new RuntimeException("Could not intantiate test class: " + toRun.getName(), e);
+//    }
+//    catch (Exception e) {
+//      throw new RuntimeException("could not bootstrap", e);
+//    }
+//
+//    for (Description description : getDescription().getChildren()) {
+//      try {
+//
+//      }
+//      catch (InvocationTargetException e) {
+//        notifier.fireTestFailure(new Failure(description, e.getTargetException()));
+//      }
+//      catch (Exception e) {
+//        notifier.fireTestFailure(new Failure(description, e));
+//      }
+//    }
+//  }
 }
