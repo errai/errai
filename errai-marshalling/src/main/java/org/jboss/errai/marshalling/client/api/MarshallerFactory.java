c82da22923298b7f90f16899cda3aecb022fc636
Mike Brock <brockm@gmail.com>
2011-09-26 17:57:49 -0400

    fix.

--- errai-marshalling/src/main/java/org/jboss/errai/marshalling/client/api/MarshallerFactory.java
+++ errai-marshalling/src/main/java/org/jboss/errai/marshalling/client/api/MarshallerFactory.java
@@ -4,5 +4,5 @@ package org.jboss.errai.marshalling.client.api;
  * @author Mike Brock <cbrock@redhat.com>
  */
 public interface MarshallerFactory {
-  <T> Marshaller<T> getMarshaller(String encodingType, Class<T> encodedType);
+  <T> Marshaller<?, T> getMarshaller(String encodingType, Class<T> encodedType);
 }
