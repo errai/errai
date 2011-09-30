ba8b73548a0d239962ffeb732d8918c8664e6b26
Christian Sadilek <christian.sadilek@gmail.com>
2011-09-30 19:31:42 -0400

    renamed TestEntity to fix the build

---
+++ errai-marshalling/src/test/java/org/jboss/errai/marshalling/tests/res/Entity.java
@@ -0,0 +1,40 @@
+package org.jboss.errai.marshalling.tests.res;
+
+import org.jboss.errai.common.client.api.annotations.Portable;
+import org.jboss.errai.marshalling.client.api.annotations.MapsTo;
+
+import java.util.List;
+
+/**
+ * @author Mike Brock <cbrock@redhat.com>
+ */
+@Portable
+public class Entity {
+  private final String foo;
+  private final String foobar;
+  private final String bar;
+//  private final List<TestEntity> entityList;
+
+  private int cachedHashCode = -1;
+
+  public Entity(@MapsTo("foo") String foo, @MapsTo("bar") String bar
+  //        , @MapsTo("entityList") List<TestEntity> entityList
+  ) {
+    this.foo = foo;
+    this.bar = bar;
+  //  this.entityList = entityList;
+    this.foobar = foo + bar;
+  }
+
+  public String getFoo() {
+    return foo;
+  }
+
+  @Override
+  public int hashCode() {
+    if (cachedHashCode == -1) {
+      cachedHashCode = foo.hashCode() + 37 * bar.hashCode();
+    }
+    return cachedHashCode;
+  }
+}
