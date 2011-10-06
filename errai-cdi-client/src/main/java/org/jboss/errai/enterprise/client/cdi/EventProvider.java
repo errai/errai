e6f50f902d8c05ca6f7055127d169c6c010aba01
Jonathan Fuerth <jfuerth@gmail.com>
2011-10-06 12:26:51 -0400

    Added some hints about getting these projects to compile in Eclipse

--- errai-cdi-client/src/main/java/org/jboss/errai/enterprise/client/cdi/EventProvider.java
+++ errai-cdi-client/src/main/java/org/jboss/errai/enterprise/client/cdi/EventProvider.java
@@ -1,14 +1,15 @@
 package org.jboss.errai.enterprise.client.cdi;
 
-import org.jboss.errai.enterprise.client.cdi.api.CDI;
-import org.jboss.errai.enterprise.client.cdi.api.Conversation;
-import org.jboss.errai.ioc.client.ContextualProviderContext;
-import org.jboss.errai.ioc.client.api.IOCProvider;
+import java.lang.annotation.Annotation;
 
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.inject.Provider;
-import java.lang.annotation.Annotation;
+
+import org.jboss.errai.enterprise.client.cdi.api.CDI;
+import org.jboss.errai.enterprise.client.cdi.api.Conversation;
+import org.jboss.errai.ioc.client.ContextualProviderContext;
+import org.jboss.errai.ioc.client.api.IOCProvider;
 
 @IOCProvider
 public class EventProvider implements Provider<Event<?>> {
@@ -16,8 +17,19 @@ public class EventProvider implements Provider<Event<?>> {
   ContextualProviderContext context;
 
   public Event<?> get() {
+    
+    /*
+     * If you see a compile error here, ensure that you are using Errai's custom
+     * version of javax.enterprise.event.Event, which comes from the
+     * errai-javax-enterprise project. The errai-cdi-client POM is set up this
+     * way.
+     * 
+     * Eclipse users: seeing an error here probably indicates that M2E has
+     * clobbered your errai-javax-enterprise source folder settings. To fix your
+     * setup, see the README in the root of errai-javax-enterprise.
+     */
     return new Event<Object>() {
-      private Class eventType = (context.getTypeArguments().length == 1 ? context.getTypeArguments()[0] : Object.class);
+      private Class<?> eventType = (context.getTypeArguments().length == 1 ? context.getTypeArguments()[0] : Object.class);
       private Conversation conversation;
       private Annotation[] _qualifiers = context.getQualifiers();
 
