cf3a6d4882aba45f2d5ca0bdab7828577848c806
Jonathan Fuerth <jfuerth@gmail.com>
2011-11-09 16:37:06 -0500

    Fix for ERRAI-163

--- errai-common/src/main/java/org/jboss/errai/common/metadata/JBossVFSDIr.java
+++ errai-common/src/main/java/org/jboss/errai/common/metadata/JBossVFSDIr.java
@@ -1,18 +1,23 @@
 package org.jboss.errai.common.metadata;
 
-import org.jboss.vfs.VirtualFile;
-import org.reflections.vfs.Vfs;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
+import org.jboss.vfs.VirtualFile;
+import org.reflections.vfs.Vfs;
+import org.reflections.vfs.Vfs.File;
+
+import com.google.common.collect.AbstractIterator;
 
 /**
  * Implementation of {@link Vfs.Dir} to support JBoss VFS for proper classpath scanning on JBoss AS.
  *
  * @author Mike Brock <cbrock@redhat.com>
+ * @author Jonathan Fuerth <jfuerth@redhat.com>
  */
 public class JBossVFSDIr implements Vfs.Dir {
   private VirtualFile virtualFile;
@@ -24,8 +29,9 @@ public class JBossVFSDIr implements Vfs.Dir {
         virtualFile = (VirtualFile) content;
       }
       else {
-        throw new RuntimeException("URL content is not a JBoss VFS VirtualFile. Type is: " + (content == null ?
-                "null" : content.getClass().getName()));
+        throw new IllegalArgumentException(
+            "URL content is not a JBoss VFS VirtualFile. Type is: " +
+                (content == null ? "null" : content.getClass().getName()));
       }
     }
     catch (IOException e) {
@@ -43,54 +49,41 @@ public class JBossVFSDIr implements Vfs.Dir {
     return new Iterable<Vfs.File>() {
       @Override
       public Iterator<Vfs.File> iterator() {
-        final ArrayList<VirtualFile> toVisit = new ArrayList<VirtualFile>();
+        final List<VirtualFile> toVisit = new ArrayList<VirtualFile>(virtualFile.getChildren());
 
-        {
-          toVisit.addAll(virtualFile.getChildren());
-        }
-
-        return new Iterator<Vfs.File>() {
+        return new AbstractIterator<Vfs.File>() {
 
           @Override
-          public boolean hasNext() {
-            return !toVisit.isEmpty();
-          }
+          protected File computeNext() {
+            while (!toVisit.isEmpty()) {
+              final VirtualFile nextFile = toVisit.remove(toVisit.size() - 1);
+              if (nextFile.isDirectory()) {
+                toVisit.addAll(nextFile.getChildren());
+                continue;
+              }
+              return new Vfs.File() {
+                @Override
+                public String getName() {
+                  return nextFile.getName();
+                }
 
-          @Override
-          public Vfs.File next() {
-            final VirtualFile file = toVisit.remove(toVisit.size() - 1);
+                @Override
+                public String getRelativePath() {
+                  return nextFile.getPathName();
+                }
 
-            if (file.isDirectory()) {
-              toVisit.addAll(file.getChildren());
-              return next();
+                @Override
+                public String getFullPath() {
+                  return nextFile.getPathName();
+                }
+
+                @Override
+                public InputStream openInputStream() throws IOException {
+                  return nextFile.openStream();
+                }
+              };
             }
-
-            return new Vfs.File() {
-              @Override
-              public String getName() {
-                return file.getName();
-              }
-
-              @Override
-              public String getRelativePath() {
-                return file.getPathName();
-              }
-
-              @Override
-              public String getFullPath() {
-                return file.getPathName();
-              }
-
-              @Override
-              public InputStream openInputStream() throws IOException {
-                return file.openStream();
-              }
-            };
-          }
-
-          @Override
-          public void remove() {
-            throw new IllegalAccessError();
+            return endOfData();
           }
         };
       }
