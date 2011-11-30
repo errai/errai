96e68b88e23f65d01b85a372bf5a3b75d47b481b
Christian Sadilek <christian.sadilek@gmail.com>
2011-11-30 13:20:00 -0500

    OSS license info

--- demos/jaxrs-demo/src/main/java/org/jboss/errai/samples/restdemo/server/CustomerServiceImpl.java
+++ demos/jaxrs-demo/src/main/java/org/jboss/errai/samples/restdemo/server/CustomerServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 JBoss, a divison Red Hat, Inc
+ * Copyright 2011 JBoss, a division of Red Hat, Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- errai-jaxrs-client/src/main/java/org/jboss/errai/enterprise/rebind/JaxrsProxyMethodGenerator.java
+++ errai-jaxrs-client/src/main/java/org/jboss/errai/enterprise/rebind/JaxrsProxyMethodGenerator.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2011 JBoss, a division of Red Hat, Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.jboss.errai.enterprise.rebind;
 
 import static org.jboss.errai.enterprise.rebind.TypeMarshaller.demarshal;
