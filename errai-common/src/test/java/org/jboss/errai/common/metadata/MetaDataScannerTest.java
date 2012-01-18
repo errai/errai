c7fded0c2586bb4caf96b5155b867f670d542380
Jonathan Fuerth <jfuerth@gmail.com>
2012-01-18 12:30:35 -0500

    Redid metadata scanner test for problematic filenames using ShrinkWrap rather than a file that git keeps losing track of. :)

--- errai-common/src/test/java/org/jboss/errai/common/metadata/MetaDataScannerTest.java
+++ errai-common/src/test/java/org/jboss/errai/common/metadata/MetaDataScannerTest.java
@@ -17,45 +17,80 @@
 package org.jboss.errai.common.metadata;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
+import org.reflections.vfs.Vfs;
 import org.reflections.vfs.ZipDir;
 
 /**
  * Tests for {@link MetaDataScanner}.
- * 
+ *
  * @author Christian Sadilek <csadilek@redhat.com>
  */
 public class MetaDataScannerTest {
 
   @Test
   public void testJarUrlDecodingWithSimpleName() throws Exception {
-    testJarUrlDecoding("test.jar");
+    testJarUrlDecoding("test");
   }
-  
+
   @Test
   public void testJarUrlDecodingWithSpecialCharacters() throws Exception {
-    testJarUrlDecoding("testjar-\u00e4\u00f6\u00e9\u00f8 +abc!@$%^&()_+}{.jar");
+    testJarUrlDecoding("testjar-\u00e4\u00f6\u00e9\u00f8 +abc!@$%^&()_+}{");
   }
-   
-  private void testJarUrlDecoding(String jarFileName) {
-    URL testJarURL = getClass().getClassLoader().getResource(jarFileName);
-    assertNotNull("Jar file not found: " + jarFileName, testJarURL);
+
+  private void testJarUrlDecoding(String jarFileName) throws IOException {
+    File erraiAppPropertiesFile = new File(System.getProperty("java.io.tmpdir"), "ErraiApp.properties");
+    erraiAppPropertiesFile.createNewFile();
+
+    File jarFile = File.createTempFile(jarFileName, ".jar");
+    JavaArchive jarArchive = ShrinkWrap.create(JavaArchive.class)
+        .addClass(getClass())
+        .addAsResource(erraiAppPropertiesFile);
+    jarArchive.as(ZipExporter.class).exportTo(jarFile, true);
+
+    erraiAppPropertiesFile.delete();
+
+    URL testJarURL = jarFile.toURI().toURL();
+    assertNotNull("Jar file not found: " + jarFile, testJarURL);
 
     ClassLoader loader = URLClassLoader.newInstance(new URL[] { testJarURL }, getClass().getClassLoader());
     List<URL> urls = MetaDataScanner.getConfigUrls(loader);
     assertFalse("No URLs returned", urls.isEmpty());
     String[] segments = urls.get(0).getPath().split("/");
-    
+
     assertTrue("No path segments found in URL", segments.length > 0);
-    assertEquals("URL not properly decoded", jarFileName +"!", segments[segments.length - 1]);
+    assertEquals("URL not properly decoded", jarFile.getName() +"!", segments[segments.length - 1]);
     assertNotNull("Could not open jar", new ZipDir(urls.get(0)).getFiles());
+
+    Set<String> zipContents = new TreeSet<String>();
+    for (Vfs.File path : new ZipDir(urls.get(0)).getFiles()) {
+      if (!path.getRelativePath().endsWith("/")) {
+        zipContents.add(path.getRelativePath());
+      }
+    }
+
+    Set<String> expectedContents = new TreeSet<String>();
+    expectedContents.add("ErraiApp.properties");
+    expectedContents.add("org/jboss/errai/common/metadata/MetaDataScannerTest.class");
+
+    assertEquals("Wrong file contents", expectedContents, zipContents);
+
+    // delete if test passes (otherwise, we may want to inspect its contents)
+    jarFile.delete();
   }
 }
