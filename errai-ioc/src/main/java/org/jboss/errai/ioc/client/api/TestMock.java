940cf7c1d4ebeb69a69ce52ccdb1034b11666131
Mike Brock <brockm@gmail.com>
2012-03-12 19:27:04 -0400

    - added JavaDoc for @TestMock

--- errai-ioc/src/main/java/org/jboss/errai/ioc/client/api/TestMock.java
+++ errai-ioc/src/main/java/org/jboss/errai/ioc/client/api/TestMock.java
@@ -22,6 +22,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Specifies the annotated bean is a replacement type for all matching common subtypes in the context of a unit test.
+ * <p>
+ * When running unit tests with Errai IOC managed applications, beans which have been annotated with <tt>@TestMock</tt>
+ * will be given injection consideration preference over any other matching beans.
+ * <p>
+ * For the purposes of beans annotated with <tt>@TestMock</tt>, the bean is treated as an otherwise normal bean, with
+ * normal resolution rules, including the use of qualifiers.
+ *
  * @author Mike Brock
  */
 @Retention(RetentionPolicy.RUNTIME)
