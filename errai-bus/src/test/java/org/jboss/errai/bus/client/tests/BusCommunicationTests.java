ac9256c6eb3404c2443dd73a7dbaa4973e32a3ff
Mike Brock <brockm@gmail.com>
2012-03-23 16:33:39 -0400

    code cleanup

--- errai-bus/src/test/java/org/jboss/errai/bus/client/tests/BusCommunicationTests.java
+++ errai-bus/src/test/java/org/jboss/errai/bus/client/tests/BusCommunicationTests.java
@@ -331,9 +331,7 @@ public class BusCommunicationTests extends AbstractErraiTest {
     });
   }
 
-
   public void testMultipleEndpointsOnRemoteServiceWithAuthentication() {
-
     class TestCount {
       private int countdown;
 
