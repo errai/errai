edb897e29a492bb786a92d0501086a5316c0a8b4
Jonathan Fuerth <jfuerth@gmail.com>
2012-03-26 13:22:15 -0400

    Fixed ERRAI-238: MetaClass.isAssignableFrom() does not take into account interfaces on supertypes
    https://issues.jboss.org/browse/ERRAI-238

---
+++ errai-codegen/src/test/java/org/jboss/errai/codegen/framework/tests/model/tree/Grandparent.java
@@ -0,0 +1,5 @@
+package org.jboss.errai.codegen.framework.tests.model.tree;
+
+public class Grandparent implements GrandparentInterface {
+
+}
---
+++ errai-codegen/src/test/java/org/jboss/errai/codegen/framework/tests/model/tree/GrandparentSuperInterface.java
@@ -0,0 +1,5 @@
+package org.jboss.errai.codegen.framework.tests.model.tree;
+
+public interface GrandparentSuperInterface {
+
+}
