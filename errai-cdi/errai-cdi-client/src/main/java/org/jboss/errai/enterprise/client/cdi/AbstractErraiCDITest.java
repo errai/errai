8b972d3a197f3df8a7dd055777eca382ab91407a
Christian Sadilek <christian.sadilek@gmail.com>
2012-03-27 16:18:16 -0400

    minor refactoring: organized imports

--- errai-cdi/errai-cdi-client/src/main/java/org/jboss/errai/enterprise/client/cdi/AbstractErraiCDITest.java
+++ errai-cdi/errai-cdi-client/src/main/java/org/jboss/errai/enterprise/client/cdi/AbstractErraiCDITest.java
@@ -19,9 +19,7 @@ package org.jboss.errai.enterprise.client.cdi;
 import org.jboss.errai.common.client.api.extension.InitVotes;
 import org.jboss.errai.enterprise.client.cdi.api.CDI;
 import org.jboss.errai.ioc.client.Container;
-import org.jboss.errai.ioc.client.api.Bootstrapper;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.junit.client.GWTTestCase;
 
 /**
@@ -40,11 +38,9 @@ public abstract class AbstractErraiCDITest extends GWTTestCase {
 
     CDI.removePostInitTasks();
 
-    new Container().onModuleLoad();
-
-
     // Unfortunately, GWTTestCase does not call our inherited module's onModuleLoad() methods
     // http://code.google.com/p/google-web-toolkit/issues/detail?id=3791
+    new Container().onModuleLoad();
     new CDIClientBootstrap().onModuleLoad();
   }
 }
