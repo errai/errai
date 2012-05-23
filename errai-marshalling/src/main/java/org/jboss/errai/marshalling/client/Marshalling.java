a26c75473799b231ca7f852f47c0a8e4968e2988
Jonathan Fuerth <jfuerth@gmail.com>
2012-05-23 19:11:27 -0400

    Added javadoc to the main user-facing Marshalling API

--- errai-marshalling/src/main/java/org/jboss/errai/marshalling/client/Marshalling.java
+++ errai-marshalling/src/main/java/org/jboss/errai/marshalling/client/Marshalling.java
@@ -30,13 +30,41 @@ import org.jboss.errai.marshalling.client.marshallers.MapMarshaller;
 import org.jboss.errai.marshalling.client.util.NumbersUtils;
 
 /**
+ * A collection of static methods for accomplishing common tasks with the Errai
+ * marshalling API.
+ *
  * @author Mike Brock
+ * @author Jonathan Fuerth <jfuerth@redhat.com>
  */
 public abstract class Marshalling {
+
+  /**
+   * Returns true if the given type is marshallable by the Errai Marshalling
+   * system, and false otherwise.
+   * <p>
+   * Marshallable types include all native Java types, most built-in Java API
+   * types, types annotated with {@code @Portable}, types configured for
+   * marshalling via {@code ErraiApp.properties}, and arrays, Collections, and
+   * Maps of marshallable types.
+   *
+   * @param type
+   *          The type to check for marshallability.
+   * @return True for marshallable types and false for non-marshallable types.
+   */
   public static boolean canHandle(Class<?> type) {
     return MarshallingSessionProviderFactory.getProvider().hasMarshaller(type.getName());
   }
 
+  /**
+   * Returns a JSON representation of the given object, recursively including
+   * all of its nested attributes.
+   *
+   * @param obj
+   *          The object to marshall. Should be of a type for which
+   *          {@link #canHandle(Class)} returns true. Null is permitted.
+   * @return The JSON representation of the given object and all nested
+   *         properties reachable from it.
+   */
   public static String toJSON(Object obj) {
     if (obj == null) {
       return "{\"" + SerializationParts.ENCODED_TYPE + "\":\"java.lang.Object\",\""
@@ -53,22 +81,70 @@ public abstract class Marshalling {
     }
   }
 
+  /**
+   * Appends a JSON representation of the given object to the given Appendable,
+   * recursively including all of its nested attributes.
+   *
+   * @param appendTo
+   *          the Appendable to write the JSON representation to.
+   * @param obj
+   *          The object to marshall. Should be of a type for which
+   *          {@link #canHandle(Class)} returns true. Null is permitted.
+   * @return The JSON representation of the given object and all nested
+   *         properties reachable from it.
+   */
   public static void toJSON(Appendable appendTo, Object obj) throws IOException {
     appendTo.append(toJSON(obj));
   }
 
+  /**
+   * Works the same as a call to {@link #toJSON(Object)}, but may perform better.
+   *
+   * @param obj The map to marshal to JSON.
+   * @return The JSON representation of the map.
+   */
   public static String toJSON(Map<Object, Object> obj) {
     return MapMarshaller.INSTANCE.marshall(obj, MarshallingSessionProviderFactory.getEncoding());
   }
 
+  /**
+   * Works the same as a call to {@link #toJSON(Object)}, but may perform better.
+   *
+   * @param obj The list to marshal to JSON.
+   * @return The JSON representation of the list.
+   */
   public static String toJSON(List arr) {
     return ListMarshaller.INSTANCE.marshall(arr, MarshallingSessionProviderFactory.getEncoding());
   }
 
+  /**
+   * Converts the given JSON message to a Java object, recursively decoding
+   * nested attributes contained in that message.
+   *
+   * @param json
+   *          The JSON representation of the object graph to demarshall.
+   * @param type
+   *          The expected type of the root of the object graph.
+   * @return the root of the reconstructed object graph.
+   */
   public static <T> T fromJSON(String json, Class<T> type) {
     return fromJSON(json, type, null);
   }
-  
+
+  /**
+   * Converts the given JSON message (which is likely a collection) to a Java
+   * object, recursively decoding nested attributes contained in that message.
+   *
+   * @param json
+   *          The JSON representation of the object graph to demarshall.
+   * @param type
+   *          The expected type of the root of the object graph.
+   * @param assumedElementType
+   *          the type of elements assumed to be in the root collection. A null
+   *          value means that either the root object is not a collection, or
+   *          its element type is provided in the JSON message.
+   * @return the root of the reconstructed object graph.
+   */
   public static <T> T fromJSON(String json, Class<T> type, Class<?> assumedElementType) {
     EJValue parsedValue = ParserFactory.get().parse(json);
     MarshallingSession session = MarshallingSessionProviderFactory.getDecoding();
@@ -79,6 +155,15 @@ public abstract class Marshalling {
     return (T) marshallerInstance.demarshall(parsedValue, session);
   }
 
+  /**
+   * Converts the given JSON message to a Java object, recursively decoding
+   * nested attributes contained in that message, which must contain type
+   * information for the root object.
+   *
+   * @param json
+   *          The JSON representation of the object graph to demarshall.
+   * @return the root of the reconstructed object graph.
+   */
   public static Object fromJSON(String json) {
     return fromJSON(json, Object.class);
   }
