c23af8f7d9c9bcbbba2ac312e0a04a5d0aef64e3
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2012-09-05 15:17:33 -0400

    Added class file headers and JavaDoc for Errai UI API Annotations

--- errai-ui/src/main/java/org/jboss/errai/ui/shared/api/annotations/AutoBound.java
+++ errai-ui/src/main/java/org/jboss/errai/ui/shared/api/annotations/AutoBound.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 JBoss, by Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.errai.ui.shared.api.annotations;
 
 import java.lang.annotation.Documented;
@@ -8,7 +23,12 @@ import java.lang.annotation.Target;
 
 import org.jboss.errai.databinding.client.api.DataBinder;
 
+import com.google.gwt.user.client.ui.Composite;
+
 /**
+ * This annotation may only be used in subclasses of {@link Composite} that has been annotated with {@link Templated},
+ * or in a super-class of said {@link Composite} types.
+ * <p>
  * Indicates that the annotated {@link DataBinder} is used to automatically bind all enclosing {@link DataField}s to
  * properties of the corresponding data model (the model instance associated with the data binder instance). The
  * enclosing data fields are all fields annotated with {@link Bound} and {@link DataField} of the class that defines the
--- errai-ui/src/main/java/org/jboss/errai/ui/shared/api/annotations/SinkNative.java
+++ errai-ui/src/main/java/org/jboss/errai/ui/shared/api/annotations/SinkNative.java
@@ -7,10 +7,74 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.ui.Composite;
+
+/**
+ * This annotation may only be used in subclasses of {@link Composite} that has been annotated with {@link Templated},
+ * or in a super-class of said {@link Composite} types.
+ * <p>
+ * Indicate the types of events to be handled when used in conjunction with a corresponding method that has been
+ * annotated with {@link EventHandler}; these types are specified using a bit mask in the {@link #value()} attribute.
+ * <p>
+ * <b>Native events on DOM Elements</b>
+ * <p>
+ * This use of this annotation does not require that target {@link Element} instances be referenced via
+ * {@link DataField} in the {@link Templated} {@link Composite} component; they may also target un-referenced
+ * <code>data-field</code> elements from the corresponding HTML template.
+ * <p>
+ * Example:
+ * <p>
+ * 
+ * <pre>
+ * &#064;Templated
+ * public class QuickHandlerComponent extends Composite
+ * {
+ *    &#064;DataField
+ *    private AnchorElement link = DOM.createAnchor().cast();
+ * 
+ *    &#064;EventHandler(&quot;link&quot;)
+ *    &#064;SinkNative(Event.ONMOUSEOVER)
+ *    public void doSomething(Event e)
+ *    {
+ *       // do something
+ *    }
+ * 
+ *    &#064;EventHandler(&quot;div&quot;)
+ *    &#064;SinkNative(Event.ONCLICK | Event.ONMOUSEOVER)
+ *    public void doSomethingElse(Event e)
+ *    {
+ *       // do something with an element not otherwise referenced in our component
+ *    }
+ * }
+ * </pre>
+ * 
+ * <p>
+ * <b>See also:</b> {@link EventHandler}, {@link Templated}, {@link DataField}
+ * 
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ */
 @Inherited
 @Documented
 @Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface SinkNative {
-  int value();
+public @interface SinkNative
+{
+   /**
+    * Specifies the types of {@link Event} instances that are handled by the annotated handler method. Multiple event
+    * types may be handled using a bit-mask of the form:
+    * <p>
+    * 
+    * <pre>
+    * &#064;EventHandler(&quot;div&quot;)
+    * &#064;SinkNative(Event.ONCLICK | Event.ONMOUSEOVER)
+    * public void doSomething(Event e)
+    * {
+    *    // do something
+    * }
+    * </pre>
+    */
+   int value();
 }
