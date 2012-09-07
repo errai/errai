6e962fc033ceb9c07ba2d83d84dc356e809cc48b
Mike Brock <brockm@gmail.com>
2012-09-07 17:33:03 -0400

    fix unit tests in reflections

--- errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/JavaCodeSerializerTest.java
+++ errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/JavaCodeSerializerTest.java
@@ -14,88 +14,92 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
 import static java.util.Arrays.asList;
+
 import java.util.Set;
 
 /** */
 public class JavaCodeSerializerTest {
 
-    @Test
-    public void generateAndSave() {
-        //generate
-        Predicate<String> filter = new FilterBuilder().include("org.jboss.errai.reflections.TestModel\\$.*").include("org.jboss.errai.reflections.MyTestModelStore.*");
+  @Test
+  public void generateAndSave() {
+    //generate
+    Predicate<String> filter = new FilterBuilder().include("org.jboss.errai.reflections.TestModel\\$.*").include("org.jboss.errai.reflections.MyTestModelStore.*");
 
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .filterInputsBy(filter)
-                .setScanners(new TypesScanner(), new TypeElementsScanner())
-                .setUrls(asList(ClasspathHelper.forClass(TestModel.class))));
+    Reflections reflections = new Reflections(new ConfigurationBuilder()
+        .filterInputsBy(filter)
+        .setScanners(new TypesScanner(), new TypeElementsScanner())
+        .setUrls(asList(ClasspathHelper.forClass(TestModel.class))));
 
-        //no re-serializing
-        Set<String> stringSet = reflections.getStore().get(TypesScanner.class).keySet();
-        for (String s : stringSet) {
-            if (stringSet.contains("MyTestModelStore")) {
-                Assert.fail(s + " has been re-serialized");
-            }
-        }
+    reflections.scan();
 
-        //save
-        String filename = ReflectionsTest.getUserDir() + "/src/test/java/org.jboss.errai.reflections.MyTestModelStore";
-        reflections.save(filename, new JavaCodeSerializer());
+    //no re-serializing
+    Set<String> stringSet = reflections.getStore().get(TypesScanner.class).keySet();
+    for (String s : stringSet) {
+      if (stringSet.contains("MyTestModelStore")) {
+        Assert.fail(s + " has been re-serialized");
+      }
     }
 
-    @Test
-    public void classCanBeFetchedFromJavaCodeSerializer() {
-        //class
-        Class<? extends JavaCodeSerializer.IClass> aClass1 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C1.class;
-        Class aClass = JavaCodeSerializer.resolveClass(aClass1);
-        Assert.assertEquals(TestModel.C1.class, aClass);
-    }
+    //save
+    String filename = ReflectionsTest.getUserDir() + "/src/test/java/org.jboss.errai.reflections.MyTestModelStore";
+    reflections.save(filename, new JavaCodeSerializer());
+  }
 
-    @Test
-    public void methodCanBeFetchedFromJavaCodeSerializer() throws NoSuchMethodException {
-        //method
-        Class<? extends JavaCodeSerializer.IMethod> method1 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.m1.class;
-        Method m1 = JavaCodeSerializer.resolveMethod(method1);
-        Assert.assertEquals(TestModel.C4.class.getDeclaredMethod("m1"), m1);
+  @Test
+  public void classCanBeFetchedFromJavaCodeSerializer() {
+    //class
+    Class<? extends JavaCodeSerializer.IClass> aClass1 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C1.class;
+    Class aClass = JavaCodeSerializer.resolveClass(aClass1);
+    Assert.assertEquals(TestModel.C1.class, aClass);
+  }
 
-        //overloaded method with parameters
-        Class<? extends JavaCodeSerializer.IMethod> method2 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.m1_int_java$lang$String$$.class;
-        Method m2 = JavaCodeSerializer.resolveMethod(method2);
-        Assert.assertEquals(TestModel.C4.class.getDeclaredMethod("m1", int.class, String[].class), m2);
+  @Test
+  public void methodCanBeFetchedFromJavaCodeSerializer() throws NoSuchMethodException {
+    //method
+    Class<? extends JavaCodeSerializer.IMethod> method1 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.m1.class;
+    Method m1 = JavaCodeSerializer.resolveMethod(method1);
+    Assert.assertEquals(TestModel.C4.class.getDeclaredMethod("m1"), m1);
 
-        //overloaded method with parameters and multi dimensional array
-        Class<? extends JavaCodeSerializer.IMethod> method3 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.m1_int$$$$_java$lang$String$$$$.class;
-        Method m3 = JavaCodeSerializer.resolveMethod(method3);
-        Assert.assertEquals(TestModel.C4.class.getDeclaredMethod("m1", int[][].class, String[][].class), m3);
-    }
+    //overloaded method with parameters
+    Class<? extends JavaCodeSerializer.IMethod> method2 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.m1_int_java$lang$String$$.class;
+    Method m2 = JavaCodeSerializer.resolveMethod(method2);
+    Assert.assertEquals(TestModel.C4.class.getDeclaredMethod("m1", int.class, String[].class), m2);
 
-    @Test
-    public void fieldCanBeFetchedFromJavaCodeSerializer() throws NoSuchMethodException, NoSuchFieldException {
-        //field
-        Class<? extends JavaCodeSerializer.IField> field1 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.f1.class;
-        Field f1 = JavaCodeSerializer.resolveField(field1);
-        Assert.assertEquals(TestModel.C4.class.getDeclaredField("f1"), f1);
-    }
+    //overloaded method with parameters and multi dimensional array
+    Class<? extends JavaCodeSerializer.IMethod> method3 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.m1_int$$$$_java$lang$String$$$$.class;
+    Method m3 = JavaCodeSerializer.resolveMethod(method3);
+    Assert.assertEquals(TestModel.C4.class.getDeclaredMethod("m1", int[][].class, String[][].class), m3);
+  }
 
-    @Test
-    public void fieldCanBeFetchedFromAnnotationUsingJavaCodeSerializer() throws NoSuchFieldException {
-        //field can be fetched from annotation
-        Mark mark = SomeClassToMark.class.getAnnotation(Mark.class);
-        Class<? extends JavaCodeSerializer.IField> markedElement = mark.value();
-        Field markedField = JavaCodeSerializer.resolveField(markedElement);
-        Assert.assertEquals(TestModel.C4.class.getDeclaredField("f1"), markedField);
-    }
+  @Test
+  public void fieldCanBeFetchedFromJavaCodeSerializer() throws NoSuchMethodException, NoSuchFieldException {
+    //field
+    Class<? extends JavaCodeSerializer.IField> field1 = MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.f1.class;
+    Field f1 = JavaCodeSerializer.resolveField(field1);
+    Assert.assertEquals(TestModel.C4.class.getDeclaredField("f1"), f1);
+  }
 
-    //test case - use the store in annotations
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface Mark {
-        Class<? extends JavaCodeSerializer.IField> value();
-    }
+  @Test
+  public void fieldCanBeFetchedFromAnnotationUsingJavaCodeSerializer() throws NoSuchFieldException {
+    //field can be fetched from annotation
+    Mark mark = SomeClassToMark.class.getAnnotation(Mark.class);
+    Class<? extends JavaCodeSerializer.IField> markedElement = mark.value();
+    Field markedField = JavaCodeSerializer.resolveField(markedElement);
+    Assert.assertEquals(TestModel.C4.class.getDeclaredField("f1"), markedField);
+  }
 
-    @Mark(MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.f1.class)
-    //annotation can use Store to specify class/field/method in a semi strong typed manner
-    @SuppressWarnings({"UnusedDeclaration"})
-    public static class SomeClassToMark {
+  //test case - use the store in annotations
+  @Retention(RetentionPolicy.RUNTIME)
+  public @interface Mark {
+    Class<? extends JavaCodeSerializer.IField> value();
+  }
 
-    }
+  @Mark(MyTestModelStore.org.jboss.errai.reflections.TestModel$C4.f1.class)
+  //annotation can use Store to specify class/field/method in a semi strong typed manner
+  @SuppressWarnings({"UnusedDeclaration"})
+  public static class SomeClassToMark {
+
+  }
 }
--- errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
+++ errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
@@ -1,8 +1,60 @@
-//generated using Reflections JavaCodeSerializer [Fri Sep 07 17:30:43 EDT 2012]
+//generated using Reflections JavaCodeSerializer [Wed Sep 05 10:36:27 EDT 2012]
 package org.jboss.errai.reflections;
 
 import static org.jboss.errai.reflections.serializers.JavaCodeSerializer.*;
 
 public interface MyTestModelStore extends IElement {
 
+	public interface org extends IPackage {
+		public interface jboss extends IPackage {
+			public interface errai extends IPackage {
+				public interface reflections extends IPackage {
+					public interface TestModel$AC1 extends IClass {
+					}
+					public interface TestModel$AC2 extends IClass {
+						public interface value extends IMethod {}
+					}
+					public interface TestModel$AF1 extends IClass {
+						public interface value extends IMethod {}
+					}
+					public interface TestModel$AI1 extends IClass {
+					}
+					public interface TestModel$AI2 extends IClass {
+					}
+					public interface TestModel$AM1 extends IClass {
+						public interface value extends IMethod {}
+					}
+					public interface TestModel$C1 extends IClass {
+					}
+					public interface TestModel$C2 extends IClass {
+					}
+					public interface TestModel$C3 extends IClass {
+					}
+					public interface TestModel$C4 extends IClass {
+						public interface f1 extends IField {}
+						public interface f3 extends IField {}
+						public interface f2 extends IField {}
+						public interface c2toC3 extends IMethod {}
+						public interface m1_int_java$lang$String$$ extends IMethod {}
+						public interface m1_int$$$$_java$lang$String$$$$ extends IMethod {}
+						public interface m1 extends IMethod {}
+						public interface m3 extends IMethod {}
+						public interface m4 extends IMethod {}
+					}
+					public interface TestModel$C5 extends IClass {
+					}
+					public interface TestModel$C6 extends IClass {
+					}
+					public interface TestModel$I1 extends IClass {
+					}
+					public interface TestModel$I2 extends IClass {
+					}
+					public interface TestModel$I3 extends IClass {
+					}
+					public interface TestModel$MAI1 extends IClass {
+					}
+				}
+			}
+		}
+	}
 }
