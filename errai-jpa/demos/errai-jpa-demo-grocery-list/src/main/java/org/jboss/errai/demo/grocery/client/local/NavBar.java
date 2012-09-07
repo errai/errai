93557cb14edf5ad0e65b67b805bec56c5d5cd885
Jonathan Fuerth <jfuerth@gmail.com>
2012-09-07 16:32:41 -0400

    Changed NavBar component to use the typesafe navigation links

--- errai-jpa/demos/errai-jpa-demo-grocery-list/src/main/java/org/jboss/errai/demo/grocery/client/local/NavBar.java
+++ errai-jpa/demos/errai-jpa-demo-grocery-list/src/main/java/org/jboss/errai/demo/grocery/client/local/NavBar.java
@@ -1,10 +1,39 @@
 package org.jboss.errai.demo.grocery.client.local;
 
+import javax.inject.Inject;
+
+import org.jboss.errai.ui.nav.client.local.TransitionTo;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 
 @Templated
 public class NavBar extends Composite {
 
+  @Inject @DataField Anchor home;
+  @Inject @DataField Anchor items;
+  @Inject @DataField Anchor stores;
+
+  @Inject TransitionTo<WelcomePage> homeTab;
+  @Inject TransitionTo<StoresPage> storesTab;
+  @Inject TransitionTo<ItemListPage> itemsTab;
+
+  @EventHandler("home")
+  public void onHomeButtonClick(ClickEvent e) {
+    homeTab.go();
+  }
+
+  @EventHandler("items")
+  public void onItemsButtonClick(ClickEvent e) {
+    itemsTab.go();
+  }
+
+  @EventHandler("stores")
+  public void onStoresButtonClick(ClickEvent e) {
+    storesTab.go();
+  }
 }
