672ebf0778bb3bfe0562db3e693816a1a9ec256c
Jonathan Fuerth <jfuerth@gmail.com>
2012-11-02 16:48:15 -0400

    Moved tests out of the framework package into the tests package (using Wormhle to get at pkg provate bus impl details)

--- errai-bus/src/test/java/org/jboss/errai/bus/client/framework/Wormhole.java
+++ errai-bus/src/test/java/org/jboss/errai/bus/client/framework/Wormhole.java
@@ -1,5 +1,7 @@
 package org.jboss.errai.bus.client.framework;
 
+import java.util.Set;
+
 /**
  * Allows tests in the proper package to access private bus features.
  *
@@ -12,6 +14,10 @@ public class Wormhole {
     String oldUrl = ((ClientMessageBusImpl) bus).IN_SERVICE_ENTRY_POINT;
     ((ClientMessageBusImpl) bus).IN_SERVICE_ENTRY_POINT = newUrl;
     return oldUrl;
+  }
+
+  public static Set<String> getRemoteSubscriptions(ClientMessageBus bus) {
+    return ((ClientMessageBusImpl) bus).getRemoteSubscriptions();
   }
 
 }
