6f6a5564179be0c0419b9403ad0a59e6169e4843
Jonathan Fuerth <jfuerth@gmail.com>
2012-11-06 15:02:19 -0500

    Added checkbox for manipulating and observing bus.initialized state

--- errai-bus/demos/bus-stress-test/src/main/java/org/jboss/errai/demo/busstress/client/local/BusStatusWidget.java
+++ errai-bus/demos/bus-stress-test/src/main/java/org/jboss/errai/demo/busstress/client/local/BusStatusWidget.java
@@ -12,9 +12,11 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 
 public class BusStatusWidget extends Composite implements BusLifecycleListener {
 
@@ -35,12 +37,17 @@ public class BusStatusWidget extends Composite implements BusLifecycleListener {
   private final HorizontalPanel me = new HorizontalPanel();
   private final Resources resources = GWT.create(Resources.class);
   private final Image statusImage = new Image(resources.white());
+  private final Label statusLabel = new Label("Unknown");
   private final Button stopTrueButton = new Button("bus.stop(true)");
   private final Button stopFalseButton = new Button("bus.stop(false)");
   private final Button initButton = new Button("bus.init()");
+  private final CheckBox busInitialized = new CheckBox("bus.initialized");
 
   public BusStatusWidget() {
     initWidget(me);
+
+    statusLabel.setWordWrap(false);
+    statusLabel.setWidth("6em");
 
     stopTrueButton.addClickHandler(new ClickHandler() {
 
@@ -66,30 +73,48 @@ public class BusStatusWidget extends Composite implements BusLifecycleListener {
       }
     });
 
+    busInitialized.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(ClickEvent event) {
+        ((ClientMessageBusImpl) ErraiBus.get()).setInitialized(busInitialized.getValue());
+      }
+    });
+
     me.add(statusImage);
+    me.add(statusLabel);
     me.add(stopFalseButton);
     me.add(stopTrueButton);
     me.add(initButton);
+    me.add(busInitialized);
   }
 
   @Override
   public void busAssociating(BusLifecycleEvent e) {
+    statusLabel.setText("Connecting");
     statusImage.setResource(resources.yellow());
+    busInitialized.setValue(e.getBus().isInitialized());
   }
 
   @Override
   public void busDisassociating(BusLifecycleEvent e) {
+    statusLabel.setText("Local Only");
     statusImage.setResource(resources.red());
+    busInitialized.setValue(e.getBus().isInitialized());
   }
 
   @Override
   public void busOnline(BusLifecycleEvent e) {
+    statusLabel.setText("Connected");
     statusImage.setResource(resources.green());
+    busInitialized.setValue(e.getBus().isInitialized());
   }
 
   @Override
   public void busOffline(BusLifecycleEvent e) {
+    statusLabel.setText("Connecting");
     statusImage.setResource(resources.yellow());
+    busInitialized.setValue(e.getBus().isInitialized());
   }
 
 }
