4f510341c2f544cf0335b4c4009fa00ea3bbd60c
Tom Carchrae <tom@intellecti.ca>
2012-11-22 20:51:30 -0800

    added keyword deserialization test

--- errai-jaxrs/errai-jaxrs-client/src/test/java/org/jboss/errai/enterprise/jaxrs/client/test/JacksonIntegrationTest.java
+++ errai-jaxrs/errai-jaxrs-client/src/test/java/org/jboss/errai/enterprise/jaxrs/client/test/JacksonIntegrationTest.java
@@ -40,180 +40,209 @@ import org.junit.Test;
  */
 public class JacksonIntegrationTest extends AbstractErraiJaxrsTest {
 
-  @Override
-  public String getModuleName() {
-    return "org.jboss.errai.enterprise.jaxrs.TestModule";
-  }
+	@Override
+	public String getModuleName() {
+		return "org.jboss.errai.enterprise.jaxrs.TestModule";
+	}
 
-  @Override
-  protected void gwtSetUp() throws Exception {
-    super.gwtSetUp();
-    RestClient.setJacksonMarshallingActive(true);
-  }
+	@Override
+	protected void gwtSetUp() throws Exception {
+		super.gwtSetUp();
+		RestClient.setJacksonMarshallingActive(true);
+	}
 
-  @Test
-  @SuppressWarnings("serial")
-  public void testJacksonMarshalling() {
-    delayTestFinish(5000);
+	@Test
+	@SuppressWarnings("serial")
+	public void testJacksonMarshalling() {
+		delayTestFinish(5000);
 
-    final User user =
-        new User(11l, "first", "last", 20, Gender.MALE, new User(12l, "first2", "last2", 40, Gender.FEMALE, null));
-    final User friend1 =
-        new User(13l, "friend1-first", "friend1-last", 1, Gender.MALE, null);
-    final User friend2 =
-        new User(14l, "friend2-first", "friend2-last", 2, Gender.FEMALE, null);
+		final User user = new User(11l, "first", "last", 20, Gender.MALE,
+				new User(12l, "first2", "last2", 40, Gender.FEMALE, null));
+		final User friend1 = new User(13l, "friend1-first", "friend1-last", 1,
+				Gender.MALE, null);
+		final User friend2 = new User(14l, "friend2-first", "friend2-last", 2,
+				Gender.FEMALE, null);
 
-    user.setPetNames(new ArrayList<String>() {
-      {
-        add("pet1");
-        add("pet2");
-      }
-    });
-    user.setFriends(new ArrayList<User>() {
-      {
-        add(friend1);
-        add(friend2);
-      }
-    });
-    user.setFriendsNameMap(new HashMap<Integer, String>() {
-      {
-        put(13, "friend1-first");
-        put(14, "friend2-frist");
-      }
-    });
-    user.setFriendsMap(new HashMap<String, User>() {
-      {
-        put("friend1-first", friend1);
-        put("friend2-first", friend2);
-      }
-    });
+		user.setPetNames(new ArrayList<String>() {
+			{
+				add("pet1");
+				add("pet2");
+			}
+		});
+		user.setFriends(new ArrayList<User>() {
+			{
+				add(friend1);
+				add(friend2);
+			}
+		});
+		user.setFriendsNameMap(new HashMap<Integer, String>() {
+			{
+				put(13, "friend1-first");
+				put(14, "friend2-frist");
+			}
+		});
+		user.setFriendsMap(new HashMap<String, User>() {
+			{
+				put("friend1-first", friend1);
+				put("friend2-first", friend2);
+			}
+		});
 
-    String jackson = MarshallingWrapper.toJSON(user);
+		String jackson = MarshallingWrapper.toJSON(user);
 
-    call(JacksonTestService.class,
-        new RemoteCallback<String>() {
-          @Override
-          public void callback(String jackson) {
-            assertNotNull("Server failed to parse JSON using Jackson", jackson);
-            assertEquals(user, MarshallingWrapper.fromJSON(jackson, User.class));
-            finishTest();
-          }
-        }).postJackson(jackson);
-  }
+		call(JacksonTestService.class, new RemoteCallback<String>() {
+			@Override
+			public void callback(String jackson) {
+				assertNotNull("Server failed to parse JSON using Jackson",
+						jackson);
+				assertEquals(user,
+						MarshallingWrapper.fromJSON(jackson, User.class));
+				finishTest();
+			}
+		}).postJackson(jackson);
+	}
 
-  @Test
-  public void testJacksonMarshallingOfList() {
-    delayTestFinish(5000);
+	@Test
+	public void testJacksonMarshallingOfList() {
+		delayTestFinish(5000);
 
-    final List<User> users = new ArrayList<User>();
-    users.add(new User(11l, "first", "last", 20, Gender.MALE, null));
-    users.add(new User(12l, "first2", "last2", 40, Gender.MALE, null));
+		final List<User> users = new ArrayList<User>();
+		users.add(new User(11l, "first", "last", 20, Gender.MALE, null));
+		users.add(new User(12l, "first2", "last2", 40, Gender.MALE, null));
 
-    String jackson = MarshallingWrapper.toJSON(users);
+		String jackson = MarshallingWrapper.toJSON(users);
 
-    call(JacksonTestService.class,
-        new RemoteCallback<String>() {
-          @Override
-          public void callback(String jackson) {
-            assertNotNull("Server failed to parse JSON using Jackson", jackson);
-            assertEquals(users, MarshallingWrapper.fromJSON(jackson, List.class, User.class));
-            finishTest();
-          }
-        }).postJacksonList(jackson);
-  }
+		call(JacksonTestService.class, new RemoteCallback<String>() {
+			@Override
+			public void callback(String jackson) {
+				assertNotNull("Server failed to parse JSON using Jackson",
+						jackson);
+				assertEquals(users, MarshallingWrapper.fromJSON(jackson,
+						List.class, User.class));
+				finishTest();
+			}
+		}).postJacksonList(jackson);
+	}
 
-  @Test
-  public void testJacksonMarshallingOfCollection() {
-    delayTestFinish(5000);
+	@Test
+	public void testJacksonMarshallingOfCollection() {
+		delayTestFinish(5000);
 
-    final List<User> users = new ArrayList<User>();
-    users.add(new User(11l, "first", "last", 20, Gender.MALE, null));
-    users.add(new User(12l, "first2", "last2", 40, Gender.MALE, null));
+		final List<User> users = new ArrayList<User>();
+		users.add(new User(11l, "first", "last", 20, Gender.MALE, null));
+		users.add(new User(12l, "first2", "last2", 40, Gender.MALE, null));
 
-    String jackson = MarshallingWrapper.toJSON(users);
+		String jackson = MarshallingWrapper.toJSON(users);
 
-    call(JacksonTestService.class,
-        new RemoteCallback<String>() {
-          @Override
-          public void callback(String jackson) {
-            assertNotNull("Server failed to parse JSON using Jackson", jackson);
-            assertEquals(users, MarshallingWrapper.fromJSON(jackson, Collection.class, User.class));
-            finishTest();
-          }
-        }).postJacksonList(jackson);
-  }
+		call(JacksonTestService.class, new RemoteCallback<String>() {
+			@Override
+			public void callback(String jackson) {
+				assertNotNull("Server failed to parse JSON using Jackson",
+						jackson);
+				assertEquals(users, MarshallingWrapper.fromJSON(jackson,
+						Collection.class, User.class));
+				finishTest();
+			}
+		}).postJacksonList(jackson);
+	}
 
-  @Test
-  public void testJacksonMarshallingOfListOfBytes() {
-    delayTestFinish(5000);
+	@Test
+	public void testJacksonMarshallingOfListOfBytes() {
+		delayTestFinish(5000);
 
-    final List<Byte> bytes = new ArrayList<Byte>();
-    bytes.add(new Byte("10"));
-    bytes.add(new Byte("4"));
+		final List<Byte> bytes = new ArrayList<Byte>();
+		bytes.add(new Byte("10"));
+		bytes.add(new Byte("4"));
 
-    String jackson = MarshallingWrapper.toJSON(bytes);
+		String jackson = MarshallingWrapper.toJSON(bytes);
 
-    call(JacksonTestService.class,
-        new RemoteCallback<String>() {
-          @Override
-          public void callback(String jackson) {
-            assertNotNull("Server failed to parse JSON using Jackson", jackson);
-            assertEquals(bytes, MarshallingWrapper.fromJSON(jackson, List.class, Byte.class));
-            finishTest();
-          }
-        }).postJacksonListOfBytes(jackson);
-  }
+		call(JacksonTestService.class, new RemoteCallback<String>() {
+			@Override
+			public void callback(String jackson) {
+				assertNotNull("Server failed to parse JSON using Jackson",
+						jackson);
+				assertEquals(bytes, MarshallingWrapper.fromJSON(jackson,
+						List.class, Byte.class));
+				finishTest();
+			}
+		}).postJacksonListOfBytes(jackson);
+	}
 
-  @Test
-  public void testJacksonMarshallingOfPortableWithByteArray() {
-    delayTestFinish(5000);
+	@Test
+	public void testJacksonMarshallingOfPortableWithByteArray() {
+		delayTestFinish(5000);
 
-    final ByteArrayTestWrapper entity = new ByteArrayTestWrapper();
-    String jackson = MarshallingWrapper.toJSON(entity);
+		final ByteArrayTestWrapper entity = new ByteArrayTestWrapper();
+		String jackson = MarshallingWrapper.toJSON(entity);
 
-    call(JacksonTestService.class,
-        new RemoteCallback<String>() {
-          @Override
-          public void callback(String jackson) {
-            assertNotNull("Server failed to parse JSON using Jackson", jackson);
-            assertEquals(entity, MarshallingWrapper.fromJSON(jackson, ByteArrayTestWrapper.class));
-            finishTest();
-          }
-        }).postJacksonPortableWithByteArray(jackson);
-  }
+		call(JacksonTestService.class, new RemoteCallback<String>() {
+			@Override
+			public void callback(String jackson) {
+				assertNotNull("Server failed to parse JSON using Jackson",
+						jackson);
+				assertEquals(entity, MarshallingWrapper.fromJSON(jackson,
+						ByteArrayTestWrapper.class));
+				finishTest();
+			}
+		}).postJacksonPortableWithByteArray(jackson);
+	}
 
-  @Test
-  public void testJacksonMarshallingOfMap() {
-    delayTestFinish(5000);
+	@Test
+	public void testJacksonMarshallingOfMap() {
+		delayTestFinish(5000);
 
-    final Map<String, User> users = new HashMap<String, User>();
-    users.put("1", new User(11l, "first", "last", 20, Gender.MALE, null));
-    users.put("2", new User(12l, "first2", "last2", 40, Gender.MALE, null));
+		final Map<String, User> users = new HashMap<String, User>();
+		users.put("1", new User(11l, "first", "last", 20, Gender.MALE, null));
+		users.put("2", new User(12l, "first2", "last2", 40, Gender.MALE, null));
 
-    String jackson = MarshallingWrapper.toJSON(users);
+		String jackson = MarshallingWrapper.toJSON(users);
 
-    call(JacksonTestService.class,
-        new RemoteCallback<String>() {
-          @SuppressWarnings("unchecked")
-          @Override
-          public void callback(String jackson) {
-            assertNotNull("Server failed to parse JSON using Jackson", jackson);
-            Map<String, User> result = MarshallingWrapper.fromJSON(jackson, Map.class, String.class, User.class);
-            assertEquals(users, result);
-            finishTest();
-          }
-        }).postJacksonMap(jackson);
-  }
+		call(JacksonTestService.class, new RemoteCallback<String>() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public void callback(String jackson) {
+				assertNotNull("Server failed to parse JSON using Jackson",
+						jackson);
+				Map<String, User> result = MarshallingWrapper.fromJSON(jackson,
+						Map.class, String.class, User.class);
+				assertEquals(users, result);
+				finishTest();
+			}
+		}).postJacksonMap(jackson);
+	}
 
-  /**
-   * This test ensures the assumed element type is correctly inferred when generating marshallers for types that use @MapsTo
-   * on List<T> parameters. See https://issues.jboss.org/browse/ERRAI-436.
-   */
-  @Test
-  public void testJacksonDemarshallingOfImmutableTypeWithList() {
-    final ImmutableEntity ftd = new ImmutableEntity.Builder().build();
-    String jackson = "{\"entities\":[{\"id\":1, \"name\":\"name1\"}, {\"id\":2, \"name\":\"name2\"}]}";
-    assertEquals(ftd, MarshallingWrapper.fromJSON(jackson, ImmutableEntity.class));
-  }
+	/**
+	 * This test ensures the assumed element type is correctly inferred when
+	 * generating marshallers for types that use @MapsTo on List<T> parameters.
+	 * See https://issues.jboss.org/browse/ERRAI-436.
+	 */
+	@Test
+	public void testJacksonDemarshallingOfImmutableTypeWithList() {
+		final ImmutableEntity ftd = new ImmutableEntity.Builder().build();
+		String jackson = "{\"entities\":[{\"id\":1, \"name\":\"name1\"}, {\"id\":2, \"name\":\"name2\"}]}";
+		assertEquals(ftd,
+				MarshallingWrapper.fromJSON(jackson, ImmutableEntity.class));
+	}
 
+	@Test
+	public void testJacksonDemarshallingOfObject() {
+		final ImmutableEntity ftd = new ImmutableEntity.Builder().build();
+		final String DOC = "{\n" + "\"key1\" : null,\n" + "\"key2\" : true,\n"
+				+ "\"key3\" : false,\n" + "\"key4\" : [ false, null, true ]\n"
+				+ "}";
+		Map<String, Object> result = MarshallingWrapper.fromJSON(DOC,
+				Map.class, String.class, Object.class);
+		assertEquals(result.size(), 4);
+		assertNull(result.get("key1"));
+		assertTrue((Boolean) result.get("key2"));
+		assertFalse((Boolean) result.get("key3"));
+		List<Object> list = (List<Object>) result.get("key4");
+		assertNotNull(list);
+		assertEquals(list.size(), 3);
+		assertFalse((Boolean) list.get(0));
+		assertNull(list.get(1));
+		assertTrue((Boolean) list.get(2));
+		
+		System.out.println("WOOOOOOOOOOOOOO!");
+	}
 }
