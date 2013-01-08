edbdb5069d45b1d59560eb525e1dcefb107eafd3
Eric Wittmann <eric.wittmann@redhat.com>
2013-01-08 12:47:14 -0500

    Added a unit test for relative vs. absolute paths to the template HTML files for @Templated widgets.

---
+++ errai-ui/src/test/java/org/jboss/errai/ui/test/path/client/PathTemplateTestApp.java
@@ -0,0 +1,37 @@
+package org.jboss.errai.ui.test.path.client;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.jboss.errai.ioc.client.api.EntryPoint;
+import org.jboss.errai.ui.test.path.client.res.PathAbsoluteComponent;
+import org.jboss.errai.ui.test.path.client.res.PathRelativeComponent;
+
+import com.google.gwt.user.client.ui.RootPanel;
+
+@EntryPoint
+public class PathTemplateTestApp {
+
+  @Inject
+  private RootPanel root;
+
+  @Inject
+  private PathRelativeComponent relativeComponent;
+  @Inject
+  private PathAbsoluteComponent absoluteComponent;
+
+  @PostConstruct
+  public void setup() {
+    root.add(relativeComponent);
+    root.add(absoluteComponent);
+    System.out.println(root.getElement().getInnerHTML());
+  }
+
+  public PathRelativeComponent getRelativeComponent() {
+    return relativeComponent;
+  }
+
+  public PathAbsoluteComponent getAbsoluteComponent() {
+    return absoluteComponent;
+  }
+}
