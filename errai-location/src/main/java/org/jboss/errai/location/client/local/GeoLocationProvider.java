e9430b4aeed24b1ac1d3814a792cdcbab26438df
edewit <erikjan.dewit@gmail.com>
2013-01-15 17:05:37 +0100

    initial version of geo location cdi event provider

---
+++ errai-location/src/main/java/org/jboss/errai/location/client/local/GeoLocationProvider.java
@@ -0,0 +1,46 @@
+package org.jboss.errai.location.client.local;
+
+import com.google.gwt.core.client.Callback;
+import com.google.gwt.geolocation.client.Geolocation;
+import com.google.gwt.geolocation.client.Position;
+import com.google.gwt.geolocation.client.PositionError;
+import org.jboss.errai.location.client.shared.GeoLocationEvent;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.event.Event;
+import javax.inject.Singleton;
+
+import static com.google.gwt.geolocation.client.Position.Coordinates;
+
+/**
+ * @author edewit
+ */
+@Singleton
+public class GeoLocationProvider {
+
+  protected Event<GeoLocationEvent> geoLocationEventSource;
+
+  @PostConstruct
+  public void init() {
+    Geolocation geolocation = Geolocation.getIfSupported();
+    if (geolocation != null) {
+      geolocation.watchPosition(new Callback<Position, PositionError>() {
+        @Override
+        public void onFailure(PositionError reason) {
+          //TODO handle error
+        }
+
+        @Override
+        public void onSuccess(Position result) {
+          fire(result);
+        }
+      });
+    }
+  }
+
+  private void fire(Position result) {
+    Coordinates c = result.getCoordinates();
+    GeoLocationEvent geoLocationEvent = new GeoLocationEvent(c.getLatitude(), c.getLongitude(), c.getAccuracy());
+    geoLocationEventSource.fire(geoLocationEvent);
+  }
+}
---
+++ errai-location/src/main/java/org/jboss/errai/location/client/shared/GeoLocationEvent.java
@@ -0,0 +1,43 @@
+package org.jboss.errai.location.client.shared;
+
+import org.jboss.errai.common.client.api.annotations.Portable;
+import org.jboss.errai.marshalling.client.api.annotations.MapsTo;
+
+/**
+ * @author edewit
+ */
+@Portable
+public class GeoLocationEvent {
+  private double latitude;
+  private double longitude;
+  private double accuracy;
+
+  public GeoLocationEvent(
+          @MapsTo("latitude") double latitude,
+          @MapsTo("longitude") double longitude,
+          @MapsTo("accuracy") double accuracy) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.accuracy = accuracy;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public double getAccuracy() {
+    return accuracy;
+  }
+
+  @Override
+  public String toString() {
+    return "GeoLocationEvent [" +
+            "latitude=" + latitude +
+            ", longitude=" + longitude +
+            ']';
+  }
+}
