2cdcc843da160464c68a3a4e7b14e8d0675fb5ec
Jonathan Fuerth <jfuerth@gmail.com>
2013-02-14 12:19:11 -0500

    New test for relative template paths that use "../"

--- errai-ui/src/test/java/org/jboss/errai/ui/test/path/client/PathTemplateTest.java
+++ errai-ui/src/test/java/org/jboss/errai/ui/test/path/client/PathTemplateTest.java
@@ -32,6 +32,23 @@ public class PathTemplateTest extends AbstractErraiCDITest {
   }
 
   @Test
+  public void testRelativeParentPathTemplate() {
+    PathTemplateTestApp app = IOC.getBeanManager().lookupBean(PathTemplateTestApp.class).getInstance();
+    assertNotNull(app.getRelativeParentComponent());
+    assertTrue(app.getRelativeParentComponent().getElement().getInnerHTML().contains("<h1>This will be rendered</h1>"));
+    assertTrue(app.getRelativeParentComponent().getElement().getInnerHTML().contains("<div>This will be rendered</div>"));
+    assertTrue(app.getRelativeParentComponent().getElement().getInnerHTML().contains("This will be rendered inside button"));
+
+    Element c1 = Document.get().getElementById("c1");
+    assertNotNull(c1);
+    assertEquals("Added by component", c1.getInnerText());
+
+    assertNotNull(Document.get().getElementById("c2"));
+    assertNotNull(Document.get().getElementById("c3"));
+    assertNull(Document.get().getElementById("content"));
+  }
+
+  @Test
   public void testAbsolutePathTemplate() {
     PathTemplateTestApp app = IOC.getBeanManager().lookupBean(PathTemplateTestApp.class).getInstance();
     assertNotNull(app.getAbsoluteComponent());
