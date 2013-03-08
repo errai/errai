c603953fc87ad90d94c49d06dc3124581919b30a
edewit <erikjan.dewit@gmail.com>
2013-03-08 16:38:50 -0500

    added divert binding with dummy implementation for non cordova environments.

---
+++ errai-cordova/src/main/java/org/jboss/errai/ui/cordova/geofencing/GeoFencingStandardProvider.java
@@ -0,0 +1,77 @@
+package org.jboss.errai.ui.cordova.geofencing;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+import org.jboss.errai.ui.cordova.JavascriptInjector;
+
+import javax.enterprise.event.Event;
+
+/**
+ * GeoFencingProvider provides geo fencing events dependent on the Geofencing cordova plugin
+ *
+ * @author edewit@redhat.com
+ * @see <a href="https://github.com/phonegap/phonegap-plugins/tree/master/iOS/Geofencing">Cordova Geofencing Plugin</a>
+ */
+public class GeoFencingStandardProvider implements GeoFencingProvider {
+
+  private Event<GeoFencingEvent> geoFencingEventEvent;
+
+  public GeoFencingStandardProvider() {
+    initRegionListener();
+    TextResource javascript = Resources.RESOURCES.javascript();
+    JavascriptInjector.inject(javascript.getText());
+  }
+
+  private native void initRegionListener() /*-{
+    var instance = this;
+    $doc.addEventListener("region-update", function (event) {
+      var fid = event.regionupdate.fid;
+      console.log("got region update event ['" + fid + "']");
+      $entry(instance.@org.jboss.errai.ui.cordova.geofencing.GeoFencingStandardProvider::fireCdiEvent(I)(fid));
+    });
+  }-*/;
+
+  @Override
+  public void addRegion(Region region) {
+    addRegion(region.getId(), region.getLatitude(), region.getLongitude(), region.getRadius());
+  }
+
+  private native void addRegion(int id, double latitude, double longitude, int radius) /*-{
+    $wnd.DGGeofencing.addRegion(
+            {"fid":id, "radius":radius, "latitude":latitude, "longitude":longitude},
+            function (result) {
+              console.log("region add success");
+            }
+    );
+  }-*/;
+
+  @Override
+  public void removeRegion(Region region) {
+    removeRegion(region.getId(), region.getLatitude(), region.getLongitude());
+  }
+
+  private native void removeRegion(int id, double latitude, double longitude) /*-{
+    $wnd.DGGeofencing.removeRegion(
+            {"fid":id, "latitude":latitude, "longitude":longitude},
+            function () {
+              console.log("region removed")
+            }
+    )
+  }-*/;
+
+  protected void fireCdiEvent(int regionId) {
+    geoFencingEventEvent.fire(new GeoFencingEvent(regionId));
+  }
+
+  public void setEventSource(Event<GeoFencingEvent> eventSource) {
+    this.geoFencingEventEvent = eventSource;
+  }
+
+  public static interface Resources extends ClientBundle {
+    public static Resources RESOURCES = GWT.create(Resources.class);
+
+    @Source("org/jboss/errai/ui/cordova/js/DGGeofencing.js")
+    TextResource javascript();
+  }
+}
---
+++ errai-cordova/src/main/java/org/jboss/errai/ui/cordova/js/DGGeofencing.js
@@ -0,0 +1,87 @@
+/**
+ * DGGeofencing.js
+ *  
+ * Phonegap DGGeofencing Plugin
+ * Copyright (c) Dov Goldberg 2012
+ * http://www.ogonium.com
+ * dov.goldberg@ogonium.com
+ *
+ */
+var exec = cordova.require('cordova/exec');
+var DGGeofencing = {
+	/*
+	Params:
+	#define KEY_REGION_ID       @"fid"
+	#define KEY_REGION_LAT      @"latitude"
+    #define KEY_REGION_LNG      @"longitude"
+    #define KEY_REGION_RADIUS   @"radius"
+    #define KEY_REGION_ACCURACY @"accuracy"
+	*/
+     addRegion: function(params, success, fail) {
+          return exec(success, fail, "DGGeofencing", "addRegion", [params]);
+     },
+
+     /*
+	Params:
+	#define KEY_REGION_ID      @"fid"
+	#define KEY_REGION_LAT     @"latitude"
+    #define KEY_REGION_LNG     @"longitude"
+	*/
+     removeRegion: function(params, success, fail) {
+          return exec(success, fail, "DGGeofencing", "removeRegion", [params]);
+     },
+
+     /*
+	Params:
+	NONE
+	*/
+	getWatchedRegionIds: function(success, fail) {
+		return exec(success, fail, "DGGeofencing", "getWatchedRegionIds", []);
+	},
+	
+	/*
+	Params:
+	NONE
+	*/
+	getPendingRegionUpdates: function(success, fail) {
+		return exec(success, fail, "DGGeofencing", "getPendingRegionUpdates", []);
+	},
+	
+	/*
+	Params:
+	NONE
+	*/
+	startMonitoringSignificantLocationChanges: function(success, fail) {
+		return exec(success, fail, "DGGeofencing", "startMonitoringSignificantLocationChanges", []);
+	},
+	
+	/*
+	Params:
+	NONE
+	*/
+	stopMonitoringSignificantLocationChanges: function(success, fail) {
+		return exec(success, fail, "DGGeofencing", "stopMonitoringSignificantLocationChanges", []);
+	},
+	
+	/* 
+	This is used so the JavaScript can be updated when a region is entered or exited
+	*/
+	regionMonitorUpdate: function(regionupdate) {
+		console.log("regionMonitorUpdate: " + regionupdate);
+		var ev = document.createEvent('HTMLEvents');
+		ev.regionupdate = regionupdate;
+		ev.initEvent('region-update', true, true, arguments);
+		document.dispatchEvent(ev);
+	},
+	
+	/* 
+	This is used so the JavaScript can be updated when a significant change has occured
+	*/
+	locationMonitorUpdate: function(locationupdate) {
+		console.log("locationMonitorUpdate: " + locationupdate);
+		var ev = document.createEvent('HTMLEvents');
+		ev.locationupdate = locationupdate;
+		ev.initEvent('location-update', true, true, arguments);
+		document.dispatchEvent(ev);
+	}
+};
