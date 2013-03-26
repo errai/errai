68be078a76deacdc7c54083166ba236ab89ad95d
Christian Sadilek <christian.sadilek@gmail.com>
2013-03-26 14:36:19 -0400

    Fixed ERRAI-511: ensure demos are deployed, defined all plugin version in pluginmanagement section of parent pom

--- errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
+++ errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
@@ -1,4 +1,4 @@
-//generated using Reflections JavaCodeSerializer [Fri Jan 25 11:04:48 EST 2013]
+//generated using Reflections JavaCodeSerializer [Tue Mar 26 14:13:22 EDT 2013]
 package org.jboss.errai.reflections;
 
 import static org.jboss.errai.reflections.serializers.JavaCodeSerializer.*;
