6ba3559fc228f2b738154756c442d6b3ee152668
edewit <erikjan.dewit@gmail.com>
2013-04-09 16:03:06 +0200

    added project dropdown list

--- errai-cordova/demos/aerogear/src/main/java/org/jboss/errai/example/client/local/TaskForm.java
+++ errai-cordova/demos/aerogear/src/main/java/org/jboss/errai/example/client/local/TaskForm.java
@@ -3,15 +3,20 @@ package org.jboss.errai.example.client.local;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.text.shared.Renderer;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.datepicker.client.DateBox;
 import org.jboss.errai.aerogear.api.pipeline.Pipe;
 import org.jboss.errai.databinding.client.api.DataBinder;
 import org.jboss.errai.databinding.client.api.InitialState;
+import org.jboss.errai.example.client.local.events.ProjectRefreshEvent;
 import org.jboss.errai.example.client.local.events.TaskRefreshEvent;
 import org.jboss.errai.example.client.local.events.TaskUpdateEvent;
+import org.jboss.errai.example.client.local.pipe.ProjectPipe;
+import org.jboss.errai.example.client.local.pipe.TagPipe;
 import org.jboss.errai.example.client.local.pipe.TaskPipe;
 import org.jboss.errai.example.client.local.util.DefaultCallback;
+import org.jboss.errai.example.shared.Project;
 import org.jboss.errai.example.shared.Tag;
 import org.jboss.errai.example.shared.Task;
 import org.jboss.errai.ioc.client.api.AfterInitialization;
@@ -21,6 +26,8 @@ import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.util.List;
 
 import static org.jboss.errai.example.client.local.Animator.hide;
 import static org.jboss.errai.example.client.local.Animator.show;
@@ -43,6 +50,14 @@ public class TaskForm extends Composite {
   private Pipe<Task> taskPipe;
 
   @Inject
+  @ProjectPipe
+  private Pipe<Project> projectPipe;
+
+  @Inject
+  @TagPipe
+  private Pipe<Tag> tagPipe;
+
+  @Inject
   @Bound
   @DataField("task-title")
   private TextBox title;
@@ -56,9 +71,21 @@ public class TaskForm extends Composite {
   @DataField
   private TextArea description;
 
-  @Inject
-  @DataField
-  private ListBox project;
+  @DataField("project")
+  private ValueListBox<Project> projectListBox = new ValueListBox<Project>(new Renderer<Project>() {
+    @Override
+    public String render(Project project) {
+      if (project == null) {
+        return "No Project";
+      }
+      return project.getTitle();
+    }
+
+    @Override
+    public void render(Project project, Appendable appendable) throws IOException {
+      appendable.append(render(project));
+    }
+  });
 
   @Inject
   @DataField
@@ -72,12 +99,28 @@ public class TaskForm extends Composite {
   private void settings() {
     DateTimeFormat dateFormat = DateTimeFormat.getFormat("yyyy-MM-dd");
     date.setFormat(new DateBox.DefaultFormat(dateFormat));
+    refreshProjectList();
+  }
+
+  private void updatedProjectList(@Observes ProjectRefreshEvent event) {
+    refreshProjectList();
+  }
+
+  private void refreshProjectList() {
+    projectPipe.read(new DefaultCallback<List<Project>>() {
+      @Override
+      public void onSuccess(List<Project> result) {
+        result.add(0, null);
+        projectListBox.setAcceptableValues(result);
+      }
+    });
   }
 
   @EventHandler("submit")
   public void onSubmit(ClickEvent event) {
     final Element div = getContainer(event);
     Task task = taskBinder.getModel();
+    task.setProject(projectListBox.getValue());
     task.setDate(date.getTextBox().getValue());
     taskPipe.save(task, new DefaultCallback<Task>() {
       @Override
