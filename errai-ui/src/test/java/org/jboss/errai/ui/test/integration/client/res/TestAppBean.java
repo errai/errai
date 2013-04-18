e07a7d6d6bfb0355ad7bf8453ca49627aba0b52d
Mike Brock <brockm@gmail.com>
2013-04-18 16:06:39 -0400

    add integration test that covers case in ERRAI-539 to Errai UI

---
+++ errai-ui/src/test/java/org/jboss/errai/ui/test/integration/client/res/TestAppBean.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 JBoss, by Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.errai.ui.test.integration.client.res;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import org.jboss.errai.ioc.client.api.EntryPoint;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.Templated;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+/**
+ * @author Mike Brock
+ */
+@EntryPoint
+@Templated("TestTemplate.html")
+public class TestAppBean extends Composite {
+  @Inject @ContentPanel @DataField("content") private Widget w;
+
+  @Produces
+  @ContentPanel
+  public Widget produceContentPanel(final SomeDependency someDependency) {
+    return someDependency.makeWidget();
+  }
+
+  public Widget getW() {
+    return w;
+  }
+}
