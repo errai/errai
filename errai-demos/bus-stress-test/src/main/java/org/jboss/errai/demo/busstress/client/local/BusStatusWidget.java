68d0d1b5bf2354f2bf10c9cc0575a34e1a9ad81b
pslegr <pslegr@redhat.com>
2013-04-23 13:45:28 +0200

    clean up demos

---
+++ errai-demos/bus-stress-test/src/main/java/org/jboss/errai/demo/busstress/client/local/BusStatusWidget.java
@@ -0,0 +1,104 @@
+package org.jboss.errai.demo.busstress.client.local;
+
+import org.jboss.errai.bus.client.ErraiBus;
+import org.jboss.errai.bus.client.api.BusLifecycleEvent;
+import org.jboss.errai.bus.client.api.BusLifecycleListener;
+import org.jboss.errai.bus.client.api.ClientMessageBus;
+import org.jboss.errai.bus.client.framework.ClientMessageBusImpl;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+
+public class BusStatusWidget extends Composite implements BusLifecycleListener {
+
+  interface Resources extends ClientBundle {
+    @Source("white.png")
+    ImageResource white();
+
+    @Source("red.png")
+    ImageResource red();
+
+    @Source("yellow.png")
+    ImageResource yellow();
+
+    @Source("green.png")
+    ImageResource green();
+  }
+
+  private final HorizontalPanel me = new HorizontalPanel();
+  private final Resources resources = GWT.create(Resources.class);
+  private final Image statusImage = new Image(resources.white());
+  private final Label statusLabel = new Label("Unknown");
+  private final Button stopTrueButton = new Button("bus.stop(true)");
+  private final Button stopFalseButton = new Button("bus.stop(false)");
+  private final Button initButton = new Button("bus.init()");
+
+  public BusStatusWidget() {
+    initWidget(me);
+
+    statusLabel.setWordWrap(false);
+    statusLabel.setWidth("6em");
+
+    stopTrueButton.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(ClickEvent event) {
+        ((ClientMessageBusImpl) ErraiBus.get()).stop(true);
+      }
+    });
+
+    stopFalseButton.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(ClickEvent event) {
+        ((ClientMessageBusImpl) ErraiBus.get()).stop(false);
+      }
+    });
+
+    initButton.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(ClickEvent event) {
+        ((ClientMessageBus) ErraiBus.get()).init();
+      }
+    });
+
+    me.add(statusImage);
+    me.add(statusLabel);
+    me.add(stopFalseButton);
+    me.add(stopTrueButton);
+    me.add(initButton);
+  }
+
+  @Override
+  public void busAssociating(BusLifecycleEvent e) {
+    statusLabel.setText("Connecting");
+    statusImage.setResource(resources.yellow());
+  }
+
+  @Override
+  public void busDisassociating(BusLifecycleEvent e) {
+    statusLabel.setText("Local Only");
+    statusImage.setResource(resources.red());
+  }
+
+  @Override
+  public void busOnline(BusLifecycleEvent e) {
+    statusLabel.setText("Connected");
+    statusImage.setResource(resources.green());
+  }
+
+  @Override
+  public void busOffline(BusLifecycleEvent e) {
+    statusLabel.setText("Connecting");
+    statusImage.setResource(resources.yellow());
+  }
+}
---
+++ errai-demos/bus-stress-test/src/test/java/org/jboss/errai/demo/busstress/client/local/ErraiIocTestHelper.java
@@ -0,0 +1,65 @@
+package org.jboss.errai.demo.busstress.client.local;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.jboss.errai.bus.client.api.messaging.MessageBus;
+import org.jboss.errai.common.client.api.extension.InitVotes;
+import org.jboss.errai.ioc.client.api.EntryPoint;
+
+import org.jboss.errai.ioc.client.api.TestOnly;
+
+/**
+ * This class provides a target for injecting parts of the application that the
+ * test cases need access to. Think of it as your test suite's window into the
+ * CDI container. Test cases can access the injected members using the following
+ * code:
+ * <p/>
+ * <pre>
+ *   ErraiIocTestHelper.instance.<i>injectedFieldName</i>
+ * </pre>
+ * <p/>
+ * You can also set up CDI event producers and observers here if your test needs
+ * to fire events or assert that a particular event was fired.
+ * <p/>
+ * Note that this "CDI Test Helper" pattern is just a workaround. If there were
+ * something like the BeanManager available in the client, it would be
+ * preferable for the tests to create and destroy managed beans on demand.
+ * <p/>
+ * As an alternative workaround, you could dispense with this class altogether
+ * and have your main Entry Point class keep a static reference to itself.
+ * However, this would pollute the API with an unwanted singleton pattern: there
+ * would be the possibility of classes referring to the entry point class through
+ * its singleton rather than allowing it to be injected.
+ *
+ * @author Jonathan Fuerth <jfuerth@gmail.com>
+ */
+@EntryPoint @TestOnly
+public class ErraiIocTestHelper {
+
+  static ErraiIocTestHelper instance;
+
+  static boolean busInitialized = false;
+
+  @Inject StressTestClient client;
+  @Inject MessageBus bus;
+
+  @PostConstruct
+  void saveStaticReference() {
+    instance = this;
+  }
+
+  /**
+   * Runs the given runnable in the browser's JavaScript thread once the Errai
+   * bus has finished its initialization phase and the client is connected to
+   * the server. Once the runnable is executed, all {@link EntryPoint} classes
+   * will have been created and have their dependencies injected, and all
+   * components listening for it will have received the BusReady event.
+   *
+   * @param runnable The code to run once Errai CDI is up and running in the context of
+   *                 the web page.
+   */
+  public static void afterBusInitialized(final Runnable runnable) {
+    InitVotes.registerOneTimeInitCallback(runnable);
+  }
+}
