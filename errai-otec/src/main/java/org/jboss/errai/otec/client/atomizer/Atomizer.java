de570a6e477da91eb7a7d953b8af475fad70aaa4
Mike Brock <brockm@gmail.com>
2013-05-01 00:39:32 -0400

    atomizer mostly working!

--- errai-otec/src/main/java/org/jboss/errai/otec/client/atomizer/Atomizer.java
+++ errai-otec/src/main/java/org/jboss/errai/otec/client/atomizer/Atomizer.java
@@ -22,13 +22,18 @@ import com.google.common.collect.Multimap;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyEvent;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.EventListener;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.ValueBoxBase;
 import org.jboss.errai.otec.client.OTEngine;
 import org.jboss.errai.otec.client.OTEntity;
+import org.jboss.errai.otec.client.util.DiffUtil;
 
 /**
  * @author Mike Brock
@@ -49,9 +54,19 @@ public abstract class Atomizer {
     HANDLER_REGISTRATION_MAP.put(widget, widget.addKeyDownHandler(new KeyDownHandler() {
       @Override
       public void onKeyDown(final KeyDownEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_BACKSPACE) {
+        if (shouldIgnoreKeyPress(event)) {
+          return;
+        }
+
+        if (widget.getSelectedText().length() > 0) {
+          entityChangeStream.notifyDelete(widget.getCursorPos(), widget.getSelectedText());
+        }
+        else if (event.getNativeKeyCode() == KeyCodes.KEY_BACKSPACE) {
           final int index = widget.getCursorPos() - 1;
           entityChangeStream.notifyDelete(index, String.valueOf(widget.getText().charAt(index)));
+        }
+        else if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+          entityChangeStream.notifyInsert(widget.getCursorPos(), "\n");
         }
       }
     }));
@@ -59,12 +74,36 @@ public abstract class Atomizer {
     HANDLER_REGISTRATION_MAP.put(widget, widget.addKeyPressHandler(new KeyPressHandler() {
       @Override
       public void onKeyPress(final KeyPressEvent event) {
+        if (shouldIgnoreKeyPress(event)) {
+          return;
+        }
+
         if (event.getUnicodeCharCode() != 0) {
-          final char charCode = event.getCharCode();
-          entityChangeStream.notifyInsert(widget.getCursorPos(), String.valueOf(charCode));
+          entityChangeStream.notifyInsert(widget.getCursorPos(), String.valueOf(event.getCharCode()));
         }
       }
     }));
+
+    DOM.setEventListener(widget.getElement(), new EventListener() {
+      @Override
+      public void onBrowserEvent(Event event) {
+        if (event.getTypeInt() == Event.ONPASTE) {
+          final String before = (String) entity.getState().get();
+          new Timer() {
+            @Override
+            public void run() {
+              final String after = (String) widget.getValue();
+              final DiffUtil.Delta diff = DiffUtil.diff(before, after);
+
+              entityChangeStream.notifyInsert(diff.getCursor(), diff.getDeltaText());
+            }
+          }.schedule(1);
+        }
+        widget.onBrowserEvent(event);
+      }
+    });
+
+    DOM.sinkEvents(widget.getElement(), DOM.getEventsSunk(widget.getElement()) | Event.ONPASTE);
 
     new Timer() {
       @Override
@@ -74,7 +113,37 @@ public abstract class Atomizer {
     }.scheduleRepeating(750);
   }
 
-//  private static boolean isControlCharacter(final int charCode) {
-//    return charCode < 0x20 || (charCode >= 0x7F && charCode < 0xA0);
-//  }
+  private static boolean shouldIgnoreKeyPress(KeyEvent event) {
+    if (event.isMetaKeyDown() || event.isControlKeyDown()) {
+      return true;
+    }
+
+    int keyCode;
+    if (event instanceof KeyDownEvent) {
+      keyCode = ((KeyDownEvent) event).getNativeKeyCode();
+    }
+    else if (event instanceof KeyPressEvent) {
+      keyCode = ((KeyPressEvent) event).getUnicodeCharCode();
+      return keyCode == 0;
+    }
+    else {
+      return true;
+    }
+
+    switch (keyCode) {
+      case KeyCodes.KEY_DOWN:
+      case KeyCodes.KEY_LEFT:
+      case KeyCodes.KEY_UP:
+      case KeyCodes.KEY_RIGHT:
+      case KeyCodes.KEY_ESCAPE:
+      case KeyCodes.KEY_PAGEDOWN:
+      case KeyCodes.KEY_PAGEUP:
+      case KeyCodes.KEY_HOME:
+      case KeyCodes.KEY_END:
+        return true;
+    }
+
+    return false;
+  }
+
 }
