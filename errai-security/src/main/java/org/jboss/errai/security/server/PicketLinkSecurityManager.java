ab2c7713943c1c1b9787e2470a2636be4ee5d139
edewit <erikjan.dewit@gmail.com>
2013-05-14 09:27:00 +0200

    fixed null pointer

--- errai-security/src/main/java/org/jboss/errai/security/server/PicketLinkSecurityManager.java
+++ errai-security/src/main/java/org/jboss/errai/security/server/PicketLinkSecurityManager.java
@@ -3,14 +3,13 @@ package org.jboss.errai.security.server;
 import org.jboss.errai.bus.server.annotations.Service;
 import org.jboss.errai.security.shared.LoggedInEvent;
 import org.jboss.errai.security.shared.SecurityManager;
+import org.jboss.errai.security.shared.User;
 import org.picketlink.Identity;
 import org.picketlink.credential.DefaultLoginCredentials;
 import org.picketlink.idm.credential.Password;
-import org.jboss.errai.security.shared.User;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
-import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
 
 /**
@@ -66,6 +65,9 @@ public class PicketLinkSecurityManager implements SecurityManager {
 
   @Override
   public User getUser() {
-    return createUser(identity.getUser());
+    if (identity.isLoggedIn()) {
+      return createUser(identity.getUser());
+    }
+    return null;
   }
 }
