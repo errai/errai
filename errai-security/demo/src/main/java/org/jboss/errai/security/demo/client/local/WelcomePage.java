c554c386df292b305fec2a2448662154d37fd5a9
edewit <erikjan.dewit@gmail.com>
2013-05-14 09:27:25 +0200

    added logout event

--- errai-security/demo/src/main/java/org/jboss/errai/security/demo/client/local/WelcomePage.java
+++ errai-security/demo/src/main/java/org/jboss/errai/security/demo/client/local/WelcomePage.java
@@ -5,12 +5,14 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import org.jboss.errai.security.shared.LoggedInEvent;
+import org.jboss.errai.security.shared.LoggedOutEvent;
 import org.jboss.errai.ui.nav.client.local.Page;
 import org.jboss.errai.ui.nav.client.local.TransitionTo;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -19,6 +21,8 @@ import javax.inject.Inject;
 @Templated("#root")
 @Page(startingPage = true)
 public class WelcomePage extends Composite {
+
+  private static final String ANONYMOUS = "anonymous";
 
   @Inject
   public
@@ -37,7 +41,16 @@ public class WelcomePage extends Composite {
     startButtonClicked.go();
   }
 
+  @PostConstruct
+  private void setupUserLabel() {
+    userLabel.setText(ANONYMOUS);
+  }
+
   private void onLoggedIn(@Observes LoggedInEvent loggedInEvent) {
     userLabel.setText(loggedInEvent.getUser().getFullName());
+  }
+
+  private void onLoggedOut(@Observes LoggedOutEvent loggedOutEvent) {
+    userLabel.setText(ANONYMOUS);
   }
 }
