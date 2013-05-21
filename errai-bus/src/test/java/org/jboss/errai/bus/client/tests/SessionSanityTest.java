d80c24e78b2e3a891c4d1150a3903d7a24ae66fb
Jonathan Fuerth <jfuerth@gmail.com>
2013-05-21 18:55:47 -0400

    Resolved ERRAI-553: sessions are not serializable

---
+++ errai-bus/src/test/java/org/jboss/errai/bus/client/tests/SessionSanityTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2010 JBoss, a divison Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.errai.bus.client.tests;
+
+import org.jboss.errai.bus.client.api.Message;
+import org.jboss.errai.bus.client.api.MessageCallback;
+import org.jboss.errai.bus.client.api.base.MessageBuilder;
+
+/**
+ * Tests that HttpSessions involved with ErraiBus communication are still well-behaved.
+ * 
+ * @author Jonathan Fuerth <jfuerth@redhat.com>
+ * @author Mike Brock <cbrock@redhat.com>
+ * @author Christian Sadilek <csadilek@redhat.com>
+ */
+public class SessionSanityTest extends AbstractErraiTest {
+
+  @Override
+  public String getModuleName() {
+    return "org.jboss.errai.bus.ErraiBusTests";
+  }
+
+  @Override
+  protected void gwtSetUp() throws Exception {
+    super.gwtSetUp();
+  }
+
+  public void testBasicRoundTrip() {
+      MessageBuilder.createMessage()
+              .toSubject("SessionPassivationService")
+              .done()
+              .repliesTo(new MessageCallback() {
+                @Override
+                public void callback(Message message) {
+                  System.out.println("Got response from server");
+                  finishTest();
+                }
+              })
+              .sendNowWith(bus);
+      delayTestFinish(20000);
+  }
+}
