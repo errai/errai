d0443e02d216320d22dad657f0db9608ae48c861
edewit <erikjan.dewit@gmail.com>
2013-06-07 13:30:29 +0200

    removed the 'ugly' dropdown and made something a bit nicer looking

--- errai-jpa/demos/errai-jpa-demo-grocery-list/src/main/java/org/jboss/errai/demo/grocery/client/local/NavBar.java
+++ errai-jpa/demos/errai-jpa-demo-grocery-list/src/main/java/org/jboss/errai/demo/grocery/client/local/NavBar.java
@@ -3,13 +3,16 @@ package org.jboss.errai.demo.grocery.client.local;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
-import org.jboss.errai.ui.client.widget.LocaleListBox;
+import org.jboss.errai.ioc.client.api.AfterInitialization;
+import org.jboss.errai.ui.client.widget.ListWidget;
+import org.jboss.errai.ui.client.widget.LocaleSelector;
+import org.jboss.errai.ui.client.widget.OrderedList;
 import org.jboss.errai.ui.nav.client.local.TransitionTo;
-import org.jboss.errai.ui.shared.api.annotations.DataField;
-import org.jboss.errai.ui.shared.api.annotations.EventHandler;
-import org.jboss.errai.ui.shared.api.annotations.Templated;
+import org.jboss.errai.ui.shared.api.Locale;
+import org.jboss.errai.ui.shared.api.annotations.*;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 
 @Templated
 public class NavBar extends Composite {
@@ -17,11 +20,24 @@ public class NavBar extends Composite {
   @Inject @DataField Anchor home;
   @Inject @DataField Anchor items;
   @Inject @DataField Anchor stores;
-  @Inject @DataField LocaleListBox language;
+
+  /**
+   * Could have used the {@link org.jboss.errai.ui.client.widget.LocaleListBox} here instead, but
+   * you can also create your own customised component with the LocaleSelector
+   */
+  @Inject
+  private LocaleSelector selector;
+  @Inject @DataField @OrderedList
+  ListWidget<Locale, LanguageItem> language;
 
   @Inject TransitionTo<WelcomePage> homeTab;
   @Inject TransitionTo<StoresPage> storesTab;
   @Inject TransitionTo<ItemListPage> itemsTab;
+
+  @AfterInitialization
+  public void buildLangaugeList() {
+    language.setItems(new ArrayList<Locale>(selector.getSupportedLocales()));
+  }
 
   @EventHandler("home")
   public void onHomeButtonClick(ClickEvent e) {
@@ -37,4 +53,5 @@ public class NavBar extends Composite {
   public void onStoresButtonClick(ClickEvent e) {
     storesTab.go();
   }
+
 }
