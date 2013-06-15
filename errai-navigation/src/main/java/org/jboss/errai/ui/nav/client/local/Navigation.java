1eaa4fb39e5ec071d36b232bc4067079db574fb6
Johannes Barop <jb@barop.de>
2013-06-15 08:44:38 +0200

    Support for navigation in lifecycle methods.
    Fixes ERRAI-516

--- errai-navigation/src/main/java/org/jboss/errai/ui/nav/client/local/Navigation.java
+++ errai-navigation/src/main/java/org/jboss/errai/ui/nav/client/local/Navigation.java
@@ -8,13 +8,17 @@ import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.web.bindery.event.shared.HandlerRegistration;
 import org.jboss.errai.ioc.client.container.async.CreationalCallback;
 import org.jboss.errai.ui.nav.client.local.spi.NavigationGraph;
 import org.jboss.errai.ui.nav.client.local.spi.PageNode;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Central control point for navigating between pages of the application.
@@ -27,17 +31,65 @@ import java.util.Collection;
 @ApplicationScoped
 public class Navigation {
 
+  /**
+   * Maximum number of successive redirects until Errai suspects an endless loop.
+   */
+  final static int MAXIMUM_REDIRECTS = 99;
+
+  /**
+   * Encapsulates a navigation request to another page.
+   */
+  private static class Request<W extends Widget> {
+
+    PageNode<W> pageNode;
+
+    HistoryToken state;
+
+    /**
+     * Construct a new {@link Request}.
+     *
+     * @param pageNode
+     *          The page node to display. Normally, the implementation of PageNode
+     *          is generated at compile time based on a Widget subclass that has
+     *          been annotated with {@code @Page}. Anything calling this method
+     *          must ensure that the given PageNode has been entered into the
+     *          navigation graph, or later navigation back to {@code toPage} will
+     *          fail.
+     * @param state
+     *          The state information to pass to the page node before showing it.
+     */
+    private Request(PageNode<W> pageNode, HistoryToken state) {
+      this.pageNode = pageNode;
+      this.state = state;
+    }
+
+  }
+
   private final SimplePanel contentPanel = new SimplePanel();
 
   private final NavigationGraph navGraph = GWT.create(NavigationGraph.class);
 
   protected PageNode<Widget> currentPage;
 
+  private HandlerRegistration historyHandlerRegistration;
+
+  /**
+   * Indicates that a navigation request is currently processed.
+   */
+  private boolean locked = false;
+
+  /**
+   * Queued navigation requests which could not handled immediately.
+   */
+  private Queue<Request> queuedRequests = new LinkedList<Request>();
+
+  private int redirectDepth = 0;
+
   @PostConstruct
   private void init() {
     if (navGraph.isEmpty()) return;
 
-    History.addValueChangeHandler(new ValueChangeHandler<String>() {
+    historyHandlerRegistration = History.addValueChangeHandler(new ValueChangeHandler<String>() {
       @Override
       public void onValueChange(ValueChangeEvent<String> event) {
 
@@ -47,13 +99,21 @@ public class Navigation {
           GWT.log("Got invalid page name \"" + token.getPageName() + "\" in URL history token. Falling back to default page.");
           toPage = navGraph.getPage(""); // guaranteed at compile time to exist
         }
-        show(toPage, token);
+        navigate(new Request<Widget>(toPage, token));
 
       }
     });
 
     // finally, we bootstrap the navigation system (this invokes the callback above)
     History.fireCurrentHistoryState();
+  }
+
+  /**
+   * Package private for testability.
+   */
+  @PreDestroy
+  void cleanUp() {
+    historyHandlerRegistration.removeHandler();
   }
 
   /**
@@ -113,54 +173,78 @@ public class Navigation {
 
   private <W extends Widget> void navigate(PageNode<W> toPageInstance, Multimap<String, String> state) {
     HistoryToken token = HistoryToken.of(toPageInstance.name(), state);
-    show(toPageInstance, token);
-    History.newItem(token.toString(), false);
-  }
-
-  /**
-   * Return the current page that is being displayed.
-   * @return the current page
-   */
-  public PageNode<Widget> getCurrentPage() {
-    return currentPage;
+    navigate(new Request<W>(toPageInstance, token));
   }
 
   /**
    * Captures a backup of the current page state in history, sets the state on
    * the given PageNode from the given state token, then makes its widget
    * visible in the content area.
-   *
-   * @param toPage
-   *          The page node to display. Normally, the implementation of PageNode
-   *          is generated at compile time based on a Widget subclass that has
-   *          been annotated with {@code @Page}. Anything calling this method
-   *          must ensure that the given PageNode has been entered into the
-   *          navigation graph, or later navigation back to {@code toPage} will
-   *          fail.
-   * @param state
-   *          The state information to pass to the page node before showing it.
    */
-  private <W extends Widget> void show(final PageNode<W> toPage, final HistoryToken state) {
-	Widget currentWidget = null;
+  private <W extends Widget> void navigate(Request<W> request) {
+    if (locked) {
+      queuedRequests.add(request);
+      return;
+    }
 
-    if (currentPage != null) {
-      currentWidget = contentPanel.getWidget();
-      if (currentWidget == null) {
-        // this could happen if someone was manipulating the DOM behind our backs
-        GWT.log("Current widget vanished from navigation content panel. " +
-                "Not delivering pageHiding event to " + currentPage + ".");
-      }
-      else {
-        currentPage.pageHiding(currentWidget);
+    redirectDepth++;
+    if (redirectDepth >= MAXIMUM_REDIRECTS) {
+      throw new RuntimeException(
+              "Maximum redirect limit of " + MAXIMUM_REDIRECTS + " reached. " +
+                      "Do you have a redirect loop?");
+    }
+
+    locked = true;
+    try {
+      hideCurrentPage();
+      showPage(request.pageNode, request.state);
+    } finally {
+      locked = false;
+    }
+
+    if (queuedRequests.isEmpty()) {
+      // No new navigation requests were recorded in the lifecycle methods.
+      // This is the page which has to be displayed and the browser's history can be updated.
+      redirectDepth = 0;
+      History.newItem(request.state.toString(), false);
+    } else {
+      // Process all navigation requests captured in the lifecycle methods.
+      while (queuedRequests.size() != 0) {
+        navigate(queuedRequests.poll());
       }
     }
 
+  }
+
+  /**
+   * Hide the page currently displayed and call the associated lifecycle methods.
+   */
+  private void hideCurrentPage() {
+    Widget currentWidget = contentPanel.getWidget();
+
+    // Note: Optimized out in production mode
+    if (currentPage != null && currentWidget == null) {
+      // This could happen if someone was manipulating the DOM behind our backs
+      GWT.log("Current widget vanished from navigation content panel. " +
+              "Not delivering pageHiding event to " + currentPage + ".");
+    }
+
+    if (currentPage != null && currentWidget != null) {
+      currentPage.pageHiding(currentWidget);
+    }
+
+    // Ensure clean contentPanel regardless of currentPage being null
     contentPanel.clear();
 
     if (currentPage != null && currentWidget != null) {
-    	currentPage.pageHidden(currentWidget);
+      currentPage.pageHidden(currentWidget);
     }
+  }
 
+  /**
+   * Show the given page and call the associated lifecycle methods.
+   */
+  private <W extends Widget> void showPage(final PageNode<W> toPage, final HistoryToken state) {
     toPage.produceContent(new CreationalCallback<W>() {
       @Override
       public void callback(W widget) {
@@ -174,6 +258,14 @@ public class Navigation {
         toPage.pageShown(widget, state);
       }
     });
+  }
+
+  /**
+   * Return the current page that is being displayed.
+   * @return the current page
+   */
+  public PageNode<Widget> getCurrentPage() {
+    return currentPage;
   }
 
   /**
