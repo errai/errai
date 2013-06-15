2b2d8533f2da0d339fbb74aa4abbec9370dc31c9
Johannes Barop <jb@barop.de>
2013-06-15 08:44:38 +0200

    Support for navigation in lifecycle methods.
    Fixes ERRAI-516

---
+++ errai-navigation/src/test/java/org/jboss/errai/ui/nav/client/local/testpages/PageWithException.java
@@ -0,0 +1,23 @@
+package org.jboss.errai.ui.nav.client.local.testpages;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.PageShowing;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * Simple test page which throws an exception in {@link PageShowing}.
+ *
+ * @author Johannes Barop <jb@barop.de>
+ */
+@ApplicationScoped
+@Page
+public class PageWithException extends VerticalPanel {
+
+  @PageShowing
+  protected void beforeShow() {
+    throw new NullPointerException("raped");
+  }
+
+}
