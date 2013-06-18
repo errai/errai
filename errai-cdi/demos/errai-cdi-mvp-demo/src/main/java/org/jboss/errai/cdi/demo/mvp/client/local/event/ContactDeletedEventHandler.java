08aae1b289fbb9e1c81cabc5149fe5eeb3a21fed
Christian Sadilek <christian.sadilek@gmail.com>
2013-06-18 13:24:20 -0400

    Removing demos from errai-cdi dir (now part of errai-demos)

--- errai-cdi/demos/errai-cdi-mvp-demo/src/main/java/org/jboss/errai/cdi/demo/mvp/client/local/event/ContactDeletedEventHandler.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.errai.cdi.demo.mvp.client.local.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface ContactDeletedEventHandler extends EventHandler {
-  void onContactDeleted(ContactDeletedEvent event);
-}
--- errai-cdi/demos/errai-cdi-mvp-demo/src/main/java/org/jboss/errai/cdi/demo/mvp/client/local/event/EditContactEventHandler.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.errai.cdi.demo.mvp.client.local.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface EditContactEventHandler extends EventHandler {
-  void onEditContact(EditContactEvent event);
-}
