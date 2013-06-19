2cb7ab0d5d1922d970745e89ccb983458c4425e0
Jonathan Fuerth <jfuerth@gmail.com>
2013-06-19 14:54:47 -0400

    Enhanced version updater so it updates the quickstart versions

--- updateVersions.sh
+++ updateVersions.sh
@@ -1,3 +1,31 @@
-mvn versions:set -DnewVersion=$1
+if [ -z "$1" -o -z "$2" ]; then
+    echo "Usage:"
+    echo "  $0 <oldversion> <newversion>"
+    exit 1
+fi
+
+oldversion=$1
+newversion=$2
+
+if ! grep "$1" quickstart/src/main/docbook/en/*.xml; then
+    echo "Didn't find oldversion $oldversion in any quickstart guide"
+    exit 1
+fi
+
+mvn versions:set -DnewVersion=$newversion
 mvn clean install -Dgwt.compiler.skip=true
-for proj in errai-demos/errai-* errai-bom errai-version-master; do (cd $proj; mvn versions:set -DnewVersion=$1); done
+for proj in errai-demos errai-demos/errai-* errai-bom errai-version-master; do (cd $proj; mvn versions:set -DnewVersion=$newversion); done
+
+for file in quickstart/src/main/docbook/en/*.xml; do
+    sed "s/$oldversion/$newversion/g" <$file >$file.tmp
+    mv $file.tmp $file
+done
+
+echo "Visual Sanity Check for old version $oldversion and snapshots..."
+find . -name pom.xml | xargs grep -3 "$oldversion"
+find . -name pom.xml | xargs grep -e -SNAPSHOT
+echo "Done sanity check"
+
+echo " !!"
+echo " !! Remember to update the versions in reference/src/main/docbook/en/Book_Info.xml"
+echo " !!"
