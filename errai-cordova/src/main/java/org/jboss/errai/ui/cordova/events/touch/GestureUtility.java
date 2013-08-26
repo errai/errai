80d2fd8cf550d43c5ddb6ce48b22b98568b32c66
edewit <erikjan.dewit@gmail.com>
2013-08-26 15:14:27 +0200

    added long touch event handler

--- errai-cordova/src/main/java/org/jboss/errai/ui/cordova/events/touch/GestureUtility.java
+++ errai-cordova/src/main/java/org/jboss/errai/ui/cordova/events/touch/GestureUtility.java
@@ -1,8 +1,12 @@
 package org.jboss.errai.ui.cordova.events.touch;
 
+import com.google.gwt.event.dom.client.TouchEndEvent;
+import com.google.gwt.event.dom.client.TouchMoveEvent;
+import com.google.gwt.event.dom.client.TouchStartEvent;
 import com.google.gwt.user.client.ui.Widget;
 import org.jboss.errai.ui.cordova.events.touch.longtap.LongTapEvent;
 import org.jboss.errai.ui.cordova.events.touch.longtap.LongTapHandler;
+import org.jboss.errai.ui.cordova.events.touch.longtap.LongTapRecognizer;
 
 /**
  * @author edewit@redhat.com
@@ -16,6 +20,10 @@ public class GestureUtility {
   }
 
   public void addLongTapHandler(LongTapHandler handler) {
+    LongTapRecognizer longTapRecognizer = new LongTapRecognizer(source);
+    source.addDomHandler(longTapRecognizer, TouchStartEvent.getType());
+    source.addDomHandler(longTapRecognizer, TouchMoveEvent.getType());
+    source.addDomHandler(longTapRecognizer, TouchEndEvent.getType());
     source.addHandler(handler, LongTapEvent.getType());
   }
 }
