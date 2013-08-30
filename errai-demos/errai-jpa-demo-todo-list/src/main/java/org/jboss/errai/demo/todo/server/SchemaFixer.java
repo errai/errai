a9cfb65eb297c6c77c33a80f8df9592221a54749
Max Barkley <mbarkley@redhat.com>
2013-08-30 14:45:04 -0400

    Fixed SQL error caused by SchemaFixer in errai-jpa-demo-todo-list
    Some SQL schemas do not store column names in lower case. This caused
    SchemaFixer to incorrectly attempt to add a duplicate column causing a
    failure to deploy via jboss.

--- errai-demos/errai-jpa-demo-todo-list/src/main/java/org/jboss/errai/demo/todo/server/SchemaFixer.java
+++ errai-demos/errai-jpa-demo-todo-list/src/main/java/org/jboss/errai/demo/todo/server/SchemaFixer.java
@@ -16,7 +16,7 @@ public class SchemaFixer {
   public void alterSchemaIfNeeded() {
     System.out.println("Checking if schema needs update...");
 
-    Query schemaCheck = em.createNativeQuery("SELECT 1 FROM information_schema.columns WHERE table_name='todolist_user' AND column_name='password'");
+    Query schemaCheck = em.createNativeQuery("SELECT 1 FROM information_schema.columns WHERE lower(table_name)='todolist_user' AND lower(column_name)='password'");
     if (schemaCheck.getResultList().isEmpty()) {
       System.out.println("Attempting to add password column...");
       Query ddlStmt = em.createNativeQuery("ALTER TABLE todolist_user ADD COLUMN password VARCHAR(100)");
