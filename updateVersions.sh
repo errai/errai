a5702dd22c718443bc23968560e79d1287c547b4
Christian Sadilek <christian.sadilek@gmail.com>
2013-10-07 15:28:12 -0400

    Updated to version 2.4.1.Final

--- updateVersions.sh
+++ updateVersions.sh
@@ -7,19 +7,9 @@ fi
 oldversion=$1
 newversion=$2
 
-if ! grep "$1" quickstart/src/main/docbook/en/*.xml; then
-    echo "Didn't find oldversion $oldversion in any quickstart guide"
-    exit 1
-fi
-
 mvn versions:set -DnewVersion=$newversion
 mvn clean install -Dgwt.compiler.skip=true -Dmaven.test.skip=true
 for proj in errai-demos errai-demos/errai-* errai-bom errai-version-master; do (cd $proj; mvn versions:set -DnewVersion=$newversion); done
-
-for file in quickstart/src/main/docbook/en/*.xml; do
-    sed "s/$oldversion/$newversion/g" <$file >$file.tmp
-    mv $file.tmp $file
-done
 
 echo "Visual Sanity Check for old version $oldversion and snapshots..."
 find . -name pom.xml | xargs grep -3 "$oldversion"
