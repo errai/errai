daefdb7176597aef29d769bbe6c2f7249df6b11e
Christian Sadilek <christian.sadilek@gmail.com>
2013-10-21 13:23:37 -0400

    Fixed ERRAI-657: Weld warning for ErraiServiceBean and MessageBusBean

--- errai-cdi/weld-integration/src/main/java/org/jboss/errai/cdi/server/MessageBusBean.java
+++ errai-cdi/weld-integration/src/main/java/org/jboss/errai/cdi/server/MessageBusBean.java
@@ -18,8 +18,11 @@ package org.jboss.errai.cdi.server;
 import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableSet;
 
-import org.jboss.errai.bus.client.api.messaging.MessageBus;
-import org.jboss.errai.bus.server.api.ServerMessageBus;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.spi.CreationalContext;
@@ -27,12 +30,11 @@ import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.PassivationCapable;
 import javax.enterprise.util.AnnotationLiteral;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+
+import org.jboss.errai.bus.client.api.messaging.MessageBus;
+import org.jboss.errai.bus.server.api.ServerMessageBus;
 
 /**
  * Basically a bean wrapper that provides CDI meta data.
@@ -40,7 +42,7 @@ import java.util.Set;
  *
  * @author Heiko Braun <hbraun@redhat.com>
  */
-public class MessageBusBean implements Bean {
+public class MessageBusBean implements Bean, PassivationCapable {
   final MessageBus delegate;
 
   static final Set<Annotation> qualifiers = unmodifiableSet(new HashSet<Annotation>(
@@ -58,47 +60,63 @@ public class MessageBusBean implements Bean {
     this.delegate = delegate;
   }
 
+  @Override
   public Class<?> getBeanClass() {
     return MessageBus.class;
   }
 
+  @Override
   public Set<InjectionPoint> getInjectionPoints() {
     return Collections.emptySet();
   }
 
+  @Override
   public String getName() {
     return null;
   }
 
+  @Override
   public Set<Annotation> getQualifiers() {
     return qualifiers;
   }
 
+  @Override
   public Class<? extends Annotation> getScope() {
     return ApplicationScoped.class;
   }
 
+  @Override
   public Set<Class<? extends Annotation>> getStereotypes() {
     return Collections.emptySet();
   }
 
+  @Override
   public Set<Type> getTypes() {
     return types;
   }
 
+  @Override
   public boolean isAlternative() {
     return false;
   }
 
+  @Override
   public boolean isNullable() {
     return false;
   }
 
+  @Override
   public Object create(CreationalContext ctx) {
     return delegate;
   }
 
+  @Override
   public void destroy(Object instance, CreationalContext ctx) {
     ctx.release();
+  }
+
+  @Override
+  public String getId() {
+    return this.getClass().getName();
   }
 }
