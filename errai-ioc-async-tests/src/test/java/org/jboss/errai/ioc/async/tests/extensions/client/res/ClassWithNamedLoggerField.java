10200fdd5dd4c7b7e3e90a5e4cea790bdd9e28d9
Max Barkley <mbarkley@redhat.com>
2013-11-07 13:33:58 -0500

    Add async tests for logger injector

---
+++ errai-ioc-async-tests/src/test/java/org/jboss/errai/ioc/async/tests/extensions/client/res/ClassWithNamedLoggerField.java
@@ -0,0 +1,19 @@
+package org.jboss.errai.ioc.async.tests.extensions.client.res;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.jboss.errai.ioc.client.api.EntryPoint;
+import org.slf4j.Logger;
+
+@EntryPoint
+public class ClassWithNamedLoggerField {
+
+  public static final String LOGGER_NAME = "a unique logger name!!!";
+  
+  @Inject @Named(LOGGER_NAME) private Logger logger;
+  
+  public Logger getLogger() {
+    return logger;
+  }
+}
