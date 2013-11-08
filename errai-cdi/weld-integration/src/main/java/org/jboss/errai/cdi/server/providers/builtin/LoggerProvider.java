8c0ac621eb1895f1d75e448082bebba044ec530c
Max Barkley <mbarkley@redhat.com>
2013-11-08 15:06:38 -0500

    Change injection by name to use custom annotation (workaround issue with @Named)

--- errai-cdi/weld-integration/src/main/java/org/jboss/errai/cdi/server/providers/builtin/LoggerProvider.java
+++ errai-cdi/weld-integration/src/main/java/org/jboss/errai/cdi/server/providers/builtin/LoggerProvider.java
@@ -3,23 +3,25 @@ package org.jboss.errai.cdi.server.providers.builtin;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
+import org.jboss.errai.common.client.api.annotations.AltLogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.inject.name.Named;
-
 public class LoggerProvider {
+  
+  
 
-  @Produces public Logger produceLogger(InjectionPoint injectionPoint) {
+  @Produces
+  public Logger produceLogger(final InjectionPoint injectionPoint) {
     final String loggerName;
-    if (injectionPoint.getQualifiers().contains(Named.class)) {
-      loggerName = injectionPoint.getAnnotated().getAnnotation(Named.class).value();
+    if (injectionPoint.getAnnotated().isAnnotationPresent(AltLogger.class)) {
+      loggerName = injectionPoint.getAnnotated().getAnnotation(AltLogger.class).value();
     }
     else {
       loggerName = injectionPoint.getMember().getDeclaringClass().getName();
     }
-    
+
     return LoggerFactory.getLogger(loggerName);
   }
-  
+
 }
---
+++ errai-common/src/main/java/org/jboss/errai/common/client/api/annotations/AltLogger.java
@@ -0,0 +1,31 @@
+package org.jboss.errai.common.client.api.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.slf4j.Logger;
+
+/**
+ * When injecting a {@link Logger}, this annotation can be used to specify a
+ * name. If this annotation is used but no value is given, the root logger will
+ * be provided.
+ * 
+ * Example usage:
+ * 
+ * <pre>
+ * // Gets root logger
+ * {@literal @Inject @AltLogger} Logger logger;
+ * 
+ * // Gets logger with name 'LoggerName'
+ * {@literal @Inject @AltLogger("LoggerName")} Logger logger;
+ * </pre>
+ * 
+ * @author mbarkley <mbarkley@redhat.com>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface AltLogger {
+  String value() default org.slf4j.Logger.ROOT_LOGGER_NAME;
+}
