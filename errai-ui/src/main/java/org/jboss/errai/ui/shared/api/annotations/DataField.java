381a3ac4a26820a770f37c5b4b4ccc3a87255450
Jonathan Fuerth <jfuerth@gmail.com>
2013-11-25 11:57:16 -0500

    Updated javadoc for new match-by-id and -class capabilities

--- errai-ui/src/main/java/org/jboss/errai/ui/shared/api/annotations/DataField.java
+++ errai-ui/src/main/java/org/jboss/errai/ui/shared/api/annotations/DataField.java
@@ -28,16 +28,45 @@ import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Composite;
 
 /**
- * This annotation may only be used in subclasses of {@link Composite} that has been annotated with {@link Templated},
- * or in a super-class of said {@link Composite} types.
+ * Indicates that the target Java reference (field, method parameter, or constructor parameter) in a {@link Templated}
+ * {@link Composite} component corresponds to an HTML element in the class's companion template.
  * <p>
- * Indicates that a given field, method parameter, or constructor parameter in a {@link Templated} {@link Composite}
- * component corresponds to the HTML template {@link Element} with <code>data-field</code> attribute matching the
- * annotated element.
+ * <b>This annotation can only be used in subclasses of {@link Composite} that have been annotated with {@link Templated},
+ * or in super-classes of such {@link Composite} types.</b>
  * <p>
- * By default, the name of the corresponding <code>data-field</code> will be defined by the name of the field, method
- * parameter, or constructor parameter name, but the name may also be overridden using the {@link #value()} attribute of
- * this annotation.
+ * The matching of Java references to HTML elements is performed as follows:
+ * <ol>
+ *  <li>A <i>name</i> for the Java reference is determined. If the {@code @DataField} annotation has a {@link #value}
+ *      argument, it is used as the reference name. For fields, the default name is the field name. Method and constructor
+ *      parameters have no default name, so they must always specify a value.
+ *  <li>If there is an element in the HTML template with attribute <tt>data-field=<i>name</i></tt>, the Java reference
+ *      will point to this element. If there is more than one such element, the Java reference will point to the first.
+ *  <li>Otherwise, if there is an element in the HTML template with attribute <tt>id=<i>name</i></tt>, the Java reference
+ *      will point to this element. If there is more than one such element, the Java reference will point to the first.
+ *  <li>Otherwise, if there is an element in the HTML template with a CSS style class <tt><i>name</i></tt>, the Java reference
+ *      will point to this element. If there is more than one such element, the Java reference will point to the first.
+ *      For elements with more than one CSS style, each style name is considered individually. For example:
+ * <pre>
+ *  &lt;div class="eat drink be-merry"&gt;
+ * </pre>
+ *      matches Java references named <tt>eat</tt>, <tt>drink</tt>, or <tt>be-merry</tt>.
+ *  <li>If no matching element is found by this point, it is an error.
+ * </ol>
+ * <p>
+ * If more than one Java reference matches the same HTML element in the template, it is an error. For example,
+ * given a template containing the element <tt>&lt;div class="eat drink be-merry"&gt;</tt>, the following Java code
+ * is in error:
+ * <pre>
+ * @Templated
+ * public class ErroneousTemplate extends Composite {
+ *  @Inject @DataField
+ *  private Label eat;
+ * 
+ *  @Inject @DataField
+ *  private Label drink;
+ * }
+ * </pre>
+ * because both fields <tt>eat</tt> and <tt>drink</tt> refer to the same HTML <tt>div</tt> element.
  * <p>
  * When used on a method or constructor parameter, this must be accompanied by a corresponding {@link Inject}
  * annotation; however, when used on a field, construction may be performed manually or via {@link Inject}.
@@ -46,6 +75,8 @@ import com.google.gwt.user.client.ui.Composite;
  * <b>See also:</b> {@link Templated}, {@link EventHandler}, {@link Bound}
  * 
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * @author Christian Sadilek <csadilek@redhat.com>
+ * @author Jonathan Fuerth <jfuerth@redhat.com>
  */
 @Inherited
 @Documented
