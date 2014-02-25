42e7b17bda22d1c749d637eb77fcae52756afcb7
Max Barkley <mbarkley@redhat.com>
2014-02-25 14:32:17 -0500

    Remove RPC call from RoleStyleBindingProvider.

--- errai-security/errai-security-client/src/main/java/org/jboss/errai/security/client/local/RoleStyleBindingProvider.java
+++ errai-security/errai-security-client/src/main/java/org/jboss/errai/security/client/local/RoleStyleBindingProvider.java
@@ -1,19 +1,23 @@
 package org.jboss.errai.security.client.local;
 
-import com.google.gwt.dom.client.Style;
-import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import org.jboss.errai.ioc.client.api.AfterInitialization;
-import org.jboss.errai.security.client.local.identity.Identity;
-import org.jboss.errai.security.shared.RequireRoles;
-import org.jboss.errai.ui.shared.api.style.AnnotationStyleBindingExecutor;
-import org.jboss.errai.ui.shared.api.style.StyleBindingsRegistry;
-
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import java.lang.annotation.Annotation;
+import org.jboss.errai.security.client.local.identity.ActiveUserProvider;
+import org.jboss.errai.security.shared.RequireRoles;
+import org.jboss.errai.security.shared.Role;
+import org.jboss.errai.security.shared.User;
+import org.jboss.errai.ui.shared.api.style.AnnotationStyleBindingExecutor;
+import org.jboss.errai.ui.shared.api.style.StyleBindingsRegistry;
+
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.Element;
 
 /**
  * RoleStyleBindingProvider makes sure that client elements annotated by {@link RequireRoles} are made invisible for
@@ -21,34 +25,44 @@ import java.lang.annotation.Annotation;
  *
  * @see RequireRoles
  * @author edewit@redhat.com
+ * @author Max Barkley <mbarkley@redhat.com>
  */
 @Singleton
 public class RoleStyleBindingProvider {
 
-  private final Identity identity;
+  private final ActiveUserProvider userProvider;
 
   @Inject
-  public RoleStyleBindingProvider(Identity identity) {
-    this.identity = identity;
+  public RoleStyleBindingProvider(final ActiveUserProvider userProvider) {
+    this.userProvider = userProvider;
   }
 
-  @AfterInitialization
+  @PostConstruct
   public void init() {
     StyleBindingsRegistry.get().addStyleBinding(this, RequireRoles.class, new AnnotationStyleBindingExecutor() {
       @Override
       public void invokeBinding(final Element element, final Annotation annotation) {
-        identity.hasPermission(new AsyncCallback<Boolean>() {
-
-          @Override
-          public void onSuccess(Boolean result) {
-            element.getStyle().setVisibility(result ? Style.Visibility.VISIBLE : Style.Visibility.HIDDEN);
-          }
-
-          @Override
-          public void onFailure(Throwable caught) {
-          }
-        }, ((RequireRoles) annotation).value());
+        final User user = userProvider.getActiveUser();
+        if (user == null || !hasRoles(user.getRoles(), ((RequireRoles) annotation).value()))
+          element.getStyle().setVisibility(Style.Visibility.HIDDEN);
+        else
+          element.getStyle().setVisibility(Style.Visibility.VISIBLE);
       }
     });
   }
+  
+  private boolean hasRoles(final List<Role> userRoles, final String[] requiredRoles) {
+    final Set<String> userRolesByName = new HashSet<String>();
+    for (final Role role : userRoles) {
+      userRolesByName.add(role.getName());
+    }
+
+    for (int i = 0; i < requiredRoles.length; i++) {
+      if (!userRolesByName.contains(requiredRoles[i]))
+        return false;
+    }
+    
+    return true;
+  }
+
 }
