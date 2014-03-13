b3daf3d9bf69e1e556d500eeef63ccb137b20111
Max Barkley <mbarkley@redhat.com>
2014-03-13 15:37:24 -0400

    Use JAX-RS for MessageService.

--- errai-demos/errai-security-demo/src/main/java/org/jboss/errai/security/demo/client/local/App.java
+++ errai-demos/errai-security-demo/src/main/java/org/jboss/errai/security/demo/client/local/App.java
@@ -4,6 +4,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.jboss.errai.enterprise.client.jaxrs.api.RestClient;
 import org.jboss.errai.ui.nav.client.local.Navigation;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
@@ -12,9 +13,6 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 
-/**
- *
- */
 @Templated("#body")
 @ApplicationScoped
 public class App extends Composite {
@@ -34,5 +32,6 @@ public class App extends Composite {
   public void clientMain() {
     content.add(navigation.getContentPanel());
     RootPanel.get().add(this);
+    RestClient.setApplicationRoot("/errai-security-demo/rest/");
   }
 }
--- errai-demos/errai-security-demo/src/main/java/org/jboss/errai/security/demo/client/shared/MessageService.java
+++ errai-demos/errai-security-demo/src/main/java/org/jboss/errai/security/demo/client/shared/MessageService.java
@@ -1,17 +1,24 @@
 package org.jboss.errai.security.demo.client.shared;
 
-import org.jboss.errai.bus.server.annotations.Remote;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 import org.jboss.errai.security.shared.RequireAuthentication;
 import org.jboss.errai.security.shared.RequireRoles;
 
 /**
  * @author edewit@redhat.com
  */
-@Remote
+@Path("message")
 public interface MessageService {
   @RequireAuthentication
+  @GET
+  @Produces("application/json")
   String hello();
 
   @RequireRoles("admin")
+  @GET
+  @Produces("application/json")
   String ping();
 }
