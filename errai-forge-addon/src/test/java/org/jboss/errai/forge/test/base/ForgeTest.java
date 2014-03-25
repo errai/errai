56f149c77a7dee0737d28bf7c734c747643980e7
Max Barkley <mbarkley@redhat.com>
2014-03-25 16:15:19 -0400

    Changed versions in errai addon to align with the rest of errai.

--- errai-forge-addon/src/test/java/org/jboss/errai/forge/test/base/ForgeTest.java
+++ errai-forge-addon/src/test/java/org/jboss/errai/forge/test/base/ForgeTest.java
@@ -35,7 +35,7 @@ import org.junit.runner.RunWith;
 public abstract class ForgeTest {
 
   public static final String DEPENDENCY = "org.jboss.errai.forge:errai-forge-addon";
-  public static final String VERSION = "2.0.0-SNAPSHOT";
+  public static final String VERSION = "3.0-SNAPSHOT";
   public static final String FORGE_VERSION = "2.1.1.Final";
   public static final String ADDON_GROUP = "org.jboss.forge.addon";
 
