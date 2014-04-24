errai/errai

commit 246dbcdb46a25bb5adcb80f8c08acfef8935fa43

Resolve ERRAI-715.
CoreFacet now sets a persistent property after installation, which is
used to identify if it is installed. This allows users to change plugin
configurations and core configurations without removing the ability to
use the "errai-(add)|(remove)-features" commands.

errai-forge-addon/src/main/java/org/jboss/errai/forge/facet/aggregate/CoreFacet.java
@@ -16,7 +16,10 @@
  */
 package org.jboss.errai.forge.facet.aggregate;
 
+import javax.inject.Inject;
+
 import org.jboss.errai.forge.config.ProjectConfig;
+import org.jboss.errai.forge.config.ProjectProperty;
 import org.jboss.errai.forge.facet.dependency.ErraiBuildDependencyFacet;
 import org.jboss.errai.forge.facet.module.ModuleCoreFacet;
 import org.jboss.errai.forge.facet.plugin.CleanPluginFacet;
@@ -26,7 +29,8 @@ import org.jboss.errai.forge.facet.plugin.GwtPluginFacet;
 import org.jboss.errai.forge.facet.plugin.JbossPluginFacet;
 import org.jboss.errai.forge.facet.plugin.WarPluginFacet;
 import org.jboss.errai.forge.facet.resource.ErraiAppPropertiesFacet;
-import org.jboss.forge.addon.facets.constraints.FacetConstraint;
+import org.jboss.forge.addon.facets.FacetFactory;
+import org.jboss.forge.addon.projects.ProjectFacet;
 
 /**
  * Aggregates core facets required by all other facet aggregators. Installing
@@ -36,11 +40,24 @@ import org.jboss.forge.addon.facets.constraints.FacetConstraint;
  * 
  * @author Max Barkley <mbarkley@redhat.com>
  */
-@FacetConstraint({ ProjectConfig.class, ErraiBuildDependencyFacet.class, CleanPluginFacet.class,
-    CompilerPluginFacet.class,
-    DependencyPluginFacet.class, GwtPluginFacet.class, JbossPluginFacet.class, WarPluginFacet.class,
-    ModuleCoreFacet.class, ErraiAppPropertiesFacet.class })
 public class CoreFacet extends BaseAggregatorFacet {
+  
+  @Inject
+  private FacetFactory facetFactory;
+
+  @SuppressWarnings({ "unchecked" })
+  private static final Class<? extends ProjectFacet>[] coreFacets = new Class[] {
+    ProjectConfig.class,
+    ErraiBuildDependencyFacet.class,
+    CleanPluginFacet.class,
+    CompilerPluginFacet.class,
+    DependencyPluginFacet.class,
+    GwtPluginFacet.class,
+    JbossPluginFacet.class,
+    WarPluginFacet.class,
+    ModuleCoreFacet.class,
+    ErraiAppPropertiesFacet.class
+  };
 
   @Override
   public String getFeatureName() {
@@ -55,5 +72,36 @@ public class CoreFacet extends BaseAggregatorFacet {
   @Override
   public String getShortName() {
     return "core";
+  }
+
+  @Override
+  public boolean install() {
+    boolean installWasSuccessful = true;
+    
+    for (int i = 0; i < coreFacets.length && installWasSuccessful; i++) {
+      try {
+        facetFactory.install(getProject(), coreFacets[i]);
+      }
+      catch (IllegalStateException e) {
+        installWasSuccessful = false;
+      }
+    }
+
+    if (installWasSuccessful) {
+      getProject().getFacet(ProjectConfig.class).setProjectProperty(ProjectProperty.CORE_IS_INSTALLED, true);
+    }
+
+    return installWasSuccessful;
+  }
+
+  @Override
+  public boolean isInstalled() {
+    if (!getProject().hasFacet(ProjectConfig.class))
+      return false;
+
+    final Boolean isCoreInstalled = getProject().getFacet(ProjectConfig.class).getProjectProperty(
+            ProjectProperty.CORE_IS_INSTALLED, Boolean.class);
+
+    return isCoreInstalled != null && isCoreInstalled;
   }
 }

errai-forge-addon/src/main/java/org/jboss/errai/forge/config/ProjectProperty.java
@@ -16,7 +16,8 @@ public enum ProjectProperty {
    */
   MODULE_NAME(String.class),
   INSTALLED_FEATURES(SerializableSet.class),
-  ERRAI_VERSION(String.class);
+  ERRAI_VERSION(String.class),
+  CORE_IS_INSTALLED(Boolean.class);
 
   public final Class<?> valueType;
 

errai-forge-addon/src/main/java/org/jboss/errai/forge/config/converter/BooleanConverter.java
@@ -1,0 +1,20 @@
+package org.jboss.errai.forge.config.converter;
+
+public class BooleanConverter implements ConfigTypeConverter<Boolean> {
+
+  @Override
+  public Boolean convertFromString(final String stored) {
+    return Boolean.valueOf(stored);
+  }
+
+  @Override
+  public String convertToString(final Boolean toStore) {
+    return toStore.toString();
+  }
+
+  @Override
+  public Class<Boolean> getType() {
+    return Boolean.class;
+  }
+
+}

errai-forge-addon/src/main/java/org/jboss/errai/forge/config/converter/ConfigTypeConverterFactory.java
@@ -11,7 +11,8 @@ public class ConfigTypeConverterFactory {
   private static final ConfigTypeConverter<?>[] DEFAULT_CONVERTERS = new ConfigTypeConverter<?>[] {
     new FileConverter(),
     new SetConverter(),
-    new StringConverter()
+    new StringConverter(),
+    new BooleanConverter()
   };
   
   private final Map<Class<?>, ConfigTypeConverter<?>> converterMap;

commit 5166d57c01f61609b0875095e4365aa181b641ce

Add aggregator facet tests and fix uninstall bug introduced by 246dbcd

errai-forge-addon/src/main/java/org/jboss/errai/forge/config/ProjectConfig.java
@@ -104,8 +104,8 @@ public class ProjectConfig extends AbstractFacet<Project> implements ProjectFace
     final ConfigTypeConverter<T> converter = (ConfigTypeConverter<T>) converterFactory.getConverter(property.valueType);
     final Configuration config = project.getFacet(ConfigurationFacet.class).getConfiguration();
     final String attribute = getProjectAttribute(property);
-    
-    config.addProperty(attribute, converter.convertToString(value));
+
+    config.setProperty(attribute, converter.convertToString(value));
   }
 
   /**

errai-forge-addon/src/main/java/org/jboss/errai/forge/config/converter/SetConverter.java
@@ -11,10 +11,7 @@ class SetConverter implements ConfigTypeConverter<SerializableSet> {
 
   @Override
   public String convertToString(final SerializableSet toStore) {
-    final SerializableSet serializableSet = new SerializableSet();
-    serializableSet.addAll(toStore);
-    
-    return serializableSet.serialize();
+    return toStore.serialize();
   }
 
   @Override

errai-forge-addon/src/main/java/org/jboss/errai/forge/facet/aggregate/BaseAggregatorFacet.java
@@ -16,6 +16,7 @@
  */
 package org.jboss.errai.forge.facet.aggregate;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -73,7 +74,7 @@ public abstract class BaseAggregatorFacet implements ProjectFacet, MutableFacet<
                 toUninstall.getFeatureName());
       }
       else {
-        return String.format("The facet %s still requires %s.", facetType.getSimpleName());
+        return String.format("The facet %s still requires %s.", facetType.getSimpleName(), toUninstall.getFeatureName());
       }
     }
   }
@@ -134,6 +135,7 @@ public abstract class BaseAggregatorFacet implements ProjectFacet, MutableFacet<
     }
     directlyInstalled.remove(getClass());
     directlyInstalled.add(CoreFacet.class);
+    directlyInstalled.addAll(Arrays.asList(CoreFacet.coreFacets));
 
     final Set<Class<? extends ProjectFacet>> toUninstall = traverseUninstallable(directlyInstalled);
 
@@ -204,7 +206,7 @@ public abstract class BaseAggregatorFacet implements ProjectFacet, MutableFacet<
     while (!toVisit.isEmpty()) {
       final Class<? extends ProjectFacet> cur = toVisit.poll();
       if (!traversed.contains(cur)
-              && (!BaseAggregatorFacet.class.isAssignableFrom(cur) || !intentionallyInstalled.contains(cur))
+              && !intentionallyInstalled.contains(cur)
               // Only add errai facets to be uninstalled
               && (BaseAggregatorFacet.class.isAssignableFrom(cur) || AbstractBaseFacet.class.isAssignableFrom(cur))) {
         traversed.add(cur);

errai-forge-addon/src/main/java/org/jboss/errai/forge/facet/aggregate/CoreFacet.java
@@ -20,6 +20,7 @@ import javax.inject.Inject;
 
 import org.jboss.errai.forge.config.ProjectConfig;
 import org.jboss.errai.forge.config.ProjectProperty;
+import org.jboss.errai.forge.facet.base.CoreBuildFacet;
 import org.jboss.errai.forge.facet.dependency.ErraiBuildDependencyFacet;
 import org.jboss.errai.forge.facet.module.ModuleCoreFacet;
 import org.jboss.errai.forge.facet.plugin.CleanPluginFacet;
@@ -30,6 +31,7 @@ import org.jboss.errai.forge.facet.plugin.JbossPluginFacet;
 import org.jboss.errai.forge.facet.plugin.WarPluginFacet;
 import org.jboss.errai.forge.facet.resource.ErraiAppPropertiesFacet;
 import org.jboss.forge.addon.facets.FacetFactory;
+import org.jboss.forge.addon.facets.constraints.FacetConstraint;
 import org.jboss.forge.addon.projects.ProjectFacet;
 
 /**
@@ -40,24 +42,21 @@ import org.jboss.forge.addon.projects.ProjectFacet;
  * 
  * @author Max Barkley <mbarkley@redhat.com>
  */
+@FacetConstraint({ ErraiBuildDependencyFacet.class, ProjectConfig.class, CoreBuildFacet.class })
 public class CoreFacet extends BaseAggregatorFacet {
-  
+
   @Inject
   private FacetFactory facetFactory;
 
+  /*
+   * These core facets cannot be FacetConstraints because we want users to be
+   * able to modify maven plugin configurations after running errai-setup
+   * (ERRAI-715).
+   */
   @SuppressWarnings({ "unchecked" })
-  private static final Class<? extends ProjectFacet>[] coreFacets = new Class[] {
-    ProjectConfig.class,
-    ErraiBuildDependencyFacet.class,
-    CleanPluginFacet.class,
-    CompilerPluginFacet.class,
-    DependencyPluginFacet.class,
-    GwtPluginFacet.class,
-    JbossPluginFacet.class,
-    WarPluginFacet.class,
-    ModuleCoreFacet.class,
-    ErraiAppPropertiesFacet.class
-  };
+  public static final Class<? extends ProjectFacet>[] coreFacets = new Class[] { 
+      CleanPluginFacet.class, CompilerPluginFacet.class, DependencyPluginFacet.class, GwtPluginFacet.class,
+      JbossPluginFacet.class, WarPluginFacet.class, ModuleCoreFacet.class, ErraiAppPropertiesFacet.class };
 
   @Override
   public String getFeatureName() {
@@ -77,7 +76,7 @@ public class CoreFacet extends BaseAggregatorFacet {
   @Override
   public boolean install() {
     boolean installWasSuccessful = true;
-    
+
     for (int i = 0; i < coreFacets.length && installWasSuccessful; i++) {
       try {
         facetFactory.install(getProject(), coreFacets[i]);

errai-forge-addon/src/main/java/org/jboss/errai/forge/facet/aggregate/ErraiMessagingFacet.java
@@ -16,7 +16,6 @@
  */
 package org.jboss.errai.forge.facet.aggregate;
 
-import org.jboss.errai.forge.facet.dependency.ErraiBuildDependencyFacet;
 import org.jboss.errai.forge.facet.dependency.ErraiBusDependencyFacet;
 import org.jboss.errai.forge.facet.module.ErraiBusModuleFacet;
 import org.jboss.errai.forge.facet.resource.ErraiBusServletConfigFacet;
@@ -29,8 +28,8 @@ import org.jboss.forge.addon.facets.constraints.FacetConstraint;
  * 
  * @author Max Barkley <mbarkley@redhat.com>
  */
-@FacetConstraint({ CoreFacet.class, ErraiBusDependencyFacet.class, ErraiBuildDependencyFacet.class,
-    ErraiBusModuleFacet.class, ErraiBusServletConfigFacet.class })
+@FacetConstraint({ CoreFacet.class, ErraiBusDependencyFacet.class, ErraiBusModuleFacet.class,
+    ErraiBusServletConfigFacet.class })
 public class ErraiMessagingFacet extends BaseAggregatorFacet {
 
   @Override

errai-forge-addon/src/test/java/org/jboss/errai/forge/facet/aggregate/AggregatorFacetTest.java
@@ -1,0 +1,202 @@
+package org.jboss.errai.forge.facet.aggregate;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import org.jboss.errai.forge.config.ProjectConfig;
+import org.jboss.errai.forge.config.ProjectProperty;
+import org.jboss.errai.forge.config.SerializableSet;
+import org.jboss.errai.forge.facet.aggregate.BaseAggregatorFacet.UninstallationExecption;
+import org.jboss.errai.forge.test.base.ForgeTest;
+import org.jboss.forge.addon.facets.Facet;
+import org.jboss.forge.addon.facets.FacetFactory;
+import org.jboss.forge.addon.facets.MutableFaceted;
+import org.jboss.forge.addon.facets.constraints.FacetConstraint;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFacet;
+import org.junit.Test;
+
+@SuppressWarnings({ "unchecked", "rawtypes" })
+public class AggregatorFacetTest extends ForgeTest {
+
+  @Inject
+  private FacetFactory facetFactory;
+
+  @Test
+  public void testUninstallRemovesConstraints() throws Exception {
+    final Project project = createTestProject();
+
+    addFeature(project, ErraiIocFacet.class);
+    assertTrue(removeFeature(project, ErraiIocFacet.class));
+
+    final Set<Class<? extends Facet>> coreFacetTypes = getCoreFacetConstraints();
+
+    for (final Class<? extends Facet> facetType : ErraiIocFacet.class.getAnnotation(FacetConstraint.class).value()) {
+      if (!coreFacetTypes.contains(facetType)) {
+        assertFalse(facetType.getSimpleName() + " was not uninstalled.",
+                project.hasFacet((Class<? extends ProjectFacet>) facetType));
+        assertFalse(facetType.getSimpleName() + " was not uninstalled.", facetFactory.create(project, facetType)
+                .isInstalled());
+      }
+      else {
+        assertTrue(facetType.getSimpleName() + " was uninstalled.",
+                project.hasFacet((Class<? extends ProjectFacet>) facetType));
+        assertTrue(facetType.getSimpleName() + " was uninstalled.", facetFactory.create(project, facetType)
+                .isInstalled());
+      }
+    }
+  }
+
+  @Test
+  public void testAggregatorDoesNotUninstallCore() throws Exception {
+    final Project project = createTestProject();
+
+    final CoreFacet coreFacet = facetFactory.install(project, CoreFacet.class);
+    addFeature(project, ErraiMessagingFacet.class);
+
+    // Precondition
+    assertTrue(removeFeature(project, ErraiMessagingFacet.class));
+
+    // Actual test
+    assertTrue(project.hasFacet(CoreFacet.class));
+    assertTrue(coreFacet.isInstalled());
+
+    for (final Class<? extends ProjectFacet> facetType : CoreFacet.coreFacets) {
+      assertTrue(facetType.getSimpleName() + " was uninstalled.", project.hasFacet(facetType));
+      assertTrue(facetType.getSimpleName() + " was uninstalled.", facetFactory.create(project, facetType).isInstalled());
+    }
+
+    for (final Class<? extends ProjectFacet> facetType : (Class<? extends ProjectFacet>[]) CoreFacet.class
+            .getAnnotation(FacetConstraint.class).value()) {
+      assertTrue(facetType.getSimpleName() + " was uninstalled.", project.hasFacet(facetType));
+      assertTrue(facetType.getSimpleName() + " was uninstalled.", facetFactory.create(project, facetType).isInstalled());
+    }
+  }
+
+  @Test
+  public void testUninstallDoesNotRemoveOtherDirectlyInstalled() throws Exception {
+    final Project project = createTestProject();
+
+    addFeature(project, ErraiIocFacet.class);
+    addFeature(project, ErraiUiFacet.class);
+
+    assertTrue(removeFeature(project, ErraiUiFacet.class));
+
+    assertTrue("ErraiIocFacet was uninstalled.", project.hasFacet(ErraiIocFacet.class));
+    assertTrue("ErraiIocFacet was uninstalled.", facetFactory.create(project, ErraiIocFacet.class).isInstalled());
+  }
+
+  @Test
+  public void testUninstallDoesRemoveOtherNotDirectlyInstalled() throws Exception {
+    final Project project = createTestProject();
+    addFeature(project, ErraiUiFacet.class);
+    final ErraiIocFacet iocFacet = project.getFacet(ErraiIocFacet.class);
+
+    // Precondition
+    assertTrue(iocFacet.isInstalled());
+
+    assertTrue(removeFeature(project, ErraiUiFacet.class));
+    assertFalse(project.hasFacet(ErraiIocFacet.class));
+    assertFalse(iocFacet.isInstalled());
+  }
+  
+  @Test
+  public void testCanInstallThenUninstallTwoRelatedFeatures() throws Exception {
+    final Project project = createTestProject();
+
+    final ErraiIocFacet iocFacet = addFeature(project, ErraiIocFacet.class);
+    final ErraiUiFacet uiFacet = addFeature(project, ErraiUiFacet.class);
+
+    assertTrue(removeFeature(project, ErraiUiFacet.class));
+
+    assertTrue("ErraiIocFacet was uninstalled.", project.hasFacet(ErraiIocFacet.class));
+    assertTrue("ErraiIocFacet was uninstalled.", iocFacet.isInstalled());
+    assertFalse("ErraiUiFacet was not uninstalled.", project.hasFacet(ErraiUiFacet.class));
+    assertFalse("ErraiUiFacet was not uninstalled.", uiFacet.isInstalled());
+    
+    assertTrue(removeFeature(project, ErraiIocFacet.class));
+    
+    assertFalse("ErraiUiFacet was not uninstalled.", project.hasFacet(ErraiUiFacet.class));
+    assertFalse("ErraiUiFacet was not uninstalled.", uiFacet.isInstalled());
+    assertFalse("ErraiIocFacet was not uninstalled.", project.hasFacet(ErraiIocFacet.class));
+    assertFalse("ErraiIocFacet was not uninstalled.", iocFacet.isInstalled());
+  }
+  
+  private boolean removeFeature(final Project project, Class<? extends BaseAggregatorFacet> featureType) throws IllegalStateException, UninstallationExecption {
+    final BaseAggregatorFacet facet = project.getFacet(featureType);
+
+    final ProjectConfig projectConfig = project.getFacet(ProjectConfig.class);
+    final SerializableSet installedFeatures = projectConfig.getProjectProperty(ProjectProperty.INSTALLED_FEATURES,
+            SerializableSet.class);
+    installedFeatures.remove(facet.getShortName());
+    projectConfig.setProjectProperty(ProjectProperty.INSTALLED_FEATURES, installedFeatures);
+    
+    boolean success = facet.uninstallRequirements();
+    
+    if (!success)
+      return false;
+    
+    success = ((MutableFaceted<ProjectFacet>)project).uninstall(facet);
+    
+    return success;
+  }
+
+  private <T extends BaseAggregatorFacet> T addFeature(final Project project, Class<T> facetType) {
+    final T aggregatorFacet = facetFactory.install(project, facetType);
+    final ProjectConfig projectConfig = project.getFacet(ProjectConfig.class);
+
+    final SerializableSet installedFeatures = projectConfig.getProjectProperty(ProjectProperty.INSTALLED_FEATURES,
+            SerializableSet.class);
+    installedFeatures.add(aggregatorFacet.getShortName());
+    projectConfig.setProjectProperty(ProjectProperty.INSTALLED_FEATURES, installedFeatures);
+
+    return aggregatorFacet;
+  }
+  
+  private Project createTestProject() {
+    final Project project = initializeJavaProject();
+    final ProjectConfig projectConfig = facetFactory.install(project, ProjectConfig.class);
+
+    projectConfig.setProjectProperty(ProjectProperty.ERRAI_VERSION, "3.0-SNAPSHOT");
+    projectConfig.setProjectProperty(ProjectProperty.MODULE_LOGICAL, "org.jboss.errai.ForgeTest");
+    projectConfig.setProjectProperty(ProjectProperty.MODULE_FILE, new File(project.getRootDirectory()
+            .getUnderlyingResourceObject(), "src/main/java/org/jboss/errai/ForgeTest.gwt.xml"));
+    projectConfig.setProjectProperty(ProjectProperty.MODULE_NAME, "test");
+    projectConfig.setProjectProperty(ProjectProperty.INSTALLED_FEATURES, new SerializableSet());
+
+    return project;
+  }
+
+  private Set<Class<? extends Facet>> getCoreFacetConstraints() {
+    final Set<Class<? extends Facet>> coreFacetTypes = new HashSet();
+    coreFacetTypes.add(CoreFacet.class);
+
+    final Queue<Class<? extends Facet>> toVisit = new LinkedList<Class<? extends Facet>>();
+    toVisit.addAll((Collection) Arrays.asList(CoreFacet.class.getAnnotation(FacetConstraint.class).value()));
+    toVisit.addAll(Arrays.asList(CoreFacet.coreFacets));
+
+    while (!toVisit.isEmpty()) {
+      final Class<? extends Facet> facetType = toVisit.poll();
+      if (facetType.isAnnotationPresent(FacetConstraint.class)) {
+        for (final Class<? extends Facet> foundFacetType : facetType.getAnnotation(FacetConstraint.class).value()) {
+          if (!coreFacetTypes.contains(foundFacetType)) {
+            toVisit.add(foundFacetType);
+          }
+        }
+      }
+      coreFacetTypes.add(facetType);
+    }
+
+    return coreFacetTypes;
+  }
+
+}

errai-forge-addon/src/test/java/org/jboss/errai/forge/test/base/ForgeTest.java
@@ -20,6 +20,7 @@ import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.errai.forge.facet.aggregate.AggregatorFacetTest;
 import org.jboss.errai.forge.facet.plugin.BasePluginFacetTest;
 import org.jboss.forge.addon.facets.FacetFactory;
 import org.jboss.forge.addon.projects.Project;
@@ -57,7 +58,8 @@ public abstract class ForgeTest {
             .addBeansXML()
             .addClasses(
                     ForgeTest.class,
-                    BasePluginFacetTest.class
+                    BasePluginFacetTest.class,
+                    AggregatorFacetTest.class
                     )
             .addAsAddonDependencies(
                     AddonDependencyEntry.create("org.jboss.forge.furnace.container:cdi", FORGE_VERSION),