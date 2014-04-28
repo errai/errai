d88a89c42a487a21234aa152bafce3af4870c746
Christian Sadilek <christian.sadilek@gmail.com>
2014-04-28 12:09:27 -0400

    Fixes and cleanup to errai-marshalling pom

--- errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
+++ errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
@@ -1,4 +1,4 @@
-//generated using Reflections JavaCodeSerializer [Mon Apr 28 11:49:30 EDT 2014]
+//generated using Reflections JavaCodeSerializer [Mon Apr 28 12:01:17 EDT 2014]
 package org.jboss.errai.reflections;
 
 import static org.jboss.errai.reflections.serializers.JavaCodeSerializer.*;
