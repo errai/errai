d582e449a0c591ad37c4520dd1c1c6e1076a848a
Marius Gerwinn <marius.gerwinn@fileee.com>
2014-05-29 18:39:11 +0200

    errai formatting style applied

--- errai-annotation-processors/src/main/java/org/jboss/errai/processor/TemplatedAnnotationChecker.java
+++ errai-annotation-processors/src/main/java/org/jboss/errai/processor/TemplatedAnnotationChecker.java
@@ -22,40 +22,52 @@ import javax.tools.FileObject;
 import javax.tools.StandardLocation;
 
 /**
- * Evaluates usage of the ErraiUI Templated annotation and emits errors and warnings when
- * the annotation is not being used correctly.
+ * Evaluates usage of the ErraiUI Templated annotation and emits errors and
+ * warnings when the annotation is not being used correctly.
  */
 @SupportedAnnotationTypes(TypeNames.TEMPLATED)
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class TemplatedAnnotationChecker extends AbstractProcessor {
 
   @Override
-  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+  public boolean process(Set<? extends TypeElement> annotations,
+          RoundEnvironment roundEnv) {
     final Types types = processingEnv.getTypeUtils();
     final Elements elements = processingEnv.getElementUtils();
-    final TypeMirror gwtCompositeType = elements.getTypeElement(TypeNames.GWT_COMPOSITE).asType();
-    
+    final TypeMirror gwtCompositeType = elements.getTypeElement(
+            TypeNames.GWT_COMPOSITE).asType();
+
     for (TypeElement annotation : annotations) {
       for (Element target : roundEnv.getElementsAnnotatedWith(annotation)) {
         if (!types.isAssignable(target.asType(), gwtCompositeType)) {
-          processingEnv.getMessager().printMessage(
-                  Kind.ERROR, "@Templated classes must be a direct or indirect subtype of Composite", target);
+          processingEnv
+                  .getMessager()
+                  .printMessage(
+                          Kind.ERROR,
+                          "@Templated classes must be a direct or indirect subtype of Composite",
+                          target);
         }
-        
+
         PackageElement packageElement = elements.getPackageOf(target);
         String templateRef = getReferencedTemplate(target);
         String templateRefError = null;
         try {
-          FileObject resource = processingEnv.getFiler().getResource(StandardLocation.CLASS_PATH, packageElement.getQualifiedName(), templateRef);
+          FileObject resource = processingEnv.getFiler().getResource(
+                  StandardLocation.CLASS_PATH,
+                  packageElement.getQualifiedName(), templateRef);
           CharSequence charContent = resource.getCharContent(true);
         } catch (IllegalArgumentException e) {
-          // unfortunately, Eclipse just throws IAE when we try to read files from CLASS_PATH
-          // so the best we can do is ignore this error and skip validating the template reference
+          // unfortunately, Eclipse just throws IAE when we try to read files
+          // from CLASS_PATH
+          // so the best we can do is ignore this error and skip validating the
+          // template reference
         } catch (IOException e) {
-          templateRefError = "Could not access associated template " + templateRef;
+          templateRefError = "Could not access associated template "
+                  + templateRef;
         }
         if (templateRefError != null) {
-          processingEnv.getMessager().printMessage(Kind.ERROR, templateRefError, annotation);
+          processingEnv.getMessager().printMessage(Kind.ERROR,
+                  templateRefError, annotation);
         }
       }
     }
@@ -66,21 +78,25 @@ public class TemplatedAnnotationChecker extends AbstractProcessor {
    * Resolves the filename that the given class's {@code @Templated} annotation
    * points to, taking all default behaviour into account.
    * 
-   * @param target a class that bears the {@code Templated} annotation.
+   * @param target
+   *          a class that bears the {@code Templated} annotation.
    */
   private String getReferencedTemplate(Element target) {
     String templateRef = "";
-    AnnotationValue paramValue = getAnnotationParamValueWithoutDefaults(target, TypeNames.TEMPLATED, "value");
+    AnnotationValue paramValue = getAnnotationParamValueWithoutDefaults(target,
+            TypeNames.TEMPLATED, "value");
     if (paramValue != null) {
-    	if(paramValue.getValue().toString().startsWith("#")){
-    		//use simple name
-    	}else if(paramValue.getValue().toString().contains("#")){
-    		String[] split = paramValue.getValue().toString().split("#");
-    		if(split!=null && split.length>0){
-    			//use html part
-    			templateRef = split[0];
-    		}
-    	}else{
+      if (paramValue.getValue().toString().startsWith("#")) {
+        // use simple name
+      }
+      else if (paramValue.getValue().toString().contains("#")) {
+        String[] split = paramValue.getValue().toString().split("#");
+        if (split != null && split.length > 0) {
+          // use html part
+          templateRef = split[0];
+        }
+      }
+      else {
         templateRef = paramValue.getValue().toString();
       }
     }
@@ -89,5 +105,5 @@ public class TemplatedAnnotationChecker extends AbstractProcessor {
     }
     return templateRef;
   }
-  
+
 }
