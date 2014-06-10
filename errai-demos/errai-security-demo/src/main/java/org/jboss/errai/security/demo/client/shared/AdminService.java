d801a35a83b78a5b593f24e8ffaf94d74de78111
Max Barkley <mbarkley@redhat.com>
2014-06-10 15:48:04 -0400

    Add example of RequiredRolesProvider to errai-security-demo.

--- errai-demos/errai-security-demo/src/main/java/org/jboss/errai/security/demo/client/shared/AdminService.java
+++ errai-demos/errai-security-demo/src/main/java/org/jboss/errai/security/demo/client/shared/AdminService.java
@@ -8,15 +8,15 @@ import org.jboss.errai.security.shared.api.Role;
 import org.jboss.errai.security.shared.api.annotation.RestrictedAccess;
 
 /**
- * This is a secured JAX-RS endpoint. The "admin" {@link Role} is required to
- * access this service. Because the {@link RestrictedAccess} annotation is on
- * the type, any methods added to this type would also be secured. It is also
- * possible to annotate individual methods for finer-grained control, or to
- * annotate both the type and methods (in which case the roles will be combined
- * an all roles must be present for access to be granted).
+ * This is a secured JAX-RS endpoint. The {@link AdminRoleProvider} will return the "admin"
+ * {@link Role} at runtime, so this role is required to access the service. Because the
+ * {@link RestrictedAccess} annotation is on the type, any methods added to this type would also be
+ * secured. It is also possible to annotate individual methods for finer-grained control, or to
+ * annotate both the type and methods (in which case the roles will be combined an all roles must be
+ * present for access to be granted).
  */
 @Path("/admin")
-@RestrictedAccess(roles = "admin")
+@RestrictedAccess(providers = { AdminRoleProvider.class })
 public interface AdminService {
 
   @Path("/ping")
