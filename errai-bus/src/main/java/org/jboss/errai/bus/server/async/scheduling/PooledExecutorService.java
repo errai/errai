69d7bb1e991ee1e0818a6310da026b9d5c82980e
Christian Sadilek <christian.sadilek@gmail.com>
2014-06-17 15:54:13 -0400

    Use logger to report InterruptedException of scheduler thread

--- errai-bus/src/main/java/org/jboss/errai/bus/server/async/scheduling/PooledExecutorService.java
+++ errai-bus/src/main/java/org/jboss/errai/bus/server/async/scheduling/PooledExecutorService.java
@@ -18,6 +18,7 @@ package org.jboss.errai.bus.server.async.scheduling;
 
 import static java.lang.System.currentTimeMillis;
 import static java.util.concurrent.locks.LockSupport.parkUntil;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -31,8 +32,10 @@ import org.jboss.errai.bus.server.service.ErraiServiceConfiguratorImpl;
 import org.jboss.errai.common.client.api.tasks.AsyncTask;
 import org.jboss.errai.common.client.api.tasks.HasAsyncTaskRef;
 import org.jboss.errai.common.client.util.TimeUnit;
+import org.slf4j.Logger;
 
 public class PooledExecutorService implements TaskProvider {
+  private final static Logger log = getLogger(PooledExecutorService.class);
   private final BlockingQueue<TimedTask> queue;
 
   /**
@@ -397,8 +400,8 @@ public class PooledExecutorService implements TaskProvider {
           }
         }
         catch (InterruptedException e) {
-          e.printStackTrace();
-          // just fall through.
+          // This will happen during container shutdown, so logging as debug is sufficient.
+          log.debug("Scheduler thread interrupted", e);
         }
         catch (Throwable t) {
           t.printStackTrace();
