d0f431c5fcb144e8d0c38d4b2743036a779d1d0a
Max Barkley <mbarkley@redhat.com>
2014-07-16 16:26:28 -0400

    ERRAI-769: Don't throw Exception (log warning instead) when LESS file doesn't not compile.

--- errai-ui/src/test/java/org/jboss/errai/ui/rebind/less/LessConverterTest.java
+++ errai-ui/src/test/java/org/jboss/errai/ui/rebind/less/LessConverterTest.java
@@ -1,9 +1,9 @@
 package org.jboss.errai.ui.rebind.less;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.AdditionalMatchers.not;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
