d9fabf972e9adc81f3ea46601b64828c8acb9008
Michel Werren <michel.werren@axonlab.ch>
2014-08-18 21:27:53 +0200

    WebSocket filter improvement:     - one (new) method executed before message processing, and one after     - additional methods parameter: shared property map for user use     - better documentation of the filter

--- errai-bus-websocket/errai-bus-jsr356-websocket/src/main/java/org/jboss/errai/bus/server/websocket/jsr356/filter/FilterDelegate.java
+++ errai-bus-websocket/errai-bus-jsr356-websocket/src/main/java/org/jboss/errai/bus/server/websocket/jsr356/filter/FilterDelegate.java
@@ -2,25 +2,43 @@ package org.jboss.errai.bus.server.websocket.jsr356.filter;
 
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
-
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Michel Werren
  */
 public class FilterDelegate {
   /**
-   * Invoke filters defined in web.xml.
+   * Invoke filters defined in web.xml before message processing.
    * 
    * @param websocketSession
    * @param httpSession
    * @param message
    */
-  public static void invokeFilter(Session websocketSession, HttpSession httpSession, String message) {
+  public static void invokeFilterBefore(Session websocketSession, HttpSession httpSession,
+          Map<Object, Object> sharedProperties, String message) {
     final List<WebSocketFilter> filters = FilterLookup.getInstance().getFilters();
     if (filters != null) {
       for (WebSocketFilter filter : filters) {
-        filter.doFilter(websocketSession, httpSession, message);
+        filter.beforeProcessingMessage(websocketSession, httpSession, sharedProperties, message);
+      }
+    }
+  }
+
+  /**
+   * Invoke filters defined in web.xml after message processing.
+   * 
+   * @param websocketSession
+   * @param httpSession
+   * @param message
+   */
+  public static void invokeFilterAfter(Session websocketSession, HttpSession httpSession,
+          Map<Object, Object> sharedProperties, String message) {
+    final List<WebSocketFilter> filters = FilterLookup.getInstance().getFilters();
+    if (filters != null) {
+      for (WebSocketFilter filter : filters) {
+        filter.afterProcessingMessage(websocketSession, httpSession, sharedProperties, message);
       }
     }
   }
