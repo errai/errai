7cb39e9e942357d56e20a0bf485578a6ee3cbc67
Christian Sadilek <christian.sadilek@gmail.com>
2014-10-28 14:37:25 -0400

    Removed outdated setup script

--- errai-bus/tools/projectCreator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#
-# Creates an ErraiBus Project skeleton
-#
-
-mvn archetype:generate -DarchetypeGroupId=org.jboss.errai.bus -DarchetypeArtifactId=bus-starter-archetype -DarchetypeVersion=1.1-SNAPSHOT -DarchetypeRepository=http://snapshots.jboss.org/maven2
