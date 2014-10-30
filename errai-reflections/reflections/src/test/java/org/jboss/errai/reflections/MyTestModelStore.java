3b06a3cef6b47c4be3cb003225a5a02415d80d8f
Christian Sadilek <christian.sadilek@gmail.com>
2014-10-30 13:43:41 -0400

    Upgraded to jboss-integration-platform 6.0.0.CR16
    Conflicts:
    	pom.xml

--- errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
+++ errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
@@ -1,4 +1,4 @@
-//generated using Reflections JavaCodeSerializer [Wed Oct 29 13:21:21 EDT 2014]
+//generated using Reflections JavaCodeSerializer [Thu Oct 30 16:15:13 EDT 2014]
 package org.jboss.errai.reflections;
 
 import static org.jboss.errai.reflections.serializers.JavaCodeSerializer.*;
