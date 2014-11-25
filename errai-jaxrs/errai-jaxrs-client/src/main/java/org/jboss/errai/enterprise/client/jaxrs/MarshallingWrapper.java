af1a21fc1222d9ec0be9c8222d1043aae8b74645
Stefan Wokusch <stefan@wokusch.de>
2014-11-25 08:03:58 +0100

    ERRAI-796 Enhance MarshallingWrapper to be able to use a custom library
    Enhance MarshallingWrapper to be able to use a custom marsahlling
    library. This is needed to use a Jackson Marshaler that has some support
    for Jackson-Annotations. Im my example i use gwt-jackson.

--- errai-jaxrs/errai-jaxrs-client/src/main/java/org/jboss/errai/enterprise/client/jaxrs/MarshallingWrapper.java
+++ errai-jaxrs/errai-jaxrs-client/src/main/java/org/jboss/errai/enterprise/client/jaxrs/MarshallingWrapper.java
@@ -29,58 +29,110 @@ import org.jboss.errai.marshalling.client.Marshalling;
  * @author Christian Sadilek <csadilek@redhat.com>
  */
 public class MarshallingWrapper {
+  public static interface Marshaller{
+    String toJSON(final Object obj);
+    String toJSON(final Map<Object, Object> obj);
+    String toJSON(final List<?> arr);
+    <T> T fromJSON(final String json, final Class<T> type);
+    <T> T fromJSON(final String json, final Class<T> type, final Class<?> elementType);
+    <K, V> Map<K, V> fromJSON(final String json, final Class<?> type, final Class<K> mapKeyType, final Class<V> mapValueType);
+    Object fromJSON(final String json);
+  }
+  
+  private static Marshaller marschaller = new Marshaller() {
+    @Override
+    public String toJSON(Object obj) {
+      return _toJSON(Marshalling.toJSON(obj));
+    }
+    
+    @Override
+    public String toJSON(Map<Object, Object> obj) {
+      return _toJSON(Marshalling.toJSON(obj));
+    }
+    
+    @Override
+    public String toJSON(List<?> arr) {
+      return _toJSON(Marshalling.toJSON(arr));
+    }
+    
+    private String _toJSON(final String json) {
+      if (RestClient.isJacksonMarshallingActive()) {
+        return JacksonTransformer.toJackson(json);
+      }
+      return json;
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T fromJSON(String json, Class<T> type) {
+      if (json.contains(SerializationParts.ENCODED_TYPE)) {
+        // This is Errai's native JSON format (we don't need to transform and don't need to rely on
+        // the provided type since it's part of the payload)
+        return (T) Marshalling.fromJSON(json);
+      }
+      return Marshalling.fromJSON(_fromJSON(json), type);
+    }
+    
+    @Override
+    public <T> T fromJSON(String json, Class<T> type, Class<?> elementType) {
+    if (elementType == null) {
+        return fromJSON(json, type);
+      }
+      return Marshalling.fromJSON(_fromJSON(json), type, elementType);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public <K, V> Map<K, V> fromJSON(String json, Class<?> type, Class<K> mapKeyType, Class<V> mapValueType) {
+      return (Map<K, V>) Marshalling.fromJSON(_fromJSON(json), type, mapKeyType, mapValueType);
+    }
+    
+    @Override
+    public Object fromJSON(String json) {
+      return Marshalling.fromJSON(_fromJSON(json), Object.class);
+    }
+    
+    private String _fromJSON(final String json) {
+      if (RestClient.isJacksonMarshallingActive()) {
+        return JacksonTransformer.fromJackson(json);
+      }
+      return json;
+    }
+  };
+  
+  public static void setMarschaller(Marshaller marschaller) {
+    MarshallingWrapper.marschaller = marschaller;
+  }
+  
+  public static Marshaller getMarschaller() {
+    return marschaller;
+  }
 
   public static String toJSON(final Object obj) {
-    return _toJSON(Marshalling.toJSON(obj));
+    return marschaller.toJSON(obj);
   }
 
   public static String toJSON(final Map<Object, Object> obj) {
-    return _toJSON(Marshalling.toJSON(obj));
+    return marschaller.toJSON(obj);
   }
 
   public static String toJSON(final List<?> arr) {
-    return _toJSON(Marshalling.toJSON(arr));
+    return marschaller.toJSON(arr);
   }
 
-  private static String _toJSON(final String json) {
-    if (RestClient.isJacksonMarshallingActive()) {
-      return JacksonTransformer.toJackson(json);
-    }
-
-    return json;
-  }
-
-  @SuppressWarnings("unchecked")
   public static <T> T fromJSON(final String json, final Class<T> type) {
-    if (json.contains(SerializationParts.ENCODED_TYPE)) {
-      // This is Errai's native JSON format (we don't need to transform and don't need to rely on
-      // the provided type since it's part of the payload)
-      return (T) Marshalling.fromJSON(json);
-    }
-    return Marshalling.fromJSON(_fromJSON(json), type);
+    return marschaller.fromJSON(json, type);
   }
 
   public static <T> T fromJSON(final String json, final Class<T> type, final Class<?> elementType) {
-    if (elementType == null) {
-      return fromJSON(json, type);
-    }
-    return Marshalling.fromJSON(_fromJSON(json), type, elementType);
+    return marschaller.fromJSON(json, type, elementType);
   }
 
-  @SuppressWarnings("unchecked")
   public static <K, V> Map<K, V> fromJSON(final String json, final Class<?> type, final Class<K> mapKeyType, final Class<V> mapValueType) {
-    return (Map<K, V>) Marshalling.fromJSON(_fromJSON(json), type, mapKeyType, mapValueType);
+    return marschaller.fromJSON(json, type, mapKeyType, mapValueType);
   }
 
   public static Object fromJSON(final String json) {
-    return Marshalling.fromJSON(_fromJSON(json), Object.class);
-  }
-
-  private static String _fromJSON(final String json) {
-    if (RestClient.isJacksonMarshallingActive()) {
-      return JacksonTransformer.fromJackson(json);
-    }
-
-    return json;
+    return marschaller.fromJSON(json);
   }
 }
