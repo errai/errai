bc656070c6f4f1585a2652e658a24a00c160b321
Christian Sadilek <christian.sadilek@gmail.com>
2014-12-19 15:06:25 -0500

    Implemented ERRAI-826: Navigation container injectable as a @DataField

---
+++ errai-navigation/src/main/java/org/jboss/errai/ui/nav/client/local/NavigationPanelProvider.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014 JBoss, by Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.errai.ui.nav.client.local;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+import org.jboss.errai.ioc.client.api.IOCProvider;
+
+/**
+ * {@link IOCProvider} to make the default navigation panel injectable.
+ * 
+ * @author Christian Sadilek <csadilek@redhat.com>
+ */
+@IOCProvider
+@Singleton
+public class NavigationPanelProvider implements Provider<NavigationPanel> {
+
+  @Inject
+  private Navigation navigation;
+
+  @Override
+  public NavigationPanel get() {
+    if (!(navigation.getContentPanel() instanceof NavigationPanel)) {
+      throw new RuntimeException("Default navigation panel is not of type: " + NavigationPanel.class.getName()
+              + ". You replaced it with: " + navigation.getContentPanel().getClass().getName());
+    }
+
+    return (NavigationPanel) navigation.getContentPanel();
+  }
+}
