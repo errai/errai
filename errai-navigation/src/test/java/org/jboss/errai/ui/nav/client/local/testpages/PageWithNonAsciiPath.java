f04d98e38c456bab32b2a72b11c08b90c1f743da
divd <divya.dadlani@gmail.com>
2015-02-25 18:01:51 -0500

    Updated tests for non-ASCII characters in page path.

--- errai-navigation/src/test/java/org/jboss/errai/ui/nav/client/local/testpages/PageWithNonAsciiPath.java
+++ errai-navigation/src/test/java/org/jboss/errai/ui/nav/client/local/testpages/PageWithNonAsciiPath.java
@@ -7,7 +7,7 @@ import com.google.gwt.user.client.ui.SimplePanel;
  * @author Divya Dadlani <ddadlani@redhat.com>
  */
 @Page(path = "pagename/path/some:{var1}thing/öther{var2} thing")
-public class PageWithComplexPathParameters extends SimplePanel {
+public class PageWithNonAsciiPath extends SimplePanel {
 
   // tests for a URL containing spaces and non-ASCII Unicode characters
 
