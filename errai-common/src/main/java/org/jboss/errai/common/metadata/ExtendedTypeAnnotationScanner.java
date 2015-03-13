df935ad1b2182fbff0cd333f7a660ea27d4495ed
Christian Sadilek <christian.sadilek@gmail.com>
2015-03-13 12:11:11 -0400

    Avoid unnecessary calls to CacheUtil.getCache

--- errai-common/src/main/java/org/jboss/errai/common/metadata/ExtendedTypeAnnotationScanner.java
+++ errai-common/src/main/java/org/jboss/errai/common/metadata/ExtendedTypeAnnotationScanner.java
@@ -1,6 +1,8 @@
 package org.jboss.errai.common.metadata;
 
 import javassist.bytecode.ClassFile;
+
+import org.jboss.errai.common.metadata.MetaDataScanner.CacheHolder;
 import org.jboss.errai.common.rebind.CacheUtil;
 import org.jboss.errai.reflections.adapters.MetadataAdapter;
 import org.jboss.errai.reflections.scanners.TypeAnnotationsScanner;
@@ -15,14 +17,13 @@ import java.util.TreeSet;
  * @author Mike Brock
  */
 public class ExtendedTypeAnnotationScanner extends TypeAnnotationsScanner {
+  private final CacheHolder cache = CacheUtil.getCache(MetaDataScanner.CacheHolder.class);
+  
   @Override
   public void scan(final Object cls) {
-    @SuppressWarnings("unchecked") final
-    MetadataAdapter adapter = getMetadataAdapter();
-
+    final MetadataAdapter adapter = getMetadataAdapter();
     final String className = adapter.getClassName(cls);
 
-    // noinspection unchecked
     for (final String annotationType : (List<String>) adapter.getClassAnnotationNames(cls)) {
       if (acceptResult(annotationType) ||
           annotationType.equals(Inherited.class.getName())) { // as an exception, accept
@@ -30,10 +31,10 @@ public class ExtendedTypeAnnotationScanner extends TypeAnnotationsScanner {
         getStore().put(annotationType, className);
 
         if (cls instanceof ClassFile) {
-          Set<SortableClassFileWrapper> classes = CacheUtil.getCache(MetaDataScanner.CacheHolder.class).ANNOTATIONS_TO_CLASS.get(annotationType);
+          Set<SortableClassFileWrapper> classes = cache.ANNOTATIONS_TO_CLASS.get(annotationType);
           if (classes == null) {
-            CacheUtil.getCache(MetaDataScanner.CacheHolder.class).ANNOTATIONS_TO_CLASS.put(annotationType, classes =
-                Collections.synchronizedSet(new TreeSet<SortableClassFileWrapper>()));
+            cache.ANNOTATIONS_TO_CLASS.put(annotationType, classes = 
+                    Collections.synchronizedSet(new TreeSet<SortableClassFileWrapper>()));
           }
           classes.add(new SortableClassFileWrapper(className, (ClassFile) cls));
         }
