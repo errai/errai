69f7e257eac61664d2e9dd70be2edc624fdb78f4
Christian Sadilek <christian.sadilek@gmail.com>
2015-05-15 13:26:57 -0400

    Added filter to write authenticated user to js var on host page

---
+++ errai-security/errai-security-server/src/main/java/org/jboss/errai/security/shared/api/SecurityConstants.java
@@ -0,0 +1,8 @@
+package org.jboss.errai.security.shared.api;
+
+public class SecurityConstants {
+  public static final String ERRAI_SECURITY_CONTEXT_DICTIONARY = "errai_security_context";
+  public static final String DICTIONARY_USER = "user";
+
+  private SecurityConstants() {};
+}
