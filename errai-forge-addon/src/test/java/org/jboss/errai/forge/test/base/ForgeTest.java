1063aaccda3e669b7c9da4987d680238a8fae1d3
Max Barkley <mbarkley@redhat.com>
2015-12-07 14:48:07 -0500

    Update Errai version used in errai-forge-addon tests.

--- errai-forge-addon/src/test/java/org/jboss/errai/forge/test/base/ForgeTest.java
+++ errai-forge-addon/src/test/java/org/jboss/errai/forge/test/base/ForgeTest.java
@@ -16,6 +16,16 @@
  */
 package org.jboss.errai.forge.test.base;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.inject.Inject;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.errai.forge.config.ProjectConfig;
@@ -37,22 +47,13 @@ import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.runner.RunWith;
 
-import javax.inject.Inject;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 @RunWith(Arquillian.class)
 public abstract class ForgeTest {
 
   public static final String DEPENDENCY = "org.jboss.errai.forge:errai-forge-addon";
   public static final String ADDON_GROUP = "org.jboss.forge.addon";
   // TODO Programmatically lookup the Errai version this test is running in.
-  public static final String ERRAI_TEST_VERSION = "3.2.0-SNAPSHOT";
+  public static final String ERRAI_TEST_VERSION = "4.0.0-SNAPSHOT";
 
   @Inject
   protected ProjectFactory projectFactory;
@@ -101,7 +102,7 @@ public abstract class ForgeTest {
 
     return project;
   }
-  
+
   protected void assertResourceAndFileContentsSame(final String resourcePath, final File file)
           throws IOException {
     assertTrue(file.getAbsolutePath() + " was not created.", file.exists());
@@ -123,7 +124,7 @@ public abstract class ForgeTest {
         builders[i].append(chars, 0, read);
       }
     }
-    
+
     try {
       assertEquals(builders[0].toString(), builders[1].toString());
     }
@@ -133,7 +134,7 @@ public abstract class ForgeTest {
 
       System.out.println("OBSERVED");
       System.out.println(builders[1].toString());
-      
+
       throw e;
     }
   }
@@ -141,14 +142,14 @@ public abstract class ForgeTest {
   protected Project createErraiTestProject() {
     final Project project = initializeJavaProject();
     final ProjectConfig projectConfig = facetFactory.install(project, ProjectConfig.class);
-  
+
     projectConfig.setProjectProperty(ProjectProperty.ERRAI_VERSION, ERRAI_TEST_VERSION);
     projectConfig.setProjectProperty(ProjectProperty.MODULE_LOGICAL, "org.jboss.errai.ForgeTest");
     projectConfig.setProjectProperty(ProjectProperty.MODULE_FILE, new File(project.getRootDirectory()
             .getUnderlyingResourceObject(), "src/main/java/org/jboss/errai/ForgeTest.gwt.xml"));
     projectConfig.setProjectProperty(ProjectProperty.MODULE_NAME, "test");
     projectConfig.setProjectProperty(ProjectProperty.INSTALLED_FEATURES, new SerializableSet());
-  
+
     return project;
   }
 
