ec742a4a0874938b6b00ed57b34d999f2d42074f
Max Barkley <mbarkley@redhat.com>
2015-12-18 16:13:59 -0500

    Add missing license headers and update copyright notice in existing headers.

--- errai-security/errai-security-server/src/main/java/org/jboss/errai/security/shared/api/identity/UserImpl.java
+++ errai-security/errai-security-server/src/main/java/org/jboss/errai/security/shared/api/identity/UserImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 JBoss Inc
+ * Copyright (C) 2014 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.jboss.errai.security.shared.api.identity;
 
 import static java.util.Collections.unmodifiableMap;
