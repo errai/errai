acf27005d3096eb33f5ba8eea1cd20b3e1d7dd34
Max Barkley <mbarkley@redhat.com>
2015-12-18 13:16:01 -0500

    Revert "Remove old JBoss and Red Hat license headers. Add new "Red Hat and affiliates" header."
    Changes from the reverted commit did not preserve original copyright dates.
    
    This reverts commit d8e735dfe2f6ed25fe9e06b753ae2f6154d76b02.

--- errai-codegen/src/test/java/org/jboss/errai/codegen/test/model/PortableIntegerParameterDefinition.java
+++ errai-codegen/src/test/java/org/jboss/errai/codegen/test/model/PortableIntegerParameterDefinition.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (C) 2015 Red Hat, Inc. and/or its affiliates.
+/*
+ * Copyright 2012 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jboss.errai.codegen.test.model;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
