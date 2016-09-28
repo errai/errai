2665cd6923964982d97d7f32bd0c7642038c4906
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2016-09-28 22:42:22 +1000

    New DOM elements Paragraph and Label

---
+++ errai-common/src/main/java/org/jboss/errai/common/client/dom/Paragraph.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.errai.common.client.dom;
+
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.jboss.errai.common.client.api.annotations.Element;
+
+/**
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/HTMLParagraphElement">Web API</a>
+ */
+@JsType(isNative = true)
+@Element("p")
+public interface Paragraph extends HTMLElement {
+
+    @JsProperty
+    String getAlign();
+
+    @JsProperty
+    void setAlign(String align);
+
+}
