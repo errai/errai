21ae12f90f23c2c497c35b4a7d5d87df441028f0
Cristiano Nicolai <cristiano.nicolai@gmail.com>
2016-10-17 11:41:08 +1000

    New IFrame DOM element

---
+++ errai-common/src/main/java/org/jboss/errai/common/client/dom/IFrame.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.errai.common.client.dom;
+
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import org.jboss.errai.common.client.api.annotations.Element;
+
+/**
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/HTMLIFrameElement">Web API</a>
+ */
+@JsType(isNative = true)
+@Element("iframe")
+public interface IFrame extends HTMLElement {
+
+  @JsProperty String getHeight();
+  @JsProperty void setHeight(String height);
+
+  @JsProperty String getWidth();
+  @JsProperty void setWidth(String width);
+
+  @JsProperty String getName();
+  @JsProperty void setName(String name);
+
+  @JsProperty String getSrc();
+  @JsProperty void setSrc(String src);
+
+}
