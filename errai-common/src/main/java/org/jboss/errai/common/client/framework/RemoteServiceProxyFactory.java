9c413c3c80d21b7e109e977ee03331a5f7768c7e
Max Barkley <mbarkley@redhat.com>
2017-01-31 15:07:25 -0500

    ERRAI-1024: Add hints to "no proxy provider found" error message

--- errai-common/src/main/java/org/jboss/errai/common/client/framework/RemoteServiceProxyFactory.java
+++ errai-common/src/main/java/org/jboss/errai/common/client/framework/RemoteServiceProxyFactory.java
@@ -23,38 +23,43 @@ import org.jboss.errai.common.client.api.Assert;
 
 /**
  * {@link ProxyFactory} storing {@link ProxyProvider}s for generated remote proxies.
- * 
+ *
  * @author Christian Sadilek <csadilek@redhat.com>
  */
 public class RemoteServiceProxyFactory implements ProxyFactory {
-  private static Map<Class<?>, ProxyProvider> remoteProxyProviders = new HashMap<Class<?>, ProxyProvider>();
+  private static Map<Class<?>, ProxyProvider> remoteProxyProviders = new HashMap<>();
 
   @Override
   @SuppressWarnings({ "unchecked" })
   public <T> T getRemoteProxy(final Class<T> proxyType) {
     Assert.notNull(proxyType);
-    
+
     if (remoteProxyProviders.isEmpty()) {
-      throw new RuntimeException("There are no proxy providers registered.");
+      throw new RuntimeException("There are no proxy providers registered.\n"
+              + "Hint: You may need to move this call to an @AfterInitialization method.");
     }
-    
+
     final ProxyProvider proxyProvider = remoteProxyProviders.get(proxyType);
     if (proxyProvider == null) {
-      throw new RuntimeException("No proxy provider found for type: " + proxyType.getName());
+      throw new RuntimeException("No proxy provider found for type [" + proxyType.getName() + "].\n"
+              + "Hint: Did you just add a new remote interface? The GWT generator may not have run in which case you should try:\n"
+              + "\t1. Touching an RPC interface that already has a proxy and refreshing (i.e. run `touch RemoteInterface.java`).\n"
+              + "\t2. Clear your Super Dev Mode cache (go to 127.0.0.1:9876 and click \"clear cache\").\n"
+              + "\t3. Run Super Dev Mode without incremental compilation.");
     }
-    
+
     final Object proxy = proxyProvider.getProxy();
     if (proxy == null) {
       throw new RuntimeException("No proxy instance provided for: " + proxyType.getName());
     }
-    
+
     return (T) proxy;
   }
-  
+
   public static void addRemoteProxy(final Class<?> proxyType, final ProxyProvider proxyProvider) {
     Assert.notNull(proxyType);
     Assert.notNull(proxyProvider);
-    
+
     remoteProxyProviders.put(proxyType, proxyProvider);
   }
 }
