236b96cefcb16cc4812b0feb36624ca4484b6ccc
Tiago Bento <tiagobentofernandes@gmail.com>
2017-08-14 15:53:51 -0300

    More tests and made @Element have RUNTIME retention policy

--- errai-ui/src/test/java/org/jboss/errai/ui/rebind/ioc/element/ElementProviderExtensionTest.java
+++ errai-ui/src/test/java/org/jboss/errai/ui/rebind/ioc/element/ElementProviderExtensionTest.java
@@ -2,11 +2,11 @@ package org.jboss.errai.ui.rebind.ioc.element;
 
 import org.jboss.errai.codegen.meta.MetaClass;
 import org.jboss.errai.codegen.meta.impl.java.JavaReflectionClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Collection;
 
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.jboss.errai.ui.rebind.ioc.element.ElementProviderExtension.elemental2ElementTags;
 import static org.junit.Assert.assertEquals;
@@ -26,21 +26,30 @@ public class ElementProviderExtensionTest {
   }
 
   @Test
-  @Ignore //FIXME: remove @Ignore
   public void elemental2ElementTag_customElementWithCustomTag() {
     final Collection<String> tags = elemental2ElementTags(metaClass(CustomElement.WithCustomTag.class));
     assertEquals(singletonList("foo"), tags);
   }
 
+
   @Test
-  @Ignore //FIXME: remove @Ignore
   public void elemental2ElementTag_customElementWithCustomTagChild() {
-    final Collection<String> tags = elemental2ElementTags(metaClass(CustomElement.WithCustomTag.Child.class));
-    assertEquals(singletonList("foo"), tags);
+    final Collection<String> tags = elemental2ElementTags(
+            metaClass(CustomElement.WithCustomTag.ChildWithoutElementAnnotation.class));
+
+    assertEquals(emptyList(), tags);
   }
 
   @Test
-  @Ignore //FIXME: remove @Ignore
+  public void elemental2ElementTag_customElementWithCustomTagChildWithElementAnnotation() {
+
+    final Collection<String> tags = elemental2ElementTags(
+            metaClass(CustomElement.WithCustomTag.ChildWithoutElementAnnotation.class));
+
+    assertEquals(emptyList(), tags);
+  }
+
+  @Test
   public void elemental2ElementTag_customElementWithCustomTagChildWithCustomTag() {
 
     final Collection<String> tags = elemental2ElementTags(
