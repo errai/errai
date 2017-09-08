d50e5884eb1559ca80101ce95175dcdc62685f38
Tiago Bento <tiagobentofernandes@gmail.com>
2017-09-08 16:24:42 -0300

    Remove random field name on Export Files

--- errai-common-apt/src/main/java/org/jboss/errai/common/apt/exportfile/ExportFile.java
+++ errai-common-apt/src/main/java/org/jboss/errai/common/apt/exportfile/ExportFile.java
@@ -16,9 +16,16 @@
 
 package org.jboss.errai.common.apt.exportfile;
 
+import org.jboss.errai.codegen.builder.ClassStructureBuilder;
+import org.jboss.errai.codegen.builder.impl.ClassBuilder;
+import org.jboss.errai.codegen.meta.MetaClass;
+import org.jboss.errai.codegen.meta.impl.apt.APTClass;
+
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.util.Set;
+
+import static org.jboss.errai.common.apt.ErraiAptPackages.exportFilesPackagePath;
 
 /**
  * @author Tiago Bento <tfernand@redhat.com>
@@ -28,14 +35,35 @@ public class ExportFile {
   public final String moduleName;
   public final TypeElement annotation;
   public final Set<? extends Element> exportedTypes;
+  public final String simpleClassName;
 
   public ExportFile(final String moduleName, final TypeElement annotation, final Set<? extends Element> exportedTypes) {
     this.moduleName = moduleName;
     this.annotation = annotation;
     this.exportedTypes = exportedTypes;
+    this.simpleClassName = ExportFileName.encodeAnnotationNameAsExportFileName(this);
+  }
+
+  public String generateSource() {
+    final ClassStructureBuilder<?> classBuilder = ClassBuilder.define(getFullClassName()).publicScope().body();
+
+    exportedTypes.stream()
+            .map(Element::asType)
+            .map(APTClass::new)
+            .forEach(exportedType -> classBuilder.publicField(fieldName(exportedType), exportedType).finish());
+
+    return classBuilder.toJavaString();
+  }
+
+  private String fieldName(final MetaClass exportedType) {
+    return exportedType.getCanonicalName().replace(".", "_");
   }
 
   public Boolean hasExportedTypes() {
     return !exportedTypes.isEmpty();
+  }
+
+  public String getFullClassName() {
+    return exportFilesPackagePath() + "." + simpleClassName;
   }
 }
