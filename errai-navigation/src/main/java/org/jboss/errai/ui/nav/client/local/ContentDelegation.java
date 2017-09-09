e34249edd425e521d5b7a27a69b6913f163322f7
BenDol <dolb90@gmail.com>
2017-09-09 13:20:18 +1200

    Update the ContentDelegation java docs.

--- errai-navigation/src/main/java/org/jboss/errai/ui/nav/client/local/ContentDelegation.java
+++ errai-navigation/src/main/java/org/jboss/errai/ui/nav/client/local/ContentDelegation.java
@@ -5,13 +5,32 @@ import org.jboss.errai.ui.nav.client.local.api.DelegationControl;
 
 /**
  * Content delegation control interface.
+ *
  * @author Ben Dol
  */
 public interface ContentDelegation {
 
+    /**
+     * Called when the page is showing its content (setting the container widget).
+     *
+     * @param page the current page being shown.
+     * @param defaultContainer the default content container.
+     * @param widget the widget reference object for the page.
+     * @param previousPage the previous page, <b>this can be null</b>.
+     * @param control the delegation control for proceeding navigation process.
+     */
     void showContent(Object page, NavigatingContainer defaultContainer, IsWidget widget, Object previousPage,
                      DelegationControl control);
 
+    /**
+     * Called when the page is hiding its content (clearing container).
+     *
+     * @param page the current page being hidden.
+     * @param defaultContainer the default content container.
+     * @param widget the widget reference object for the page.
+     * @param nextPage potential next requested page, <b>this can be null</b>.
+     * @param control the delegation control for proceeding navigation process.
+     */
     void hideContent(Object page, NavigatingContainer defaultContainer, IsWidget widget, Object nextPage,
                      DelegationControl control);
 }
