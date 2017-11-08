474cbc6be44764ca5b832e33c50ff2557be161d4
Tiago Bento <tiagobentofernandes@gmail.com>
2017-11-08 12:33:34 -0200

    Add asyncBeanManager property to async tests @ErraiApp

--- errai-ioc/src/test/java/org/jboss/errai/ioc/async/test/beanmanager/ErraiTestApp.java
+++ errai-ioc/src/test/java/org/jboss/errai/ioc/async/test/beanmanager/ErraiTestApp.java
@@ -25,6 +25,7 @@ import org.jboss.errai.ioc.tests.common.ErraiTestModule;
  * @author Tiago Bento <tfernand@redhat.com>
  */
 @ErraiApp(gwtModuleName = "org.jboss.errai.ioc.async.test.beanmanager.AsyncBeanManagerTests",
+          asyncBeanManager = true,
           local = true,
           modules = { ErraiTestApp.class, ErraiIocModule.class, ErraiTestModule.class })
 @ErraiModule
