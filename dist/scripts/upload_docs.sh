9f88317079579776f493dba3684fa37ee1229b2b
Max Barkley <mbarkley@redhat.com>
2017-12-07 11:57:30 -0500

    Remove a-la-carte distribution zip.

--- dist/scripts/upload_docs.sh
+++ dist/scripts/upload_docs.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
 export version=$1
+skip_mkdirs=""
+
+# TODO use getopt
+if [ "$2" = "--skip-mkdirs" ]; then
+    skip_mkdirs=yes
+fi
+
+if [ -z "$version" ]; then
+    echo "FAIL: you must specify the Errai release version number as the first command line arg."
+    exit 1
+fi
+
+set -e
+
+if [ -z "$skip_mkdirs" ]; then
+
+    echo "Making directories..."
+
+    sftp -b - errai@filemgmt.jboss.org <<END
+cd /docs_htdocs/errai
+mkdir $version
+cd $version
+mkdir errai
+END
+
+    echo "Done!"
+fi
 
 echo "Uploading documentation..."
 
