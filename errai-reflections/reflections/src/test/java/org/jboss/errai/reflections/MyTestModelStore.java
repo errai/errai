b8c4beeef9bf6c8d93e0f1d14003bcdad02bcf34
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-03-29 12:52:50 +0100

    Updated to new version 4.6.0.Final

--- errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
+++ errai-reflections/reflections/src/test/java/org/jboss/errai/reflections/MyTestModelStore.java
@@ -1,4 +1,4 @@
-//generated using Reflections JavaCodeSerializer [Sat Nov 25 14:02:08 CET 2017]
+//generated using Reflections JavaCodeSerializer [Fri Mar 29 10:52:40 CET 2019]
 package org.jboss.errai.reflections;
 
 import static org.jboss.errai.reflections.serializers.JavaCodeSerializer.*;
