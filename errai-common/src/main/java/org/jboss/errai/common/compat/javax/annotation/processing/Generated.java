377fa703960286fe249f248d5810fe92b92180f0
Dmitrii Tikhomirov <chani@me.com>
2020-08-12 13:05:53 +0200

    @Generated relocated from errai-ioc to errai-common

--- errai-common/src/main/java/org/jboss/errai/common/compat/javax/annotation/processing/Generated.java
+++ errai-common/src/main/java/org/jboss/errai/common/compat/javax/annotation/processing/Generated.java
@@ -39,9 +39,20 @@
  */
 
 package javax.annotation.processing;
-import java.lang.annotation.*;
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 /**
  * The Generated annotation is used to mark source code that has been generated.
